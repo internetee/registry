ac975f65c74e7ad238f827430b0773cb8c850c1f
Martin Lensment <mlensment@gmail.com>
2014-06-10 13:28:36 +0300

    Skip redundant files when using generators

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,11 @@ module Internetee
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    config.generators do |g|
+      g.stylesheets false
+      g.javascripts false
+      g.helper false
+    end
   end
 end
