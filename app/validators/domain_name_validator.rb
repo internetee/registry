c5355ca8f5642c64da2f8459bcfff1450f19abd3
Martin Lensment <mlensment@gmail.com>
2014-07-01 15:38:45 +0300

    Domain name validation

--- app/validators/domain_name_validator.rb
+++ app/validators/domain_name_validator.rb
@@ -1,5 +1,5 @@
 class DomainNameValidator < ActiveModel::EachValidator
-  #TODO 
+  #TODO
   # validates lenght of 2-63
   # validates/honours Estonian additional letters zäõüö
   # honours punicode and all interfces honors utf8
@@ -14,7 +14,12 @@ class DomainNameValidator < ActiveModel::EachValidator
 
   class << self
     def validate(value)
-      ok = value =~ /\A[a-zA-Z0-9][a-zA-Z0-9-]{0,61}[a-zA-Z0-9]\.ee\z/
+      value = value.mb_chars.downcase.strip
+
+      general_domains = /(.pri.ee|.edu.ee|.aip.ee|.org.ee|.med.ee|.riik.ee|.ee)/ #TODO Add more general domains here
+      unicode_chars = /\u00E4\u00F5\u00F6\u00FC\u0161\u017E/ #äõöüšž
+      ok = value =~ /\A[a-zA-Z0-9#{unicode_chars}][a-zA-Z0-9#{unicode_chars}-]{0,61}[a-zA-Z0-9#{unicode_chars}]#{general_domains}\z/
+
       ok &&= !(value[2] == '-' && value[3] == '-')
     end
   end
--- spec/models/domain_spec.rb
+++ spec/models/domain_spec.rb
@@ -11,13 +11,13 @@ describe Domain do
     d = Fabricate(:domain)
     expect(d.name).to_not be_nil
 
-    invalid = ['a.ee', "#{'a' * 64}.ee", 'ab.eu', 'test.ab.ee', '-test.ee', '-test-.ee', 'test-.ee', 'te--st.ee', 'õ.pri.ee']
+    invalid = ['a.ee', "#{'a' * 64}.ee", 'ab.eu', 'test.ab.ee', '-test.ee', '-test-.ee', 'test-.ee', 'te--st.ee', 'õ.pri.ee', 'test.com', 'www.ab.ee', 'test.eu', '  .ee', 'a b.ee', 'Ž .ee']
 
     invalid.each do |x|
       expect(Fabricate.build(:domain, name: x).valid?).to be false
     end
 
-    valid = ['ab.ee', "#{'a' * 63}.ee", 'te-s-t.ee', 'jäääär.ee', 'päike.pri.ee', 'õigus.edu.ee', 'õäöü.aip.ee', 'test.org.ee', 'test.med.ee', 'test.riik.ee']
+    valid = ['ab.ee', "#{'a' * 63}.ee", 'te-s-t.ee', 'jäääär.ee', 'päike.pri.ee', 'õigus.edu.ee', 'õäöü.aip.ee', 'test.org.ee', 'test.med.ee', 'test.riik.ee', 'žä.ee', '  ŽŠ.ee  ']
 
     valid.each do |x|
       expect(Fabricate.build(:domain, name: x).valid?).to be true
