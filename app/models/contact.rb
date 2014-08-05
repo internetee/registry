1349abbbc764558610c4dbafc88dca479c7af675
Andres Keskküla <andres@gitlab.eu>
2014-08-05 16:55:42 +0300

    Made contact accept address as nested attribute

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -9,6 +9,8 @@ class Contact < ActiveRecord::Base
   belongs_to :created_by, class_name: 'EppUser', foreign_key: :created_by_id
   belongs_to :updated_by, class_name: 'EppUser', foreign_key: :updated_by_id
 
+  accepts_nested_attributes_for :address
+
   validates_presence_of :code, :name, :phone, :email, :ident
 
   validate :ident_must_be_valid
