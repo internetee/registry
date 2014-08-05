a9e1414ddcf59c81a62a6d9e233c7108eb41b0e9
Martin Lensment <mlensment@gmail.com>
2014-08-05 17:50:20 +0300

    Refactor

--- app/models/nameserver.rb
+++ app/models/nameserver.rb
@@ -2,6 +2,6 @@ class Nameserver < ActiveRecord::Base
   belongs_to :registrar
   has_and_belongs_to_many :domains
 
-  validates :hostname, hostname: true
-  validates :ip, format: { with: /\A(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\z/, allow_nil: true, message: :ip_invalid}
+  validates :hostname, format: { with: /\A(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])\z/ }
+  validates :ip, format: { with: /\A(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\z/, allow_nil: true }
 end
