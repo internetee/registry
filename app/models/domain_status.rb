1711c084c092e40ff568115f299190278ca095ee
Martin Lensment <mlensment@gmail.com>
2014-09-02 15:53:25 +0300

    Cleanup

--- app/models/domain_status.rb
+++ app/models/domain_status.rb
@@ -2,16 +2,7 @@ class DomainStatus < ActiveRecord::Base
   # Domain statuses are stored as settings
   include EppErrors
 
-  EPP_ATTR_MAP = {
-    setting: 'status'
-  }
-
   belongs_to :domain
-  # belongs_to :setting
-
-  # delegate :value, :code, to: :setting
-
-  # validates :setting, uniqueness: { scope: :domain_id }
 
   CLIENT_DELETE_PROHIBITED = 'clientDeleteProhibited'
   SERVER_DELETE_PROHIBITED = 'serverDeleteProhibited'
@@ -32,10 +23,4 @@ class DomainStatus < ActiveRecord::Base
   PENDING_UPDATE = 'pendingUpdate'
 
   STATUSES = [CLIENT_DELETE_PROHIBITED, SERVER_DELETE_PROHIBITED, CLIENT_HOLD, SERVER_HOLD, CLIENT_RENEW_PROHIBITED, SERVER_RENEW_PROHIBITED, CLIENT_TRANSFER_PROHIBITED, SERVER_TRANSFER_PROHIBITED, CLIENT_UPDATE_PROHIBITED, SERVER_UPDATE_PROHIBITED, INACTIVE, OK, PENDING_CREATE, PENDING_DELETE, PENDING_RENEW, PENDING_TRANSFER, PENDING_UPDATE]
-
-  def epp_code_map
-    {
-      '2302' => [[:setting, :taken]]
-    }
-  end
 end
