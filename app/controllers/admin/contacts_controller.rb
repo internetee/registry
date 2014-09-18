1777a0e57215bd6148fac0fea6230460478e3f8e
Martin Lensment <mlensment@gmail.com>
2014-09-18 17:15:54 +0300

    Contacts index view

--- app/controllers/admin/contacts_controller.rb
+++ app/controllers/admin/contacts_controller.rb
@@ -1,4 +1,10 @@
 class Admin::ContactsController < ApplicationController
+
+  def index
+    @q = Contact.search(params[:q])
+    @contacts = @q.result.page(params[:page])
+  end
+
   def search
     render json: Contact.search_by_query(params[:q])
   end
