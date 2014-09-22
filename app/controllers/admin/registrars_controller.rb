18c7c2f374a3767455fe536f9bf30f4b7f2a0323
Martin Lensment <mlensment@gmail.com>
2014-09-22 20:19:35 +0300

    Add registrars index

--- app/controllers/admin/registrars_controller.rb
+++ app/controllers/admin/registrars_controller.rb
@@ -2,4 +2,9 @@ class Admin::RegistrarsController < ApplicationController
   def search
     render json: Registrar.search_by_query(params[:q])
   end
+
+  def index
+    @q = Registrar.search(params[:q])
+    @registrars = @q.result.page(params[:page])
+  end
 end
