7963330aebff9320fb21a776d02816353d61ccc2
Martin Lensment <mlensment@gmail.com>
2014-09-26 14:13:20 +0300

    Refactor contact forms to client

--- app/controllers/admin/contacts_controller.rb
+++ app/controllers/admin/contacts_controller.rb
@@ -1,47 +1,9 @@
 class Admin::ContactsController < AdminController
-  # TODO created_by and updated_by ids
-  before_action :set_contact, only: [:show, :destroy, :edit, :update]
+  before_action :set_contact, only: [:show]
 
   def index
     @q = Contact.search(params[:q])
     @contacts = @q.result.page(params[:page])
-  end
-
-  def new
-    @contact = Contact.new
-    @contact.build_address
-  end
-
-  def create
-    @contact = Contact.new(contact_params)
-    @contact.generate_code
-    if @contact.save
-      flash[:notice] = I18n.t('shared.contact_added')
-      redirect_to [:admin, @contact]
-    else
-      flash[:alert] = I18n.t('shared.failed_to_create_contact')
-      render "new"
-    end
-  end
-
-  def destroy
-    if @contact.destroy_and_clean
-      flash[:notice] = I18n.t('shared.contact_deleted')
-      redirect_to admin_contacts_path
-    else
-      flash[:alert] = I18n.t('shared.failed_to_delete_contact')
-      redirect_to [:admin, @contact]
-    end
-  end
-
-  def update
-    if @contact.update_attributes(contact_params)
-      flash[:notice] = I18n.t('shared.contact_updated')
-      redirect_to [:admin, @contact]
-    else
-      flash[:alert] = I18n.t('shared.failed_to_update_contact')
-      redirect_to [:admin, @contact]
-    end
   end
 
   def search
@@ -52,10 +14,5 @@ class Admin::ContactsController < AdminController
 
   def set_contact
     @contact = Contact.find(params[:id])
-  end
-
-  def contact_params
-    params.require(:contact).permit( :email, :phone, :fax, :ident_type, :ident, :auth_info, :name, :org_name,
-                                    address_attributes: [:city, :street, :zip, :street2, :street3, :country_id])
   end
 end
