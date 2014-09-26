f837500e7a277b109019960270020a8fd39cef01
Martin Lensment <mlensment@gmail.com>
2014-09-26 14:30:55 +0300

    Test login to production env too

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,15 @@
 class SessionsController < Devise::SessionsController
   def create
-    if Rails.env.development? || Rails.env.test?
-      @user = User.find_by(username: 'gitlab') if params[:gitlab]
-      @user = User.find_by(username: 'zone') if params[:zone]
+    #TODO: Create ID Card login here:
+    # this is just testing config
+    # if Rails.env.development? || Rails.env.test?
+    @user = User.find_by(username: 'gitlab') if params[:gitlab]
+    @user = User.find_by(username: 'zone') if params[:zone]
 
-      flash[:notice] = I18n.t('shared.welcome')
-      sign_in_and_redirect @user, :event => :authentication
-      return
-    end
+    flash[:notice] = I18n.t('shared.welcome')
+    sign_in_and_redirect @user, :event => :authentication
+    return
+    # end
   end
 
   def login
