d2565d646df63e48c26a5e50c521353a7a86d603
Martin Lensment <mlensment@gmail.com>
2014-09-29 12:41:15 +0300

    More tests on domain transferring

--- spec/features/domain_transfer_spec.rb
+++ spec/features/domain_transfer_spec.rb
@@ -12,7 +12,7 @@ feature 'Domain transfer', type: :feature do
     Fabricate(:domain, registrar: zone)
   end
 
-  scenario 'when registrar requests transfer on own domain', js: true do
+  scenario 'Registrar requests transfer on own domain', js: true do
     sign_in zone_user
     click_on 'Domains'
     click_on 'Transfer domain'
@@ -32,7 +32,7 @@ feature 'Domain transfer', type: :feature do
     expect(page).to have_text('Domain already belongs to the querying registrar')
   end
 
-  scenario 'when other registrar requests transfer' do
+  scenario 'Other registrar requests transfer with 0 wait time' do
     sign_in elkdata_user
     d = Domain.first
     visit client_domains_path
@@ -46,6 +46,72 @@ feature 'Domain transfer', type: :feature do
     expect(page).to have_text('Domain transfer approved!')
     expect(page).to have_text('serverApproved')
 
+    visit client_domains_path
+    expect(page).to have_link(d.name)
+  end
+
+  scenario 'Other registrar requests transfer with 1 wait time' do
+    s = Setting.find_by(code: 'transfer_wait_time')
+    s.value = 1
+    s.save
+
+    sign_in elkdata_user
+    d = Domain.first
+    visit client_domains_path
+    expect(page).to_not have_link(d.name)
+
+    visit new_client_domain_transfer_path
+    fill_in 'Domain name', with: d.name
+    fill_in 'Domain password', with: d.auth_info
+    click_on 'Request domain transfer'
+
+    expect(page).to have_text('Domain transfer requested!')
+    expect(page).to have_text('pending')
+
+    visit new_client_domain_transfer_path
+    fill_in 'Domain name', with: d.name
+    fill_in 'Domain password', with: d.auth_info
+    click_on 'Request domain transfer'
+
+    expect(page).to have_text('Domain transfer requested!')
+    expect(page).to have_text('pending')
+
+    visit client_domains_path
+    expect(page).to_not have_link(d.name)
+  end
+
+  scenario 'Domain owner approves request' do
+    d = Domain.first
+    d.domain_transfers.create(
+      status: DomainTransfer::PENDING,
+      transfer_requested_at: Time.zone.now,
+      transfer_to: elkdata,
+      transfer_from: zone
+    )
+
+    sign_in elkdata_user
+    visit new_client_domain_transfer_path
+    fill_in 'Domain name', with: d.name
+    fill_in 'Domain password', with: d.auth_info
+    click_on 'Request domain transfer'
+
+    expect(page).to have_text('Domain transfer requested!')
+    expect(page).to_not have_button('Approve')
+
+    sign_in zone_user
+
+    visit new_client_domain_transfer_path
+    fill_in 'Domain name', with: d.name
+    fill_in 'Domain password', with: d.auth_info
+    click_on 'Request domain transfer'
+
+    expect(page).to have_button('Approve')
+
+    click_on 'Approve'
+    expect(page).to have_text('Domain transfer approved!')
+    expect(page).to have_text('clientApproved')
+
+    sign_in elkdata_user
     visit client_domains_path
     expect(page).to have_link(d.name)
   end
