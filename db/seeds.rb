95abb1161dc80e8114591facd8dcccac830ee71e
Priit Tamboom <priit@gitlab.eu>
2014-10-07 17:02:20 +0300

    Added bang to seeds methods, otherwise they fail silently

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,29 +6,62 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Country.where(name: 'Estonia', iso: 'EE').first_or_create
-Country.where(name: 'Latvia', iso: 'LV').first_or_create
+Country.where(name: 'Estonia', iso: 'EE').first_or_create!
+Country.where(name: 'Latvia', iso: 'LV').first_or_create!
 
 zone = Registrar.where(
   name: 'Zone Media OÜ',
   reg_no: '10577829',
-  address: 'Lõõtsa 2, Tallinna linn, Harju maakond, 11415'
-  #country: Country.first
-).first_or_create
+  address: 'Lõõtsa 2, Tallinna linn, Harju maakond, 11415',
+  country: Country.first
+).first_or_create!
 
-EppUser.where(username: 'zone', password: 'ghyt9e4fu', active: true, registrar: zone).first_or_create
+EppUser.where(
+  username: 'zone', 
+  password: 'ghyt9e4fu',
+  active: true,
+  registrar: zone
+).first_or_create!
 
 elkdata = Registrar.where(
   name: 'Elkdata OÜ',
   reg_no: '10510593',
-  address: 'Tondi 51-10, 11316 Tallinn'
-  #country: Country.first
-).first_or_create
+  address: 'Tondi 51-10, 11316 Tallinn',
+  country: Country.first
+).first_or_create!
 
-EppUser.where(username: 'elkdata', password: '8932iods', active: true, registrar: elkdata).first_or_create
+EppUser.where(
+  username: 'elkdata',
+  password: '8932iods',
+  active: true,
+  registrar: elkdata
+).first_or_create!
 
-User.where(username: 'gitlab', password: '12345', email: 'enquiries@gitlab.eu', admin: true, identity_code: '37810013855', country: Country.where(name: 'Estonia').first).first_or_create
-User.where(username: 'zone', password: '54321', email: 'info-info@zone.ee', admin: false, identity_code: '37810010085',
-           registrar_id: zone.id, country: Country.where(name: 'Estonia').first).first_or_create
-User.where(username: 'elkdata', password: '32154', email: 'info-info@elkdata.ee', admin: false, identity_code: '37810010727',
-           registrar_id: elkdata.id, country: Country.where(name: 'Estonia').first).first_or_create
+User.where(
+  username: 'gitlab',
+  password: '12345',
+  email: 'enquiries@gitlab.eu',
+  admin: true,
+  identity_code: '37810013855',
+  country: Country.where(name: 'Estonia').first
+).first_or_create!
+
+User.where(
+  username: 'zone',
+  password: '54321',
+  email: 'info-info@zone.ee',
+  admin: false,
+  identity_code: '37810010085',
+  registrar_id: zone.id, 
+  country: Country.where(name: 'Estonia').first
+).first_or_create!
+
+User.where(
+  username: 'elkdata',
+  password: '32154',
+  email: 'info-info@elkdata.ee',
+  admin: false,
+  identity_code: '37810010727',
+  registrar_id: elkdata.id,
+  country: Country.where(name: 'Estonia').first
+).first_or_create!
