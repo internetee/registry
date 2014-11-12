c7b58d8b06e4f703226497c12c59e18ce1e45071
Andres Keskküla <andres@gitlab.eu>
2014-11-12 18:12:17 +0200

    Mina also restarts active job daemon

--- config/deploy.rb
+++ config/deploy.rb
@@ -85,6 +85,8 @@ task deploy: :environment do
 
     to :launch do
       queue "mkdir -p #{deploy_to}/current/tmp; touch #{deploy_to}/current/tmp/restart.txt"
+      queue "RAILS_ENV=production bin/delayed_job stop"
+      queue "RAILS_ENV=production bin/delayed_job start"
     end
   end
 end
