89c57d22b078432257323266d2c9730dc117733d
Martin Lensment <mlensment@gmail.com>
2014-12-11 16:57:26 +0200

    Glue records fix

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141210085432) do
+ActiveRecord::Schema.define(version: 20141211095604) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
