f48df84da1953c786cfda8f3e6af222947c0be31
Andres Keskküla <andres@gitlab.eu>
2014-12-22 16:38:35 +0200

    Rubocop

--- app/models/whois/public_server.rb
+++ app/models/whois/public_server.rb
@@ -1,6 +1,6 @@
 module Whois
   class PublicServer < ActiveRecord::Base
     self.abstract_class = true
-    #establish_connection :"#{Rails.env}_public_whois"
+    # establish_connection :"#{Rails.env}_public_whois"
   end
 end
