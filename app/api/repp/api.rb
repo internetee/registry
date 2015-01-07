4af3e80087f5c4d6ebcc9f5aa238335586803411
Martin Lensment <mlensment@gmail.com>
2015-01-07 16:36:07 +0200

    Refactor

--- app/api/repp/api.rb
+++ app/api/repp/api.rb
@@ -2,7 +2,6 @@ module Repp
   class API < Grape::API
     format :json
     prefix :repp
-    version 'v1', using: :path
 
     http_basic do |username, password|
       @current_user ||= EppUser.find_by(username: username, password: password)
@@ -12,15 +11,6 @@ module Repp
       attr_reader :current_user
     end
 
-    resource :domains do
-      desc 'Return list of domains'
-      get '/' do
-        domains = current_user.registrar.domains.page(params[:page])
-        {
-          domains: domains,
-          total_pages: domains.total_pages
-        }
-      end
-    end
+    mount Repp::DomainV1
   end
 end
---
+++ spec/requests/domain_spec.rb
@@ -0,0 +1,3 @@
+describe Repp::API do
+
+end
