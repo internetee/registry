2c2e829af24a1ecee68947a91a0fea26af6662c5
Priit Tark <priit@gitlab.eu>
2015-01-08 11:52:48 +0200

    update whenever to honor rbenv

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,22 +3,12 @@
 # It's helpful, but not entirely necessary to understand cron before proceeding.
 # http://en.wikipedia.org/wiki/Cron
 
-# Example:
-#
-set :output, 'log/cron.log'
-#
-# every 2.hours do
-#   command "/usr/bin/some_great_command"
-#   runner "MyModel.some_method"
-#   rake "some:great:rake:task"
-# end
-#
-# every 4.days do
-#   runner "AnotherModel.prune_old_records"
-# end
-
 every 10.minutes do
   runner 'ZonefileSetting.generate_zonefiles'
 end
 
-# Learn more: http://github.com/javan/whenever
+set :output, 'log/cron.log'
+
+# rbenv
+set :job_template, 
+  "/bin/bash -l -c 'export PATH=\"$HOME/.rbenv/bin:$PATH\"; eval \"$(rbenv init -)\"; :job'"
