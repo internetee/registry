9e824a43f80a05f9e544717b4c9f9e4e3077192f
Priit Tark <priit@gitlab.eu>
2015-01-08 14:29:58 +0200

    syntax cleanup

--- config/schedule.rb
+++ config/schedule.rb
@@ -5,7 +5,7 @@
 
 # rbenv support
 rbenv = 'export PATH="$HOME/.rbenv/bin:$PATH";eval "$(rbenv init -)";'
-path  = Whenever.path.sub(/\/releases\/.*/, '/current')
+path  = Whenever.path.sub(%r(\/releases\/.*), '/current')
 set :job_template, "/bin/bash -l -c '#{rbenv} :job'"
 job_type :runner, "cd #{path} && bin/rails r -e :environment \":task\" :output"
 
