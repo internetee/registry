3326b934efadef77a1808e1e3dc947a94f367901
Priit Tark <priit@gitlab.eu>
2015-01-08 11:55:25 +0200

    crontab update

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,12 +3,12 @@
 # It's helpful, but not entirely necessary to understand cron before proceeding.
 # http://en.wikipedia.org/wiki/Cron
 
-every 10.minutes do
-  runner 'ZonefileSetting.generate_zonefiles'
-end
-
-set :output, 'log/cron.log'
-
 # rbenv
 set :job_template, 
   "/bin/bash -l -c 'export PATH=\"$HOME/.rbenv/bin:$PATH\"; eval \"$(rbenv init -)\"; :job'"
+
+set :output, 'log/cron.log'
+
+every 10.minutes do
+  runner 'ZonefileSetting.generate_zonefiles'
+end
