3cfc7c12195d23fbf523b02d4374861e3c64b826
Priit Tark <priit@gitlab.eu>
2015-01-08 12:37:50 +0200

    whenever cleanup

--- config/schedule.rb
+++ config/schedule.rb
@@ -4,10 +4,10 @@
 # http://en.wikipedia.org/wiki/Cron
 
 # rbenv support
-set :job_template, 
-  "/bin/bash -l -c 'export PATH=\"$HOME/.rbenv/bin:$PATH\";eval\"$(rbenv init -)\"; :job'"
-set :path, Whenever.path.to_s.sub('^\\','')
-job_type :runner, ":path/bin/rails r -e :environment ':task' :output"
+rbenv = 'export PATH="$HOME/.rbenv/bin:$PATH";eval "$(rbenv init -)";'
+path  = Whenever.path.to_s.sub('^\\','')
+set :job_template, "/bin/bash -l -c '#{rbenv} :job'"
+job_type :runner, "#{path}/bin/rails r -e :environment ':task' :output"
 
 # cron output
 set :output, 'log/cron.log'
