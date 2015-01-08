d692088f3bc1884f3c3277d5f802c38cee6c8d5e
Martin Lensment <mlensment@gmail.com>
2015-01-08 14:34:33 +0200

    Contact list for REPP

---
+++ app/api/repp/contact_v1.rb
@@ -0,0 +1,16 @@
+module Repp
+  class ContactV1 < Grape::API
+    version 'v1', using: :path
+
+    resource :contacts do
+      desc 'Return list of contact'
+      get '/' do
+        contacts = current_user.registrar.contacts.page(params[:page])
+        {
+          contacts: contacts,
+          total_pages: contacts.total_pages
+        }
+      end
+    end
+  end
+end
