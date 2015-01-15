6c4e5da5f6e163900f4af94892af387760ff117e
Priit Tark <priit@gitlab.eu>
2015-01-15 17:12:59 +0200

    added api log to database example

--- app/models/api_log/db.rb
+++ app/models/api_log/db.rb
@@ -1,6 +1,6 @@
 module ApiLog
   class Db < ActiveRecord::Base
     self.abstract_class = true
-    establish_connection "api_log_#{Rails.env}".to_sym
+    establish_connection "api_log_#{Rails.env}"
   end
 end
