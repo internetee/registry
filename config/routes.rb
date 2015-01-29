19c4106e18b798f39333d791333ba85c90c95263
Priit Tark <priit@gitlab.eu>
2015-01-29 16:45:30 +0200

    Routes file refactored

--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,4 @@
-class EppConstraint
-  OBJECT_TYPES = {
-    domain: { domain: 'urn:ietf:params:xml:ns:domain-1.0' },
-    contact: { contact: 'urn:ietf:params:xml:ns:contact-1.0' }
-  }
-
-  def initialize(type)
-    @type = type
-  end
-
-  # creates parsed_frame, detects epp request object
-  def matches?(request)
-    parsed_frame = Nokogiri::XML(request.params[:raw_frame])
-
-    unless [:keyrelay, :poll].include?(@type)
-      element = "//#{@type}:#{request.params[:action]}"
-      return false if parsed_frame.xpath("#{element}", OBJECT_TYPES[@type]).none?
-    end
-
-    request.params[:parsed_frame] = parsed_frame.remove_namespaces!
-    request.params[:epp_object_type] = @type
-    true
-  end
-end
+require 'epp_constraint'
 
 Rails.application.routes.draw do
   namespace(:epp, defaults: { format: :xml }) do
