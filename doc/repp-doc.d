d9316579cbf9c2e959e86da644807b1865d32cb1
Martin Lensment <mlensment@gmail.com>
2015-03-18 11:39:03 +0200

    REPP doc

---
+++ doc/repp-doc.d
@@ -0,0 +1,8 @@
+# REPP integration specification
+
+REPP uses currently Basic Authorization
+
+Main communication specification through Restful EPP (REPP):
+
+[Contact related functions](repp/v1/contact.md)
+[Domain related functions](repp/v1/domain.md)
