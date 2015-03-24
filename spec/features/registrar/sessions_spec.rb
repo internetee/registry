5d08164f1010ea113c7bee9637c3f637cdb86b74
Martin Lensment <mlensment@gmail.com>
2015-03-24 17:17:13 +0200

    SIM error test

--- spec/features/registrar/sessions_spec.rb
+++ spec/features/registrar/sessions_spec.rb
@@ -50,9 +50,36 @@ feature 'Sessions', type: :feature do
   end
 
   scenario 'Api user should when there is a sim error', js: true do
-    client = instance_double("Digidoc::Client",
-      session_code: '123'
+    client = instance_double("Digidoc::Client", session_code: '123')
+
+    allow(client).to receive('session_code=')
+
+    allow(client).to receive(:authenticate).and_return(
+      OpenStruct.new(
+        user_id_code: '14212128025'
+      )
     )
+
+    allow(client).to receive('authentication_status').and_return(
+      OpenStruct.new(status: 'SIM_ERROR')
+    )
+
+    allow(Digidoc::Client).to receive(:new) { client }
+
+    visit registrar_login_path
+    page.should have_css('a[href="/registrar/login/mid"]')
+
+    page.find('a[href="/registrar/login/mid"]').click
+
+    fill_in 'user_phone', with: '00007'
+    click_button 'Log in'
+
+    page.should have_text('Check your phone for confirmation code')
+    page.should have_text('SIM application error')
+  end
+
+  scenario 'Api user should log in', js: true do
+    client = instance_double("Digidoc::Client", session_code: '123')
 
     allow(client).to receive('session_code=')
 
