8e1648772d5e3cbeecc990dbd3c1a2d4306de1c7
Priit Tark <priit@gitlab.eu>
2015-04-07 16:01:17 +0300

    Include explicit Depp ApplicationHelper

--- vendor/gems/depp/app/controllers/depp/application_controller.rb
+++ vendor/gems/depp/app/controllers/depp/application_controller.rb
@@ -2,7 +2,7 @@ module Depp
   # class ApplicationController < ::ApplicationController
   class ApplicationController < ActionController::Base
     include CurrentUserHelper
-    include ApplicationHelper
+    include Depp::ApplicationHelper
 
     # Prevent CSRF attacks by raising an exception.
     # For APIs, you may want to use :null_session instead.
@@ -16,23 +16,7 @@ module Depp
 
     before_action :authenticate_user
     def authenticate_user
-      if ENV['session_timeout']
-        redirect_to main_app.login_url and return unless depp_current_user && session[:last_seen]
-
-        if (session[:last_seen].to_i + ENV['session_timeout'].to_i) < Time.now.to_i
-          session_timeout
-        else
-          session[:last_seen] = Time.now.to_i
-        end
-      else
-        redirect_to main_app.login_url and return unless depp_current_user
-      end
-    end
-
-    def session_timeout
-      reset_session
-      flash[:alert] = t('your_session_has_timed_out')
-      redirect_to main_app.login_url and return
+      redirect_to main_app.login_url and return unless depp_current_user
     end
 
     def depp_current_user
