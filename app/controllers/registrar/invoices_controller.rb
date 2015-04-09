56853817aaa516c3533ff7e9b1d6f2286a9f2bdd
Martin Lensment <mlensment@gmail.com>
2015-04-09 11:48:56 +0300

    Basic invoice show

--- app/controllers/registrar/invoices_controller.rb
+++ app/controllers/registrar/invoices_controller.rb
@@ -1,6 +1,17 @@
 class Registrar::InvoicesController < RegistrarController
   load_and_authorize_resource
 
+  before_action :set_invoice, only: [:show]
+
   def index
+  end
+
+  def show
+  end
+
+  private
+
+  def set_invoice
+    @invoice = Invoice.find(params[:id])
   end
 end
--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -3,4 +3,8 @@ class Invoice < ActiveRecord::Base
   belongs_to :buyer, class_name: 'Registrar'
   has_many :invoice_items
   accepts_nested_attributes_for :invoice_items
+
+  def seller_address
+    [seller_street, seller_city, seller_state, seller_zip].reject(&:blank?).compact.join(', ')
+  end
 end
