fc9b8f6ef4933a6096c7f49e6d281dc045dba467
Martin Lensment <mlensment@gmail.com>
2015-04-22 13:13:44 +0300

    Add test emails

--- app/mailers/invoice_mailer.rb
+++ app/mailers/invoice_mailer.rb
@@ -1,5 +1,10 @@
 class InvoiceMailer < ApplicationMailer
   def invoice_email(invoice, pdf)
+    unless Rails.env.production?
+      test_emails = ['martin@gitlab.eu', 'priit@gitlab.eu']
+      return unless test_emails.include?(invoice.billing_email)
+    end
+
     attachments[invoice.pdf_name] = pdf
     mail(to: invoice.billing_email, subject: invoice)
   end
