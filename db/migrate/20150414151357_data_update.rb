ede71f8e6194c667e7f2e26b83d9e68ce258c447
Priit Tark <priit@gitlab.eu>
2015-05-05 17:43:11 +0300

    EIS migration commented out

--- db/migrate/20150414151357_data_update.rb
+++ db/migrate/20150414151357_data_update.rb
@@ -1,18 +1,18 @@
 class DataUpdate < ActiveRecord::Migration
   def change
-    Registrar.where(
-      name: 'EIS',
-      reg_no: '90010019',
-      phone: '+372 727 1000',
-      country_code: 'EE',
-      vat_no: 'EE101286464',
-      email: 'info@internet.ee',
-      state: 'Harjumaa',
-      city: 'Tallinn',
-      street: 'Paldiski mnt 80',
-      zip: '10617',
-      url: 'www.internet.ee',
-      code: 'EIS'
-    ).first_or_create!
+    # Registrar.where(
+      # name: 'EIS',
+      # reg_no: '90010019',
+      # phone: '+372 727 1000',
+      # country_code: 'EE',
+      # vat_no: 'EE101286464',
+      # email: 'info@internet.ee',
+      # state: 'Harjumaa',
+      # city: 'Tallinn',
+      # street: 'Paldiski mnt 80',
+      # zip: '10617',
+      # url: 'www.internet.ee',
+      # code: 'EIS'
+    # ).first_or_create!
   end
 end
