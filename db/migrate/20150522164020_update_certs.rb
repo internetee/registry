626905fb49c564a08814b445c6e7ae805a5cd237
Martin Lensment <mlensment@gmail.com>
2015-05-22 19:49:25 +0300

    Add certs migration

---
+++ db/migrate/20150522164020_update_certs.rb
@@ -0,0 +1,15 @@
+class UpdateCerts < ActiveRecord::Migration
+  def change
+    Certificate.all.each do |x|
+      if x.crt.present? && x.csr.present?
+        x.interface = Certificate::REGISTRAR
+        x.md5 = OpenSSL::Digest::MD5.new(x.parsed_crt.to_der).to_s
+      elsif x.crt.present? && x.csr.blank?
+        x.interface = Certificate::API
+        x.md5 = OpenSSL::Digest::MD5.new(x.parsed_crt.to_der).to_s
+      elsif x.crt.blank? && x.csr.present?
+        x.interface = Certificate::REGISTRAR
+      end
+    end
+  end
+end
