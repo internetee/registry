f6c7dd48dbf6baa9af2bf8f934a407efae7d2ad6
Martin Lensment <mlensment@gmail.com>
2015-05-25 11:04:53 +0300

    Typo fix

--- db/migrate/20150525075550_update_certs_add_cn_from_csr.rb
+++ db/migrate/20150525075550_update_certs_add_cn_from_csr.rb
@@ -2,7 +2,7 @@ class UpdateCertsAddCnFromCsr < ActiveRecord::Migration
   def change
     Certificate.all.each do |x|
       if x.crt.blank? && x.csr.present?
-        pc = x.parsed_crt.try(:subject).try(:to_s) || ''
+        pc = x.parsed_csr.try(:subject).try(:to_s) || ''
         cn = pc.scan(/\/CN=(.+)/).flatten.first
         x.common_name = cn.split('/').first
       end
