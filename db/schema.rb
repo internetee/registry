cb02ac4c93ff451b74d1b58072e5f030a8d7a4af
Martin Lensment <mlensment@gmail.com>
2015-06-10 15:06:20 +0300

    Fix renew restriction #2610

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150609103333) do
+ActiveRecord::Schema.define(version: 20150610112238) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
