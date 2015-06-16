0ec635f42b761b83dcbda8dbb5d7148142309b31
Priit Tark <priit@gitlab.eu>
2015-06-16 17:47:38 +0300

    Added money helper to rspec

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,9 @@ require 'action_view/railtie'
 require 'sprockets/railtie'
 # require "rails/test_unit/railtie"
 
+# fix, otherwise rake db:all:setup not working
+require 'money-rails/hooks'
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
