8b7ac451d08e1388848acbae38936b57d91dc47c
Priit Tark <priit@gitlab.eu>
2015-06-26 18:44:46 +0300

    Refactor object validator

--- app/validators/object_count_validator.rb
+++ app/validators/object_count_validator.rb
@@ -1,7 +1,9 @@
 class ObjectCountValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     min, max = options[:min].call, options[:max].call
-    return if value.count { |i| !i.marked_for_destruction? }.between?(min, max)
+    values = value.reject(&:marked_for_destruction?)
+
+    return if values.size.between?(min, max)
     association = options[:association] || attribute
     record.errors.add(association, :out_of_range, { min: min, max: max })
   end
