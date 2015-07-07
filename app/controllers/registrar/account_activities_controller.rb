ca2a2db9ad396bfc9f0bd278ba1f348b61b1c12b
Martin Lensment <mlensment@gmail.com>
2015-07-07 11:22:32 +0300

    Add filter to account activities #2691

--- app/controllers/registrar/account_activities_controller.rb
+++ app/controllers/registrar/account_activities_controller.rb
@@ -2,9 +2,17 @@ class Registrar::AccountActivitiesController < RegistrarController
   load_and_authorize_resource
 
   def index
+    params[:q] ||= {}
     account = current_user.registrar.cash_account
+
+    ca_cache = params[:q][:created_at_lteq]
+    end_time = params[:q][:created_at_lteq].try(:to_date)
+    params[:q][:created_at_lteq] = end_time.try(:end_of_day)
+
     @q = account.activities.includes(:invoice).search(params[:q])
     @q.sorts = 'id desc' if @q.sorts.empty?
     @account_activities = @q.result.page(params[:page])
+
+    params[:q][:created_at_lteq] = ca_cache
   end
 end
--- app/models/account_activity.rb
+++ app/models/account_activity.rb
@@ -13,5 +13,11 @@ class AccountActivity < ActiveRecord::Base
     account.balance += sum
     account.save
   end
+
+  class << self
+    def types_for_select
+      [CREATE, RENEW, ADD_CREDIT].map { |x| [I18n.t(x), x] }
+    end
+  end
 end
 
