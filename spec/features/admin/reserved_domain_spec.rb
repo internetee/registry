d99c00172265786e8342bbec3cb9e0541ffbbbd2
Martin Lensment <mlensment@gmail.com>
2015-07-09 13:16:14 +0300

    Add reserved domains feature test #2565

---
+++ spec/features/admin/reserved_domain_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+feature 'ReservedDomain', type: :feature do
+  before :all do
+    @user = Fabricate(:admin_user)
+  end
+
+  before do
+    sign_in @user
+  end
+
+  it 'should manage reserved domains' do
+    visit admin_reserved_domains_url
+    page.should have_content('Reserved domains')
+
+    d = Fabricate.build(:domain, name: '110.ee')
+    d.valid?
+    d.errors.full_messages.should match_array([])
+
+    fill_in 'reserved_domains', with: "110.ee: testpw"
+    click_button 'Save'
+
+    page.should have_content('Record updated')
+    page.should have_content('110.ee: testpw')
+
+    d.valid?
+    d.errors.full_messages.should match_array(["Domain is reserved and requires correct auth info"])
+  end
+end
