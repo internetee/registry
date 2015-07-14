8eb2484e6ec463f9092728c25f9dc8451de133a0
Priit Tark <priit@gitlab.eu>
2015-07-14 11:04:23 +0300

    Filter only frame and raw_frame and not parsed_frame #2757

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -4,5 +4,5 @@
 Rails.application.config.filter_parameters += [:password]
 
 Rails.application.config.filter_parameters << lambda do |key, value|
-  value.to_s.gsub!(/pw>.+<\//, 'pw>[FILTERED]</') if key =~ /frame|raw_frame/i
+  value.to_s.gsub!(/pw>.+<\//, 'pw>[FILTERED]</') if key =~ /^(frame|raw_frame)$/i
 end
