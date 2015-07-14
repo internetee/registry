e097cac210a9f4189d3308fc8475e8f420487e1c
Martin Lensment <mlensment@gmail.com>
2015-07-14 14:04:26 +0300

    Update schema

--- db/schema-read-only.rb
+++ db/schema-read-only.rb
@@ -936,14 +936,14 @@ ActiveRecord::Schema.define(version: 20150713113436) do
   end
 
   create_table "que_jobs", id: false, force: :cascade do |t|
-    t.integer  "priority",    limit: 2, default: 100,                   null: false
-    t.datetime "run_at",                default: '2015-06-30 14:16:50', null: false
-    t.integer  "job_id",      limit: 8, default: 0,                     null: false
-    t.text     "job_class",                                             null: false
-    t.json     "args",                  default: [],                    null: false
-    t.integer  "error_count",           default: 0,                     null: false
+    t.integer  "priority",    limit: 2, default: 100,                                        null: false
+    t.datetime "run_at",                default: "now()",                                    null: false
+    t.integer  "job_id",      limit: 8, default: "nextval('que_jobs_job_id_seq'::regclass)", null: false
+    t.text     "job_class",                                                                  null: false
+    t.json     "args",                  default: [],                                         null: false
+    t.integer  "error_count",           default: 0,                                          null: false
     t.text     "last_error"
-    t.text     "queue",                 default: "",                    null: false
+    t.text     "queue",                 default: "",                                         null: false
   end
 
   create_table "registrant_verifications", force: :cascade do |t|
