4178b7d97d1a3f2c822d2a673e453124cc167097
Priit Tark <priit@gitlab.eu>
2015-07-28 10:33:13 +0300

    Restore eis custom files #2793

---
+++ config/initializers/eis_custom_active_model.rb
@@ -0,0 +1,18 @@
+# Log all active model user errors
+module ActiveModel
+  class Errors
+    def add(attribute, message = :invalid, options = {})
+      message = normalize_message(attribute, message, options)
+      if exception = options[:strict]
+        exception = ActiveModel::StrictValidationFailed if exception == true
+        raise exception, full_message(attribute, message)
+      end
+
+      # CUSTOM logging
+      Rails.logger.info "USER MSG: ACTIVEMODEL: #{@base.try(:class)} [#{attribute}] #{message}" if message.present?
+      # END of CUSTOM logging
+
+      self[attribute] << message
+    end
+  end
+end
---
+++ config/initializers/eis_custom_flash.rb
@@ -0,0 +1,27 @@
+# Log all flash messages
+module ActionDispatch
+  class Flash
+    def call(env)
+      @app.call(env)
+    ensure
+      session    = Request::Session.find(env) || {}
+      flash_hash = env[KEY]
+
+      if flash_hash && (flash_hash.present? || session.key?('flash'))
+        session["flash"] = flash_hash.to_session_value
+
+        # EIS custom logging
+        Rails.logger.info "USER MSG: FLASH: #{session['flash']['flashes'].inspect}" if session['flash']
+        # END OF EIS custom logging
+
+        env[KEY] = flash_hash.dup
+      end
+
+      if (!session.respond_to?(:loaded?) || session.loaded?) && # (reset_session uses {}, which doesn't implement #loaded?)
+        session.key?('flash') && session['flash'].nil?
+        session.delete('flash')
+      end
+    end
+  end
+end
+
