e5d03e3cdf6b9b7779cf769f2bc43300e85945bb
Priit Tark <priit@gitlab.eu>
2015-08-11 12:45:52 +0300

    Rubocop update

--- app/models/depp/user.rb
+++ app/models/depp/user.rb
@@ -71,6 +71,7 @@ module Depp
 
     private
 
+    # rubocop:disable Metrics/AbcSize
     def validate_existance_in_server
       return if errors.any?
       res = server.open_connection
@@ -96,5 +97,6 @@ module Depp
           "port: #{ENV['epp_port']}, cert_path: #{ENV['cert_path']}, key_path: #{ENV['key_path']}"
         errors.add(:base, :invalid_cert)
     end
+    # rubocop:enable Metrics/AbcSize
   end
 end
