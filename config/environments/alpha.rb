adfc79fa998815c4d7c7a0e4c07113b0de3b0241
Priit Tark <priit@gitlab.eu>
2015-08-13 16:42:39 +0300

    Turn off syslog for temp

--- config/environments/alpha.rb
+++ config/environments/alpha.rb
@@ -49,7 +49,7 @@ Rails.application.configure do
   config.log_tags = [:subdomain, :uuid, :remote_ip]
 
   # Use a different logger for distributed setups.
-  config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new(ENV['app_name']))
+  # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new(ENV['app_name']))
 
   # Use a different logger for distributed setups.
   # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
