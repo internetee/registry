87e18a8a19c8a70722058a1949780bd9d6b7a8fb
Priit Tark <priit@gitlab.eu>
2015-08-27 13:54:45 +0300

    Restore old spec

--- spec/models/setting_spec.rb
+++ spec/models/setting_spec.rb
@@ -4,8 +4,8 @@ describe Setting do
   it 'returns value' do
     expect(Setting.ns_min_count).to eq(2)
     Setting.ns_min_count = '2'
-    expect(Setting.ns_min_count).to eq(2)
+    expect(Setting.ns_min_count).to eq('2')
     Setting.ns_min_count = true
-    expect(Setting.ns_min_count).to eq(true)
+    expect(Setting.ns_min_count).to eq(2)
   end
 end
