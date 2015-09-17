1fbc5ec6ee6827ddfb3fc5c183fe70040a1aa862
Martin Lensment <mlensment@gmail.com>
2015-09-17 18:32:39 +0300

    Add loggin to cache clear cron #2925

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -2,6 +2,8 @@ class Setting < RailsSettings::CachedSettings
   include Versions # version/setting_version.rb
 
   def self.reload_settings!
+    STDOUT << "#{Time.zone.now.utc} - Clearing settings cache\n"
     Rails.cache.delete_matched('settings:.*')
+    STDOUT << "#{Time.zone.now.utc} - Settings cache cleared\n"
   end
 end
