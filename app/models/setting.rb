6ff10268f34f274fcc4eb8105098efabf898e981
Martin Lensment <mlensment@gmail.com>
2015-09-17 15:53:06 +0300

    Add method to reload settings #2925

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -1,3 +1,7 @@
 class Setting < RailsSettings::CachedSettings
   include Versions # version/setting_version.rb
+
+  def self.reload_settings!
+    Rails.cache.delete_matched('settings:.*')
+  end
 end
