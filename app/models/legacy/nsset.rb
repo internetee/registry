1b6badf699dc8d05f33363b554d303626cd72922
Vladimir Krylov <vladimir.krylov@perfectline.co>
2015-10-30 14:01:02 +0200

    Story#104941754 Fix relation of legacy Nsset

--- app/models/legacy/nsset.rb
+++ app/models/legacy/nsset.rb
@@ -3,7 +3,7 @@ module Legacy
     self.table_name = :nsset
 
     belongs_to :object, foreign_key: :id
-    belongs_to :object_state, foreign_key: :id, primary_key: :object_id
+    belongs_to :object_registry, foreign_key: :id
     has_many :hosts, foreign_key: :nssetid
   end
 end
