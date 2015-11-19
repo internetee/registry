b8f1dbbf54b5d0abe8b2e4e03c5e472244205473
Vladimir Krylov <vladimir.krylov@perfectline.co>
2015-11-19 12:27:06 +0200

    Stroy#107699154 disclosed should have array

--- app/models/whois_record.rb
+++ app/models/whois_record.rb
@@ -48,7 +48,7 @@ class WhoisRecord < ActiveRecord::Base
 
     h[:registrant]       = domain.registrant.name
     h[:registrant_email] = domain.registrant.email
-    @disclosed << domain.registrant.email
+    @disclosed << [:email, domain.registrant.email]
     h[:changed]          = domain.registrant.updated_at.try(:to_s, :iso8601)
 
     h[:admin_contacts] = []
