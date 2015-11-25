02d78ac1d3ee81aefeb48b1475b3651ce32a7adc
Matt Farnsworth <matt@perfectline.co>
2015-11-25 15:26:52 +0200

    Story 108621138 - include domain name in msg body, updates a0ff1c99d04654cb861834b2b4fa91656cd22aad

--- app/models/domain.rb
+++ app/models/domain.rb
@@ -329,7 +329,7 @@ class Domain < ActiveRecord::Base
       domain.destroy
       bye_bye = domain.versions.last
       domain.registrar.messages.create!(
-          body: "#{I18n.t(:domain_deleted)}: #{name}",
+          body: "#{I18n.t(:domain_deleted)}: #{domain.name}",
           attached_obj_id: bye_bye.id,
           attached_obj_type: bye_bye.class.to_s # DomainVersion
       )
