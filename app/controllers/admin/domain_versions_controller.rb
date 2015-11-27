3bc47e1d993c824441da33d11d27ee5a056f659e
Vladimir Krylov <vladimir.krylov@perfectline.co>
2015-11-27 11:05:05 +0200

    Story#109057934 - Admin::DomainVersions controller shouldn't go to EPP once again

--- app/controllers/admin/domain_versions_controller.rb
+++ app/controllers/admin/domain_versions_controller.rb
@@ -6,12 +6,13 @@ class Admin::DomainVersionsController < AdminController
     @domain = Domain.where(id: params[:domain_id]).includes({versions: :item}).first
     @versions = @domain.versions
 
-    if @domain.pending_json.present?
-      frame = Nokogiri::XML(@domain.pending_json['frame'])
-      @pending_user  = User.find(@domain.pending_json['current_user_id'])
-      @pending_domain = Epp::Domain.find(@domain.id)
-      @pending_domain.update(frame, @pending_user, false)
-    end
+    # Depricated it had to load legal document. We may do it by parsing and adding link.
+    # if @domain.pending_json.present?
+    #   frame = Nokogiri::XML(@domain.pending_json['frame'])
+    #   @pending_user  = User.find(@domain.pending_json['current_user_id'])
+    #   @pending_domain = Epp::Domain.find(@domain.id)
+    #   @pending_domain.update(frame, @pending_user, false)
+    # end
   end
   # rubocop:enable Style/GuardClause
 end
