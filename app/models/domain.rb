2141ac2183e548898d7d37e476598efdf5aee942
Vladimir Krylov <vladimir.krylov@perfectline.co>
2015-11-30 13:14:08 +0200

    Story#107821878 - manage old registrar

--- app/models/domain.rb
+++ app/models/domain.rb
@@ -417,6 +417,7 @@ class Domain < ActiveRecord::Base
   end
 
   def pending_update!
+    old_registrar_id = registrar_id
     return true if pending_update?
     self.epp_pending_update = true # for epp
 
@@ -428,7 +429,7 @@ class Domain < ActiveRecord::Base
     new_registrant_email = registrant.email
     new_registrant_name  = registrant.name
 
-    DomainMailer.pending_update_request_for_old_registrant(id, registrant_id_was, deliver_emails).deliver
+    DomainMailer.pending_update_request_for_old_registrant(id, old_registrar_id, deliver_emails).deliver
     DomainMailer.pending_update_notification_for_new_registrant(id, deliver_emails).deliver
 
     reload # revert back to original
