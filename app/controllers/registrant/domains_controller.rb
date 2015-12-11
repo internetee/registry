cf1f9818b3ff9c4eae0d9f78d6f95d1538c1d41d
Matt Farnsworth <matt@perfectline.co>
2015-12-11 14:41:57 +0200

    Story #105846070 - enable user view, registrant shows user domains

--- app/controllers/registrant/domains_controller.rb
+++ app/controllers/registrant/domains_controller.rb
@@ -1,5 +1,7 @@
 class Registrant::DomainsController < RegistrantController
   def index
     authorize! :view, :registrant_domains
+    ident_cc, ident = @current_user.registrant_ident.split '-'
+    @domains = BusinessRegistryCache.fetch_associated_domains ident, ident_cc
   end
 end
