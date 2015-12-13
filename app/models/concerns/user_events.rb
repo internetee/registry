5aa0ae64239bcf1f6f4f131a9d6efa1dcd5bc549
Matt Farnsworth <matt@perfectline.co>
2015-12-13 13:41:44 +0200

    Story #109367694 - add better support for cr_id

--- app/models/concerns/user_events.rb
+++ app/models/concerns/user_events.rb
@@ -1,6 +1,18 @@
 module UserEvents
   extend ActiveSupport::Concern
 
+  def cr_id
+    if versions.first.object.nil?
+      cr_registrar_id =versions.first.object_changes['registrar_id'].second
+    else
+      # untested, expected never to execute
+      cr_registrar_id = versions.first.object['registrar_id']
+    end
+    if cr_registrar_id.present?
+      Registrar.find(cr_registrar_id).code
+    end
+  end
+
   # TODO: remove old
   # module ClassMethods
     # def registrar_events(id)
