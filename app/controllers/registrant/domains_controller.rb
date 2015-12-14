3d71a01280cd62c7edca72b69254656319ebee4e
Matt Farnsworth <matt@perfectline.co>
2015-12-14 13:13:55 +0200

    Story #105846070 - revert domains_controller and domains/index to allow merge

--- app/controllers/registrant/domains_controller.rb
+++ app/controllers/registrant/domains_controller.rb
@@ -1,12 +1,5 @@
 class Registrant::DomainsController < RegistrantController
   def index
     authorize! :view, :registrant_domains
-    ident_cc, ident = @current_user.registrant_ident.split '-'
-    begin
-      @domains = BusinessRegistryCache.fetch_associated_domains ident, ident_cc
-    rescue Soap::Arireg::NotAvailableError => error
-      flash[:notice] = I18n.t(error.message[:message])
-      @domains = []
-    end
   end
 end
