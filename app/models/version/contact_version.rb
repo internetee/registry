1cefe4ed5fa710371974bb90ed93ef7aca9c1c66
Matt Farnsworth <matt@perfectline.co>
2015-12-14 11:28:26 +0200

    Story #109367694 - refactor: move UserEvents include up, wrap method in included block, say self

--- app/models/version/contact_version.rb
+++ app/models/version/contact_version.rb
@@ -2,8 +2,6 @@ class ContactVersion < PaperTrail::Version
   include VersionSession
   self.table_name    = :log_contacts
   self.sequence_name = :log_contacts_id_seq
-
-  include UserEvents
-
+  
   # scope :deleted, -> { where(event: 'destroy') }
 end
