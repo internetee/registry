6842fbccd64ff2fbbe7d6dd499604add402d0f42
Stas <stanislav@perfectline.co>
2015-12-16 11:37:02 +0200

    109818648-whois_view_change

--- app/models/whois_record.rb
+++ app/models/whois_record.rb
@@ -44,7 +44,7 @@ class WhoisRecord < ActiveRecord::Base
     h[:changed]    = domain.updated_at.try(:to_s, :iso8601)
     h[:expire]     = domain.valid_to.try(:to_date).try(:to_s)
     h[:outzone]    = domain.outzone_at.try(:to_date).try(:to_s)
-    h[:delete]     = domain.delete_at.try(:to_date).try(:to_s)
+    h[:delete]     = [domain.delete_at, domain.force_delete_at].compact.min.try(:to_date).try(:to_s)
 
 
     h[:registrant]       = domain.registrant.name
