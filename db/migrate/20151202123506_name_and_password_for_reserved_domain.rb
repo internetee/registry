6be09329d76ac410a1c66f0c5d6c272baa45c5c0
Vladimir Krylov <vladimir.krylov@perfectline.co>
2016-01-05 15:03:29 +0200

    Story#108091488 - add index to reserved domains

--- db/migrate/20151202123506_name_and_password_for_reserved_domain.rb
+++ db/migrate/20151202123506_name_and_password_for_reserved_domain.rb
@@ -2,6 +2,7 @@ class NameAndPasswordForReservedDomain < ActiveRecord::Migration
   def up
     add_column :reserved_domains, :name, :string
     add_column :reserved_domains, :password, :string
+    add_index  :reserved_domains, :name
 
     ReservedDomain.find_each do |domain|
       names = domain.names
