82caac3a176e19246464035a6caaed5139478588
Vladimir Krylov <vladimir.krylov@perfectline.co>
2016-01-25 10:51:29 +0200

    Story#112054829 - fetch file

---
+++ app/models/legacy/file.rb
@@ -0,0 +1,14 @@
+module Legacy
+  class File < Db
+    self.table_name = :files
+
+    def self.for_history history_id
+      sql = %Q{select history.id, files.path, files.name, files.crdate
+              from history
+                join action ON action.id=history.action
+                join files on action.servertrid=files.servertrid
+              where history.id =#{history_id};}
+      find_by_sql(sql).to_a
+    end
+  end
+end
