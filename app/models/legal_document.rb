653b97a86715c87e7ff55f7f07d0b2a787488e47
Stas <stanislav@perfectline.co>
2016-02-25 12:23:20 +0200

    113524121-round_for_expression

--- app/models/legal_document.rb
+++ app/models/legal_document.rb
@@ -9,7 +9,7 @@ class LegalDocument < ActiveRecord::Base
 
   belongs_to :documentable, polymorphic: true
 
-  validates :body, length: { minimum: 1.37 * 1.4 }
+  validates :body, length: { minimum: (1.37 * 1.4).round }
 
   before_create :add_creator
   before_save   :save_to_filesystem
