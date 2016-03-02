57bd1c77b6e9ae9fcdcb5cc9c1d9006fffd3c51b
Stas <stanislav@perfectline.co>
2016-03-02 17:44:25 +0200

    113430903-new_job_added

---
+++ app/jobs/domain_delete_job.rb
@@ -0,0 +1,16 @@
+class DomainDeleteJob < Que::Job
+
+  def run(domain_id)
+    domain = Domain.find(domain_id)
+
+    WhoisRecord.where(domain_id: domain.id).destroy_all
+
+    domain.destroy
+    bye_bye = domain.versions.last
+    domain.registrar.messages.create!(
+        body: "#{I18n.t(:domain_deleted)}: #{domain.name}",
+        attached_obj_id: bye_bye.id,
+        attached_obj_type: bye_bye.class.to_s
+    )
+  end
+end
--- app/jobs/domain_set_delete_candidate_job.rb
+++ app/jobs/domain_set_delete_candidate_job.rb
@@ -4,5 +4,6 @@ class DomainSetDeleteCandidateJob < Que::Job
     domain = Domain.find(domain_id)
     domain.statuses << DomainStatus::DELETE_CANDIDATE
     domain.save(validate: false)
+    DomainDeleteJob.enqueue(domain.id, run_at: rand(24*60).minutes.from_now)
   end
 end
