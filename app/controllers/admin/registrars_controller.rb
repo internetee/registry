c957bce55301c9fa6076fb7f341a7acf8cf236d0
Vladimir Krylov <vladimir.krylov@perfectline.co>
2016-03-16 17:48:35 +0200

    Story#105855968 - change registrar parameter from exclude_in_monthly_directo to test_registrar be used more widely

--- app/controllers/admin/registrars_controller.rb
+++ app/controllers/admin/registrars_controller.rb
@@ -57,7 +57,7 @@ class Admin::RegistrarsController < AdminController
   def registrar_params
     params.require(:registrar).permit(
       :name, :reg_no, :vat_no, :street, :city, :state, :zip, :billing_address,
-      :country_code, :email, :phone, :billing_email, :code, :exclude_in_monthly_directo
+      :country_code, :email, :phone, :billing_email, :code, :test_registrar
     )
   end
 end
