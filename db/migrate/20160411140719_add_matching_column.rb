c9c4e82989d1e9ab3bf5ac5aaf08fa27b17456be
Stas <stanislav@perfectline.co>
2016-04-14 17:33:07 +0300

    115595167-log_table_list_changed

--- db/migrate/20160411140719_add_matching_column.rb
+++ db/migrate/20160411140719_add_matching_column.rb
@@ -1,7 +1,12 @@
 class AddMatchingColumn < ActiveRecord::Migration
 
   def change
-    tables = [:log_domains, :log_contacts]
+    tables = [:log_account_activities, :log_accounts, :log_addresses, :log_api_users, :log_bank_statements,
+              :log_bank_transactions, :log_blocked_domains, :log_certificates, :log_contact_statuses, :log_contacts,
+              :log_countries, :log_dnskeys, :log_domain_contacts, :log_domain_statuses, :log_domain_transfers,
+              :log_domains, :log_invoice_items, :log_invoices, :log_keyrelays, :log_messages, :log_nameservers,
+              :log_pricelists, :log_registrars, :log_reserved_domains, :log_settings, :log_users, :log_white_ips,
+              :log_zonefile_settings]
 
     tables.each do |table|
       add_column table, :uuid, :text
