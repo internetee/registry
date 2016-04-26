47483f8d7576bdc1cb3b4681b8b472ce68d3c4fa
Vladimir Krylov <vladimir.krylov@perfectline.co>
2016-04-26 11:06:49 +0300

    Story#118300251 - remove old not needed code

--- app/controllers/admin/domain_versions_controller.rb
+++ app/controllers/admin/domain_versions_controller.rb
@@ -1,18 +1,8 @@
 class Admin::DomainVersionsController < AdminController
   load_and_authorize_resource
 
-  # rubocop:disable Style/GuardClause
   def index
     @domain = Domain.where(id: params[:domain_id]).includes({versions: :item}).first
     @versions = @domain.versions
-
-    # Depricated it had to load legal document. We may do it by parsing and adding link.
-    # if @domain.pending_json.present?
-    #   frame = Nokogiri::XML(@domain.pending_json['frame'])
-    #   @pending_user  = User.find(@domain.pending_json['current_user_id'])
-    #   @pending_domain = Epp::Domain.find(@domain.id)
-    #   @pending_domain.update(frame, @pending_user, false)
-    # end
   end
-  # rubocop:enable Style/GuardClause
 end
