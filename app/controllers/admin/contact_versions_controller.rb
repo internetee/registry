4b9ba0fe13d5afdfe19e4ae77a1c66d02add0ea7
Vladimir Krylov <vladimir.krylov@perfectline.co>
2016-05-25 11:09:06 +0300

    Story#108869472 - contact history uses same pagination what domain history does

--- app/controllers/admin/contact_versions_controller.rb
+++ app/controllers/admin/contact_versions_controller.rb
@@ -29,10 +29,19 @@ class Admin::ContactVersionsController < AdminController
 
   def show
     per_page = 7
-    @version = ContactVersion.find(params[:id])
-    @q = ContactVersion.where(item_id: @version.item_id).order(created_at: :desc).search
-    @versions = @q.result.page(params[:page])
-    @versions = @versions.per(per_page)
+    @version  = ContactVersion.find(params[:id])
+    @versions = ContactVersion.where(item_id: @version.item_id).order(created_at: :desc)
+
+    # what we do is calc amount of results until needed version
+    # then we cacl which page it is
+    if params[:page].blank?
+      counter = @versions.where("created_at > ?", @version.created_at).count
+      page    = counter / per_page
+      page   += 1 if (counter % per_page) != 0
+      params[:page] = page
+    end
+
+    @versions = @versions.page(params[:page]).per(per_page)
   end
 
   def search
