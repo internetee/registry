371ec46f420a769b4d3d9357e31b6a62e5fc8908
Vladimir Krylov <vladimir.krylov@perfectline.co>
2016-06-15 17:28:49 +0300

    Story#121580929 fix when incoming frame is empty

--- lib/epp_constraint.rb
+++ lib/epp_constraint.rb
@@ -12,8 +12,8 @@ class EppConstraint
   def matches?(request)
     # TODO: Maybe move this to controller to keep params clean
     request.params[:raw_frame] = request.params[:raw_frame].gsub!(/(?<=>)(.*?)(?=<)/) { |s| s.strip} if request.params[:raw_frame]
-    request.params[:nokogiri_frame] ||= Nokogiri::XML(request.params[:raw_frame])
-    request.params[:parsed_frame] ||= request.params[:nokogiri_frame].dup.remove_namespaces!
+    request.params[:nokogiri_frame] ||= Nokogiri::XML(request.params[:raw_frame] || request.params[:frame])
+    request.params[:parsed_frame]   ||= request.params[:nokogiri_frame].dup.remove_namespaces!
 
     unless [:keyrelay, :poll, :session, :not_found].include?(@type)
       element = "//#{@type}:#{request.params[:action]}"
