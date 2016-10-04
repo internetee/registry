ec3201e475bd419d6425db4567457c9b813c1649
Georg Kahest <teadur@users.noreply.github.com>
2016-10-04 13:26:16 +0300

    missing parenthesis

--- db/migrate/20161004101419_adduuid_index_to_epp_logs.rb
+++ db/migrate/20161004101419_adduuid_index_to_epp_logs.rb
@@ -1,7 +1,7 @@
 class AdduuidIndexToEppLogs < ActiveRecord::Migration
     def change
-      ApiLog::EppLog.connection.execute( "CREATE INDEX CONCURRENTLY epp_logs_uuid  ON epp_logs  USING btree uuid;")
-      ApiLog::ReppLog.connection.execute( "CREATE INDEX CONCURRENTLY repp_logs_uuid  ON repp_logs  USING btree uuid;")
+      ApiLog::EppLog.connection.execute( "CREATE INDEX CONCURRENTLY epp_logs_uuid  ON epp_logs  USING btree (uuid);")
+      ApiLog::ReppLog.connection.execute( "CREATE INDEX CONCURRENTLY repp_logs_uuid  ON repp_logs  USING btree (uuid);")
    end
 end
 
