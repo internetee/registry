f2f23a623beaf208d54e07ab428168c49ffce553
Artur Beljajev <artur.beljajev@internet.ee>
2016-10-20 23:20:04 +0300

    Do not show deprecation errors in test env
    #206

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -31,8 +31,7 @@ Rails.application.configure do
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
 
-  # Print deprecation notices to the stderr.
-  config.active_support.deprecation = :stderr
+  ActiveSupport::Deprecation.silenced = true
 
   # For rails-settings-cached conflict
   config.cache_store = :file_store, 'tmp/cache_test'
@@ -42,7 +41,7 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
-  # The available log levels are: :debug, :info, :warn, :error, :fatal, and :unknown, 
+  # The available log levels are: :debug, :info, :warn, :error, :fatal, and :unknown,
   # corresponding to the log level numbers from 0 up to 5 respectively
   config.log_level = :debug
 
@@ -70,4 +69,4 @@ end
 # In this mode, any jobs you queue will be run in the same thread, synchronously
 # (that is, MyJob.enqueue runs the job and won't return until it's completed).
 # This makes your application's behavior easier to test
-Que.mode = :sync 
+Que.mode = :sync
