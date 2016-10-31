411079f654825a6e9978f04617d7376764d23ed9
Artur Beljajev <artur.beljajev@internet.ee>
2016-10-31 17:28:02 +0200

    Fix domain factory
    registry-180

--- spec/factories/domain.rb
+++ spec/factories/domain.rb
@@ -7,6 +7,7 @@ FactoryGirl.define do
     registrant
 
     after :build do |domain|
+      domain.class.skip_callback(:commit, :after, :update_whois_record) # Otherwise FactoryGirl reports Domain#json= method as undefined
       domain.nameservers << FactoryGirl.build_pair(:nameserver)
       domain.admin_domain_contacts << FactoryGirl.build(:admin_domain_contact)
       domain.tech_domain_contacts << FactoryGirl.build(:tech_domain_contact)
