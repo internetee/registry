c9530a42cb3a2b8b58c9bad9c7bc2274be1fa476
Artur Beljajev <artur.beljajev@internet.ee>
2016-11-09 16:07:00 +0200

    Load all translation files from config/locales

--- config/application.rb
+++ config/application.rb
@@ -26,9 +26,8 @@ module Registry
     config.time_zone = ENV['time_zone'] || 'Tallinn'  # NB! It should be defined,
                                                       # otherwise ActiveRecord usese other class internally.
 
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.yml').to_s]
+    config.i18n.default_locale = :en
 
     config.paths.add File.join('app', 'api'), glob: File.join('**', '*.rb')
     config.autoload_paths += Dir[Rails.root.join('app', 'api', '*')]
