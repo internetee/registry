3a040793b042d61ec5b2f37bc1a944f089849e65
Artur Beljajev <artur.beljajev@internet.ee>
2016-11-14 03:27:03 +0200

    Refactor domain mailer views
    #180

--- spec/views/mailers/shared/registrant/_registrant.en.text.erb_spec.rb
+++ spec/views/mailers/shared/registrant/_registrant.en.text.erb_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 require_relative 'registrant_shared'
 
-RSpec.describe 'mailers/domain_mailer/registrant/_registrant.en.text.erb' do
+RSpec.describe 'mailers/shared/registrant/_registrant.en.text.erb' do
   include_examples 'domain mailer registrant info'
 end
--- spec/views/mailers/shared/registrar/_registrar.ru.text.erb_spec.rb
+++ spec/views/mailers/shared/registrar/_registrar.ru.text.erb_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 require_relative 'registrar_shared'
 
-RSpec.describe 'mailers/domain_mailer/registrar/_registrar.ru.text.erb' do
+RSpec.describe 'mailers/shared/registrar/_registrar.ru.text.erb' do
   include_examples 'domain mailer registrar info'
 end
