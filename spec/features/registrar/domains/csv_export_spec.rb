127bbf5167af731cb14c805c0c80b1a007cc6878
Artur Beljajev <artur.beljajev@internet.ee>
2016-11-26 01:45:21 +0200

    Add csv export button to domain list in registrar area
    #248

--- spec/features/registrar/domains/csv_export_spec.rb
+++ spec/features/registrar/domains/csv_export_spec.rb
@@ -2,7 +2,17 @@ require 'rails_helper'
 
 RSpec.feature 'CSV Export', db: true do
   scenario 'csv file download' do
+    sign_in
     visit registrar_domains_path
-    click_on 'download-domains-csv-btn'
+    click_on 'export-domains-csv-btn'
+  end
+
+  def sign_in(user: create(:user_with_valid_password))
+    visit registrar_login_path
+
+    fill_in 'depp_user_tag', with: user.email
+    fill_in 'depp_user_password', with: user.password
+
+    click_button 'Login'
   end
 end
