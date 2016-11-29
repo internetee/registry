ae5f2bf922b06c9a7f1f7e3edec9c2d39108e552
Artur Beljajev <artur.beljajev@internet.ee>
2016-11-30 01:50:00 +0200

    Add domain list csv export to registrar area
    #248

--- spec/features/registrar/domains/csv_export_spec.rb
+++ spec/features/registrar/domains/csv_export_spec.rb
@@ -2,11 +2,12 @@ require 'rails_helper'
 
 RSpec.feature 'CSV Export', db: true do
   background do
-    sign_in(user: FactoryGirl.create(:api_user))
+    Setting.registrar_ip_whitelist_enabled = false
+    sign_in_to_registrar_area(user: FactoryGirl.create(:api_user))
   end
 
-  scenario 'csv file download' do
-    visit registrar_domains_path
-    click_on 'export-domains-csv-btn'
+  scenario 'exports csv' do
+    visit registrar_domains_url
+    click_button t('registrar.domains.index.export_csv_btn')
   end
 end
