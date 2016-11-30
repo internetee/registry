8d173afd590da4a93ffe0ad76595a3a342b07fba
Artur Beljajev <artur.beljajev@internet.ee>
2016-11-30 02:02:37 +0200

    Fix session helpers spec support

--- spec/support/features/session_helpers.rb
+++ spec/support/features/session_helpers.rb
@@ -1,7 +1,7 @@
 module Features
   module SessionHelpers
     def sign_in_to_registrar_area(user: FactoryGirl.create(:api_user))
-      visit registrar_login_path
+      visit registrar_login_url
 
       fill_in 'depp_user_tag', with: user.username
       fill_in 'depp_user_password', with: user.password
