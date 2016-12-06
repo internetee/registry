2a8c855300af842cb00059180e84efb7116c94fc
Artur Beljajev <artur.beljajev@internet.ee>
2016-12-06 22:30:41 +0200

    Add epp contact spec
    #251

---
+++ spec/requests/epp/contacts_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe 'EPP contact' do
+  describe 'create' do
+    context 'when address processing is enabled' do
+      # get 'epp/command/create' do
+      #
+      # end
+    end
+
+    context 'when address processing is disabled' do
+      # get 'epp/command/create' do
+      #
+      # end
+    end
+  end
+end
