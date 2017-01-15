d294105c59b8be35675c0a1af29dc40373a41e64
Artur Beljajev <artur.beljajev@internet.ee>
2017-01-16 01:36:38 +0200

    Add "removed_company" template to delete domain mailer
    #268

--- spec/views/mailers/domain_delete_mailer/forced/removed_company.html.erb_spec.rb
+++ spec/views/mailers/domain_delete_mailer/forced/removed_company.html.erb_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
-require_relative 'forced_shared'
+require_relative 'removed_company'
 
-RSpec.describe 'mailers/domain_delete_mailer/forced.html.erb' do
+RSpec.describe 'mailers/domain_delete_mailer/forced/removed_company.html.erb' do
   before :example do
     stub_template 'mailers/shared/registrar/_registrar.et.html' => 'test registrar estonian'
     stub_template 'mailers/shared/registrar/_registrar.en.html' => 'test registrar english'
