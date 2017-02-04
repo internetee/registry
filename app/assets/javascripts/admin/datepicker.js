26f091affb63f59842376509b4dca1e49c99c0b6
Artur Beljajev <artur.beljajev@internet.ee>
2017-02-04 18:51:50 +0200

    Improve datepicker UI

--- app/assets/javascripts/admin/datepicker.js
+++ app/assets/javascripts/admin/datepicker.js
@@ -1,3 +1,14 @@
-$('.datepicker').datepicker({
-    dateFormat: 'yy-mm-dd'
+$.datepicker.setDefaults({
+  changeMonth: true,
+  changeYear: true,
+  duration: 'fast',
+  dateFormat: 'yy-mm-dd',
+});
+
+var dateFields = $('.datepicker');
+
+dateFields.datepicker();
+
+dateFields.each(function () {
+  this.autocomplete = 'off';
 });
