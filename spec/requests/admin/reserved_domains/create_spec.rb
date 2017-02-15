7bf16fcbf72623cf886f2aa09c3e59408e69ad76
Artur Beljajev <artur.beljajev@internet.ee>
2017-02-15 13:42:25 +0200

    Update reserved domain spec
    #269

--- spec/requests/admin/reserved_domains/create_spec.rb
+++ spec/requests/admin/reserved_domains/create_spec.rb
@@ -8,33 +8,30 @@ RSpec.describe 'admin reserved domain create' do
   end
 
   it 'creates new reserved domain' do
-    expect { post admin_reserved_domains_path, { reserved_domain: attributes_for(:reserved_domain) } }
+    expect { post admin_reserved_domains_path, reserved_domain: attributes_for(:reserved_domain) }
       .to change { ReservedDomain.count }.from(0).to(1)
   end
 
   it 'saves domain name' do
-    post admin_reserved_domains_path, { reserved_domain: attributes_for(:reserved_domain,
-                                                                        name: 'test.com')
-    }
+    post admin_reserved_domains_path, reserved_domain: attributes_for(:reserved_domain,
+                                                                      name: 'test.com')
     expect(reserved_domain.name).to eq('test.com')
   end
 
   it 'saves password' do
-    post admin_reserved_domains_path, { reserved_domain: attributes_for(:reserved_domain,
-                                                                        password: 'test')
-    }
+    post admin_reserved_domains_path, reserved_domain: attributes_for(:reserved_domain,
+                                                                      password: 'test')
     expect(reserved_domain.password).to eq('test')
   end
 
   it 'generates password' do
-    post admin_reserved_domains_path, { reserved_domain: attributes_for(:reserved_domain,
-                                                                        password: '')
-    }
+    post admin_reserved_domains_path, reserved_domain: attributes_for(:reserved_domain,
+                                                                      password: '')
     expect(reserved_domain.password).to be_present
   end
 
   it 'redirects to :index' do
-    post admin_reserved_domains_path, { reserved_domain: attributes_for(:reserved_domain) }
+    post admin_reserved_domains_path, reserved_domain: attributes_for(:reserved_domain)
     expect(response).to redirect_to admin_reserved_domains_path
   end
 
@@ -44,10 +41,9 @@ RSpec.describe 'admin reserved domain create' do
                             password: 'dispute-password') }
 
     it 'replaces password with the one from dispute' do
-      post admin_reserved_domains_path, { reserved_domain: attributes_for(:reserved_domain,
-                                                                          name: 'test.com',
-                                                                          password: 'reserved-domain-password')
-      }
+      post admin_reserved_domains_path, reserved_domain: attributes_for(:reserved_domain,
+                                                                        name: 'test.com',
+                                                                        password: 'reserved-domain-password')
       expect(reserved_domain.password).to eq('dispute-password')
     end
   end
