085e02f15e6a5c3c46ac33ab059b91e17ddaf1af
Artur Beljajev <artur.beljajev@internet.ee>
2017-02-15 04:16:00 +0200

    Close dispute if valid password is provided by EPP domain:update
    #269

--- spec/requests/registrant/domain_update_confirms/update_spec.rb
+++ spec/requests/registrant/domain_update_confirms/update_spec.rb
@@ -8,36 +8,11 @@ RSpec.describe 'domain_update_confirms update' do
                          name: 'test.com',
                          registrant_verification_asked_at: Time.zone.parse('05.07.2010'),
                          registrant_verification_token: 'test',
-                         pending_json: { frame: request_xml,
+                         pending_json: { frame: Nokogiri::XML(request_xml).remove_namespaces!.to_s,
                                          current_user_id: 1,
                                          new_registrant_id: 1 })
   }
   let!(:new_registrant) { create(:registrant, code: 'new-code') }
-  let!(:dispute) { create(:dispute, domain_name: 'test.com', password: 'test') }
-  let(:request_xml) { <<-XML
-    <?xml version="1.0" encoding="UTF-8" standalone="no"?>
-    <epp xmlns="https://epp.tld.ee/schema/epp-ee-1.0.xsd">
-      <command>
-        <update>
-          <domain:update xmlns:domain="https://epp.tld.ee/schema/domain-eis-1.0.xsd">
-            <domain:name>test.com</domain:name>
-              <domain:chg>
-                <domain:registrant>new-code</domain:registrant>
-              </domain:chg>
-          </domain:update>
-        </update>
-        <extension>
-          <eis:extdata xmlns:eis="https://epp.tld.ee/schema/eis-1.0.xsd">
-            <eis:legalDocument type="pdf">#{valid_legal_document}</eis:legalDocument>
-            <eis:reserved>
-              <eis:pw>test</eis:pw>
-            </eis:reserved>
-          </eis:extdata>
-        </extension>
-      </command>
-    </epp>
-  XML
-  }
   subject(:request) { patch registrant_domain_update_confirm_path(domain),
                             confirmed: true,
                             token: 'test' }
@@ -46,14 +21,72 @@ RSpec.describe 'domain_update_confirms update' do
     travel_to Time.zone.parse('05.07.2010')
   end
 
-  it 'closes dispute' do
-    expect { request }.to change { Dispute.count }.from(1).to(0)
+  context 'when domain is disputed' do
+    let!(:dispute) { create(:dispute, domain_name: 'test.com', password: 'test') }
+    let(:request_xml) { <<-XML
+      <?xml version="1.0" encoding="UTF-8" standalone="no"?>
+      <epp xmlns="https://epp.tld.ee/schema/epp-ee-1.0.xsd">
+        <command>
+          <update>
+            <domain:update xmlns:domain="https://epp.tld.ee/schema/domain-eis-1.0.xsd">
+              <domain:name>test.com</domain:name>
+                <domain:chg>
+                  <domain:registrant>new-code</domain:registrant>
+                </domain:chg>
+            </domain:update>
+          </update>
+          <extension>
+            <eis:extdata xmlns:eis="https://epp.tld.ee/schema/eis-1.0.xsd">
+              <eis:legalDocument type="pdf">#{valid_legal_document}</eis:legalDocument>
+              <eis:reserved>
+                <eis:pw>test</eis:pw>
+              </eis:reserved>
+            </eis:extdata>
+          </extension>
+        </command>
+      </epp>
+    XML
+    }
+
+
+    it 'closes dispute' do
+      expect { request }.to change { Dispute.count }.from(1).to(0)
+    end
+
+    it 'redirects to :show' do
+      request
+      expect(response).to redirect_to registrant_domain_update_confirm_path(domain,
+                                                                            confirmed: true)
+    end
   end
 
-  it 'redirects to :show' do
-    request
-    p controller.flash
-    expect(response).to redirect_to registrant_domain_update_confirm_path(domain,
-                                                                          confirmed: true)
+  context 'when domain is not disputed' do
+    let(:request_xml) { <<-XML
+      <?xml version="1.0" encoding="UTF-8" standalone="no"?>
+      <epp xmlns="https://epp.tld.ee/schema/epp-ee-1.0.xsd">
+        <command>
+          <update>
+            <domain:update xmlns:domain="https://epp.tld.ee/schema/domain-eis-1.0.xsd">
+              <domain:name>test.com</domain:name>
+                <domain:chg>
+                  <domain:registrant>new-code</domain:registrant>
+                </domain:chg>
+            </domain:update>
+          </update>
+          <extension>
+            <eis:extdata xmlns:eis="https://epp.tld.ee/schema/eis-1.0.xsd">
+              <eis:legalDocument type="pdf">#{valid_legal_document}</eis:legalDocument>
+            </eis:extdata>
+          </extension>
+        </command>
+      </epp>
+    XML
+    }
+
+    it 'redirects to :show' do
+      request
+      expect(response).to redirect_to registrant_domain_update_confirm_path(domain,
+                                                                            confirmed: true)
+    end
   end
 end
