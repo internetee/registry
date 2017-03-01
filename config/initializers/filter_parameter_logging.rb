e70c84b2802aac7dd6e74af8ea056b0e376652ab
Artur Beljajev <artur.beljajev@internet.ee>
2017-03-01 23:13:40 +0200

    Filter EPP XML legalDocument tag
    #314

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -1,6 +1,10 @@
 Rails.application.configure do
-  config.filter_parameters += [:password, :nokogiri_frame, :parsed_frame]
+  config.filter_parameters += [:password, /^frame$/, /^nokogiri_frame$/, /^parsed_frame$/]
   config.filter_parameters << lambda do |key, value|
-    value.to_s.gsub!(/pw>.+<\//, 'pw>[FILTERED]</') if key =~ /^(frame|raw_frame)$/i
+    if key == 'raw_frame'
+      value.to_s.gsub!(/pw>.+<\//, 'pw>[FILTERED]</')
+      value.to_s.gsub!(/<eis:legalDocument([^>]+)>([^<])+<\/eis:legalDocument>/,
+                       "<eis:legalDocument>[FILTERED]</eis:legalDocument>")
+    end
   end
 end
