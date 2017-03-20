c55b4698d9349ad1fe624b705d8777843ad88677
Artur Beljajev <artur.beljajev@internet.ee>
2017-03-20 03:14:12 +0200

    Remove whois from BlockedDomain and ReservedDomain
    #269

--- app/models/reserved_domain.rb
+++ app/models/reserved_domain.rb
@@ -1,13 +1,8 @@
 class ReservedDomain < ActiveRecord::Base
   include Versions # version/reserved_domain_version.rb
   before_save :fill_empty_passwords
-  before_save :generate_data
-  after_destroy :remove_data
 
   validates :name, domain_name: true, uniqueness: true
-
-
-
 
   class << self
     def pw_for(domain_name)
@@ -28,8 +23,6 @@ class ReservedDomain < ActiveRecord::Base
     end
   end
 
-
-
   def name= val
     super SimpleIDN.to_unicode(val)
   end
@@ -40,33 +33,6 @@ class ReservedDomain < ActiveRecord::Base
 
   def regenerate_password
     self.password = SecureRandom.hex
-  end
-
-  def generate_data
-    return if Domain.where(name: name).any?
-
-    wr = Whois::Record.find_or_initialize_by(name: name)
-    wr.json = @json = generate_json # we need @json to bind to class
-    wr.body = generate_body
-    wr.save
-  end
-  alias_method :update_whois_record, :generate_data
-
-
-  def generate_body
-    template = Rails.root.join("app/views/for_models/whois_other.erb".freeze)
-    ERB.new(template.read, nil, "-").result(binding)
-  end
-
-  def generate_json
-    h = HashWithIndifferentAccess.new
-    h[:name]       = self.name
-    h[:status]     = ['Reserved']
-    h
-  end
-
-  def remove_data
-    UpdateWhoisRecordJob.enqueue name, 'reserved'
   end
 
   def updatable?
