6f91b99b43b2b89d66460ca2118fb673197903f9
Artur Beljajev <artur.beljajev@internet.ee>
2017-03-20 02:57:22 +0200

    Remove WhoisRecord model
    #269

--- db/structure.sql
+++ db/structure.sql
@@ -2967,41 +2967,6 @@ ALTER SEQUENCE white_ips_id_seq OWNED BY white_ips.id;
 
 
 --
--- Name: whois_records; Type: TABLE; Schema: public; Owner: -; Tablespace: 
---
-
-CREATE TABLE whois_records (
-    id integer NOT NULL,
-    domain_id integer,
-    name character varying,
-    body text,
-    json json,
-    created_at timestamp without time zone NOT NULL,
-    updated_at timestamp without time zone NOT NULL,
-    registrar_id integer
-);
-
-
---
--- Name: whois_records_id_seq; Type: SEQUENCE; Schema: public; Owner: -
---
-
-CREATE SEQUENCE whois_records_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
---
--- Name: whois_records_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
---
-
-ALTER SEQUENCE whois_records_id_seq OWNED BY whois_records.id;
-
-
---
 -- Name: zonefile_settings; Type: TABLE; Schema: public; Owner: -; Tablespace: 
 --
 
@@ -3518,13 +3483,6 @@ ALTER TABLE ONLY versions ALTER COLUMN id SET DEFAULT nextval('versions_id_seq':
 --
 
 ALTER TABLE ONLY white_ips ALTER COLUMN id SET DEFAULT nextval('white_ips_id_seq'::regclass);
-
-
---
--- Name: id; Type: DEFAULT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY whois_records ALTER COLUMN id SET DEFAULT nextval('whois_records_id_seq'::regclass);
 
 
 --
@@ -4076,14 +4034,6 @@ ALTER TABLE ONLY versions
 
 ALTER TABLE ONLY white_ips
     ADD CONSTRAINT white_ips_pkey PRIMARY KEY (id);
-
-
---
--- Name: whois_records_pkey; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
---
-
-ALTER TABLE ONLY whois_records
-    ADD CONSTRAINT whois_records_pkey PRIMARY KEY (id);
 
 
 --
@@ -4823,20 +4773,6 @@ CREATE INDEX index_users_on_registrar_id ON users USING btree (registrar_id);
 
 
 --
--- Name: index_whois_records_on_domain_id; Type: INDEX; Schema: public; Owner: -; Tablespace: 
---
-
-CREATE INDEX index_whois_records_on_domain_id ON whois_records USING btree (domain_id);
-
-
---
--- Name: index_whois_records_on_registrar_id; Type: INDEX; Schema: public; Owner: -; Tablespace: 
---
-
-CREATE INDEX index_whois_records_on_registrar_id ON whois_records USING btree (registrar_id);
-
-
---
 -- Name: log_contacts_object_legacy_id; Type: INDEX; Schema: public; Owner: -; Tablespace: 
 --
 
@@ -5353,4 +5289,6 @@ INSERT INTO schema_migrations (version) VALUES ('20170212020841');
 INSERT INTO schema_migrations (version) VALUES ('20170212021349');
 
 INSERT INTO schema_migrations (version) VALUES ('20170221115548');
+
+INSERT INTO schema_migrations (version) VALUES ('20170320003225');
 
