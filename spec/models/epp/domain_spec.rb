6aee4885fb93dbb1a71433bc42960d9474b6993d
Artur Beljajev <artur.beljajev@internet.ee>
2017-03-22 13:37:01 +0200

    Do not use WhoisRecord model in EPP::Domain
    #269

--- spec/models/epp/domain_spec.rb
+++ spec/models/epp/domain_spec.rb
@@ -39,4 +39,20 @@ RSpec.describe Epp::Domain, db: false do
       end
     end
   end
+
+  describe '#apply_pending_update!' do
+    let(:domain) { described_class.new(name: 'test.com') }
+
+    before :example do
+      allow(ApiUser).to receive(:find).and_return(instance_spy(ApiUser, registrar: nil))
+      allow(domain).to receive(:update).and_return(true)
+      allow(domain).to receive(:clean_pendings!)
+      allow(domain).to receive(:save!)
+    end
+
+    it 'updates whois' do
+      expect(DNS::DomainName).to receive(:update_whois).with(domain_name: 'test.com')
+      domain.apply_pending_update!
+    end
+  end
 end
