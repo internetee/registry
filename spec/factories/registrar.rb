7fa97ed736f77faf526115877f8f903c9c41630b
Artur Beljajev <artur.beljajev@internet.ee>
2017-04-16 17:14:52 +0300

    Add registrar_with_zero_balance factory

--- spec/factories/registrar.rb
+++ spec/factories/registrar.rb
@@ -15,5 +15,11 @@ FactoryGirl.define do
         create(:account, registrar: registrar, balance: 1_000_000)
       end
     end
+
+    factory :registrar_with_zero_balance do
+      after :create do |registrar|
+        create(:account, registrar: registrar, balance: 0)
+      end
+    end
   end
 end
