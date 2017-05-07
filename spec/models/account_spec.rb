f55169c2b4a206b6de186655045007d17a0525e6
Artur Beljajev <artur.beljajev@internet.ee>
2017-05-07 14:05:32 +0300

    Improve "account_activities" DB table structure
    #475

--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -1,48 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Account do
-  context 'with invalid attribute' do
-    before :all do
-      @account = Account.new
-    end
-
-    it 'should not be valid' do
-      @account.valid?
-      @account.errors.full_messages.should match_array(["Account type is missing"])
-    end
-
-    it 'should not have any versions' do
-      @account.versions.should == []
-    end
-  end
-
-  context 'with valid attributes' do
-    before :all do
-      @account = Fabricate(:account)
-    end
-
-    it 'should be valid' do
-      @account.valid?
-      @account.errors.full_messages.should match_array([])
-      s = 0.0
-      @account.activities.map { |x| s += x.sum }
-      @account.balance.should == s
-    end
-
-    it 'should be valid twice' do
-      @account = Fabricate(:account)
-      @account.valid?
-      @account.errors.full_messages.should match_array([])
-    end
-
-    it 'should have one version' do
-      with_versioning do
-        @account.versions.should == []
-        @account.account_type = 'new_type'
-        @account.save
-        @account.errors.full_messages.should match_array([])
-        @account.versions.size.should == 1
-      end
+  it 'has versions' do
+    with_versioning do
+      price = build(:account)
+      price.save!
+      expect(price.versions.size).to be(1)
     end
   end
 
