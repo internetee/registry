3038d240f7e5b869f7f5e2224bf62589eeec5208
Artur Beljajev <artur.beljajev@internet.ee>
2017-08-18 13:04:24 +0300

    Add EPP response spec matcher

---
+++ spec/models/epp/response/result_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe EPP::Response::Result, db: false do
+  # https://tools.ietf.org/html/rfc5730#section-3
+  describe '::codes' do
+    it 'returns codes' do
+      codes = {
+        '1000' => :success,
+        '1001' => :success_pending,
+        '1300' => :success_empty_queue,
+        '1301' => :success_dequeue,
+        '2001' => :syntax_error,
+        '2003' => :required_param_missing,
+        '2005' => :param_syntax_error,
+        '2308' => :data_management_policy_violation,
+      }
+
+      expect(described_class.codes).to eq(codes)
+    end
+  end
+end
--- spec/support/requests/epp_helpers.rb
+++ spec/support/requests/epp_helpers.rb
@@ -7,5 +7,9 @@ module Requests
     def valid_legal_document
       Base64.encode64('a' * 5000)
     end
+
+    def epp_response
+      EPP::Response.from_xml(response.body)
+    end
   end
 end
