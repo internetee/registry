b632dbc2de743dcfb6c52532ca6b885dda642df2
Artur Beljajev <artur.beljajev@internet.ee>
2017-08-23 01:24:01 +0300

    Move setting types from controller to model

--- app/controllers/admin/settings_controller.rb
+++ app/controllers/admin/settings_controller.rb
@@ -26,48 +26,11 @@ module Admin
     def casted_settings
       settings = {}
 
-      ints = [
-        :admin_contacts_min_count,
-        :admin_contacts_max_count,
-        :tech_contacts_min_count,
-        :tech_contacts_max_count,
-        :orphans_contacts_in_months,
-        :ds_digest_type,
-        :dnskeys_min_count,
-        :dnskeys_max_count,
-        :ns_min_count,
-        :ns_max_count,
-        :transfer_wait_time,
-        :invoice_number_min,
-        :invoice_number_max,
-        :days_to_keep_business_registry_cache,
-        :days_to_keep_invoices_active,
-        :days_to_keep_overdue_invoices_active,
-        :days_to_renew_domain_before_expire,
-        :expire_warning_period,
-        :redemption_grace_period,
-        :expire_pending_confirmation
-      ]
-
-      floats = [:registry_vat_prc, :minimum_deposit]
-
-      booleans = [
-        :ds_data_allowed,
-        :key_data_allowed,
-        :client_side_status_editing_enabled,
-        :registrar_ip_whitelist_enabled,
-        :api_ip_whitelist_enabled,
-        :request_confrimation_on_registrant_change_enabled,
-        :request_confirmation_on_domain_deletion_enabled,
-        :nameserver_required,
-        :address_processing
-      ]
-
       params[:settings].each do |k, v|
         settings[k] = v
-        settings[k] = v.to_i if ints.include?(k.to_sym)
-        settings[k] = v.to_f if floats.include?(k.to_sym)
-        settings[k] = (v == 'true' ? true : false) if booleans.include?(k.to_sym)
+        settings[k] = v.to_i if Setting.integer_settings.include?(k.to_sym)
+        settings[k] = v.to_f if Setting.float_settings.include?(k.to_sym)
+        settings[k] = (v == 'true' ? true : false) if Setting.boolean_settings.include?(k.to_sym)
       end
 
       settings
--- app/models/setting.rb
+++ app/models/setting.rb
@@ -20,4 +20,50 @@ class Setting < RailsSettings::CachedSettings
 
     return errors
   end
+
+  def self.integer_settings
+    %i[
+      admin_contacts_min_count
+      admin_contacts_max_count
+      tech_contacts_min_count
+      tech_contacts_max_count
+      orphans_contacts_in_months
+      ds_digest_type
+      dnskeys_min_count
+      dnskeys_max_count
+      ns_min_count
+      ns_max_count
+      transfer_wait_time
+      invoice_number_min
+      invoice_number_max
+      days_to_keep_business_registry_cache
+      days_to_keep_invoices_active
+      days_to_keep_overdue_invoices_active
+      days_to_renew_domain_before_expire
+      expire_warning_period
+      redemption_grace_period
+      expire_pending_confirmation
+    ]
+  end
+
+  def self.float_settings
+    %i[
+      registry_vat_prc
+      minimum_deposit
+    ]
+  end
+
+  def self.boolean_settings
+    %i[
+      ds_data_allowed
+      key_data_allowed
+      client_side_status_editing_enabled
+      registrar_ip_whitelist_enabled
+      api_ip_whitelist_enabled
+      request_confrimation_on_registrant_change_enabled
+      request_confirmation_on_domain_deletion_enabled
+      nameserver_required
+      address_processing
+    ]
+  end
 end
