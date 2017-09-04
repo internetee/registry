3de5a0b51bc85d02ae9a07c9df66a1fbca61eace
Artur Beljajev <artur.beljajev@internet.ee>
2017-09-04 09:02:37 +0300

    Fix code climate offences
    #569

--- lib/validators/e164_validator.rb
+++ lib/validators/e164_validator.rb
@@ -1,10 +1,12 @@
 class E164Validator < ActiveModel::EachValidator
   def validate_each(record, attribute, _value)
-    validator = ActiveModel::Validations::LengthValidator.new(maximum: 17, attributes: attribute)
-    validator.validate(record)
+    length_validator = ActiveModel::Validations::
+            LengthValidator.new(maximum: 17, attributes: attribute)
+    length_validator.validate(record)
 
-    validator = ActiveModel::Validations::FormatValidator.new(with: /\+[0-9]{1,3}\.[0-9]{1,14}?/,
-                                                              attributes: attribute)
-    validator.validate(record)
+    format_validator = ActiveModel::Validations::
+            FormatValidator.new(with: /\+[0-9]{1,3}\.[0-9]{1,14}?/,
+                                attributes: attribute)
+    format_validator.validate(record)
   end
 end
