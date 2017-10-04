e6f6d24169e5cf3ac5bf525c7ec47055f90a2089
Artur Beljajev <artur.beljajev@internet.ee>
2017-10-04 20:39:40 +0300

    Improve test readability
    #600

--- spec/features/registrar/user_switch_spec.rb
+++ spec/features/registrar/user_switch_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.feature 'Registrar area current user switch', settings: false do
+RSpec.feature 'Registrar area user switch', settings: false do
   given!(:current_user) { create(:api_user, id: 1, identity_code: 'test') }
   given!(:new_user) { create(:api_user, id: 2, identity_code: 'test', username: 'new-user-name') }
 
@@ -8,7 +8,7 @@ RSpec.feature 'Registrar area current user switch', settings: false do
     sign_in_to_registrar_area(user: current_user)
   end
 
-  it 'switches current user' do
+  scenario 'successful user switch' do
     visit registrar_root_path
     click_link_or_button 'switch-current-user-2-btn'
     expect(page).to have_text('You are now signed in as a user "new-user-name"')
