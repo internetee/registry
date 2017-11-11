2f2ce97a768a70e3098feecb9b4efb7885686639
Artur Beljajev <artur.beljajev@internet.ee>
2017-11-12 00:54:51 +0200

    Include minitest/mock in test helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,7 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require 'minitest/mock'
 require 'capybara/rails'
 require 'capybara/minitest'
 
