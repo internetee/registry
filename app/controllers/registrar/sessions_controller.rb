3f1c36bd9a7416b77d3b04a9547929a1e608d23a
Georg Kahest <georg@tld.ee>
2017-11-13 18:43:37 +0200

    fix some of the style bugs

--- app/controllers/registrar/sessions_controller.rb
+++ app/controllers/registrar/sessions_controller.rb
@@ -53,7 +53,7 @@ class Registrar
     end
 
     def id
-      @user = ApiUser.find_by_idc_data_and_allowed(request.env['SSL_CLIENT_S_DN'],request.ip)
+      @user = ApiUser.find_by_idc_data_and_allowed(request.env['SSL_CLIENT_S_DN'], request.ip)
 
       if @user
         sign_in(@user, event: :authentication)
@@ -156,11 +156,11 @@ class Registrar
     def find_user_by_idc_and_allowed(idc)
       return User.new unless idc
       possible_users = ApiUser.where(identity_code: idc) || User.new
-	for i in 0..possible_users.count
-		if possible_users[i].registrar.white_ips.registrar_area.include_ip?(request.ip)
-		    break
-		end
-	end
+	  for i in 0..possible_users.count
+	    if possible_users[i].registrar.white_ips.registrar_area.include_ip?(request.ip)
+          break
+	    end
+      end
       possible_users[i]
     end
 
