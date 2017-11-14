c1b1ab41b14bc634569f59798434a8ebe1728a6e
Georg Kahest <georg@tld.ee>
2017-11-14 12:32:33 +0200

    fix indention

--- app/models/api_user.rb
+++ app/models/api_user.rb
@@ -51,19 +51,19 @@ class ApiUser < User
 
       find_by(identity_code: identity_code)
     end
-    
+
     def find_by_idc_data_and_allowed(idc_data, ip)
       return false if idc_data.blank?
       identity_code = idc_data.scan(/serialNumber=(\d+)/).flatten.first
 
       return false if ip.blank?
       possible_users = where(identity_code: identity_code)
-        for i in 0..possible_users.count
-          if possible_users[i].registrar.white_ips.registrar_area.include_ip?(ip)
-             break
-          end
+      for selected_user in 0..possible_users.count
+        if possible_users[selected_user].registrar.white_ips.registrar_area.include_ip?(ip)
+          break
         end
-      possible_users[i]
+      end
+      possible_users[selected_user]
     end
 
   end
