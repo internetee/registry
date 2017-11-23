5f69067d35cd868ccd3010642d42dbb596bb14bd
Artur Beljajev <artur.beljajev@internet.ee>
2017-11-23 10:17:59 +0200

    Extract available languages
    #640

--- app/helpers/form_helper.rb
+++ app/helpers/form_helper.rb
@@ -14,7 +14,7 @@ module FormHelper
   end
 
   def language_select(object, method, choices = nil, options = {}, html_options = {}, &block)
-    choices = options_for_select({ en: 'English', et: 'Estonian' }.invert, options[:selected])
+    choices = options_for_select(available_languages, options[:selected])
     select(object, method, choices, options, html_options, &block)
   end
 end
