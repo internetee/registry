fc134ee7fbc070e2cf38462ab7cbe941bc1cd997
Artur Beljajev <artur.beljajev@internet.ee>
2017-11-23 07:05:59 +0200

    Add language_select form field
    #640

--- app/helpers/default_form_builder.rb
+++ app/helpers/default_form_builder.rb
@@ -7,4 +7,11 @@ class DefaultFormBuilder < ActionView::Helpers::FormBuilder
   def money_field(method, options = {})
     @template.money_field(@object_name, method, objectify_options(options))
   end
+
+  def language_select(method, choices = nil, options = {}, html_options = {}, &block)
+    options[:selected] = @object.send(method) unless options[:selected]
+    @template.language_select(@object_name, method, choices, objectify_options(options),
+                              @default_options.merge(html_options),
+                              &block)
+  end
 end
