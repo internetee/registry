78f94968330916fb2320215b71d812544a70800a
Artur Beljajev <artur.beljajev@internet.ee>
2017-12-08 09:00:54 +0200

    Revert bdb1090a405f73d94c5b7441343684f0f47c1373
    #343

--- db/structure.sql
+++ db/structure.sql
@@ -2477,6 +2477,7 @@ CREATE TABLE registrars (
     vat boolean,
     legacy_id integer,
     reference_no character varying,
+    exclude_in_monthly_directo boolean DEFAULT false,
     test_registrar boolean DEFAULT false,
     language character varying NOT NULL
 );
@@ -5051,8 +5052,6 @@ INSERT INTO schema_migrations (version) VALUES ('20170606202859');
 INSERT INTO schema_migrations (version) VALUES ('20171009080822');
 
 INSERT INTO schema_migrations (version) VALUES ('20171009082321');
-
-INSERT INTO schema_migrations (version) VALUES ('20171025110933');
 
 INSERT INTO schema_migrations (version) VALUES ('20171025113808');
 
