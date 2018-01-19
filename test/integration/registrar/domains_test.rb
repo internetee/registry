0fa0c698773151bd420c481012472ab7d5b06de4
Artur Beljajev <artur.beljajev@internet.ee>
2018-01-19 12:12:07 +0200

    Convert specs to tests

---
+++ test/integration/registrar/domains_test.rb
@@ -0,0 +1,20 @@
+require 'test_helper'
+
+class DomainsTest < ActionDispatch::IntegrationTest
+  def setup
+    login_as users(:api)
+  end
+
+  def test_downloads_domain_list_as_csv
+    expected_csv = <<-CSV.strip_heredoc
+      Domain,Auth info,Registrant name,Registrant code,Date of expiry
+      library.test,45118f5,Acme Ltd,acme-ltd-001,2010-07-05
+      shop.test,65078d5,John,john-001,2010-07-05
+      airport.test,55438j5,John,john-001,2010-07-05
+    CSV
+
+    visit registrar_domains_url
+    click_button 'Download as CSV'
+    assert_equal expected_csv, page.body
+  end
+end
