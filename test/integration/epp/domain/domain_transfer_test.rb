da07639457f5cfdd98e393d94bb3c1a153ad7337
Artur Beljajev <artur.beljajev@internet.ee>
2018-01-25 14:34:17 +0200

    Update EPP domain tests
    #660

--- test/integration/epp/domain/domain_transfer_test.rb
+++ test/integration/epp/domain/domain_transfer_test.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
 
-class Epp::DomainTransferTest < ActionDispatch::IntegrationTest
+class EppDomainTransferTest < ActionDispatch::IntegrationTest
+  def setup
+    login_as users(:api_goodnames)
+  end
+
   def test_transfers_domain
     request_xml = <<-XML
       <?xml version="1.0" encoding="UTF-8" standalone="no"?>
@@ -20,7 +24,7 @@ class Epp::DomainTransferTest < ActionDispatch::IntegrationTest
 
     session_id = epp_sessions(:api_goodnames).session_id
     post '/epp/command/transfer', { frame: request_xml }, { 'HTTP_COOKIE' => "session=#{session_id}" }
-    assert_response :ok
+    assert_response 200
     assert_equal registrars(:goodnames), domains(:shop).registrar
   end
 end
---
+++ test/integration/epp/domain/domain_update_test.rb
@@ -0,0 +1,32 @@
+require 'test_helper'
+
+class EppDomainUpdateTest < ActionDispatch::IntegrationTest
+  def setup
+    login_as users(:api_bestnames)
+  end
+
+  def test_updates_transfer_code
+    request_xml = <<-XML
+      <?xml version="1.0" encoding="UTF-8" standalone="no"?>
+      <epp xmlns="https://epp.tld.ee/schema/epp-ee-1.0.xsd">
+        <command>
+          <update>
+            <domain:update xmlns:domain="https://epp.tld.ee/schema/domain-eis-1.0.xsd">
+              <domain:name>shop.test</domain:name>
+                <domain:chg>
+                  <domain:authInfo>
+                    <domain:pw>f0ff7d17b0</domain:pw>
+                  </domain:authInfo>
+                </domain:chg>
+            </domain:update>
+          </update>
+        </command>
+      </epp>
+    XML
+
+    session_id = epp_sessions(:api_bestnames).session_id
+    post '/epp/command/update', { frame: request_xml }, { 'HTTP_COOKIE' => "session=#{session_id}" }
+    assert_response 200
+    assert_equal 'f0ff7d17b0', domains(:shop).transfer_code
+  end
+end
