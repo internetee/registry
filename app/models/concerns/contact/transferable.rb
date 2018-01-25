f99b8b66b07cc899a98c9e4ecb78f1c69fb7de95
Artur Beljajev <artur.beljajev@internet.ee>
2018-01-25 12:40:17 +0200

    Regenerate `auth_info` on contact transfer
    #660

--- app/models/concerns/contact/transferable.rb
+++ app/models/concerns/contact/transferable.rb
@@ -10,13 +10,17 @@ module Concerns::Contact::Transferable
     new_contact.registrar = new_registrar
     new_contact.generate_code
     new_contact.original = self
+    new_contact.regenerate_auth_info
     new_contact.remove_address unless self.class.address_processing?
     new_contact.save!
     new_contact
   end
 
+  protected
+
   def generate_auth_info
-    return if auth_info.present?
     self.auth_info = SecureRandom.hex(11)
   end
+
+  alias_method :regenerate_auth_info, :generate_auth_info
 end
