c5f8c7e1e8c7e2798380833b66a05737fba7bdcb
Artur Beljajev <artur.beljajev@internet.ee>
2018-02-07 09:15:15 +0200

    Rename migration
    #700

--- db/migrate/20180206234620_add_epp_sessions_user_id.rb
+++ db/migrate/20180206234620_add_epp_sessions_user_id.rb
@@ -1,4 +1,4 @@
-class ExtractUserIdFromEppSessionsData < ActiveRecord::Migration
+class AddEppSessionsUserId < ActiveRecord::Migration
   def change
     add_reference :epp_sessions, :user, foreign_key: true
   end
