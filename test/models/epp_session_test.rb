40c12380ee5e6cc9ed7928395e2b173f1e320ed4
Artur Beljajev <artur.beljajev@internet.ee>
2018-02-13 19:52:34 +0200

    Improve readability
    #700

--- test/models/epp_session_test.rb
+++ test/models/epp_session_test.rb
@@ -9,13 +9,13 @@ class EppSessionTest < ActiveSupport::TestCase
     assert @epp_session.valid?
   end
 
-  def test_session_id_presence_validation
+  def test_invalid_without_session_id
     @epp_session.session_id = nil
     @epp_session.validate
     assert @epp_session.invalid?
   end
 
-  def test_user_presence_validation
+  def test_invalid_without_user
     @epp_session.user = nil
     @epp_session.validate
     assert @epp_session.invalid?
