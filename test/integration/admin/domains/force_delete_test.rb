b42d06024eadf4c82f20ab895ac54e381a1c9eaa
Artur Beljajev <artur.beljajev@internet.ee>
2018-02-20 03:49:53 +0200

    Convert spec to test
    #727

---
+++ test/integration/admin/domains/force_delete_test.rb
@@ -0,0 +1,32 @@
+require 'test_helper'
+
+class AdminAreaDomainForceDeleteTest < ActionDispatch::IntegrationTest
+  def setup
+    login_as users(:admin)
+    @domain = domains(:shop)
+  end
+
+  def test_schedules_domain_force_delete
+    refute @domain.force_delete_scheduled?
+
+    visit edit_admin_domain_url(@domain)
+    click_link_or_button 'Force delete domain'
+    @domain.reload
+
+    assert @domain.force_delete_scheduled?
+    assert_equal 1, ActionMailer::Base.deliveries.size
+    assert_text 'Force delete procedure has been scheduled'
+  end
+
+  def test_cancels_scheduled_domain_force_delete
+    @domain.update_attribute(:statuses, [DomainStatus::FORCE_DELETE])
+    assert @domain.force_delete_scheduled?
+
+    visit edit_admin_domain_url(@domain)
+    click_link_or_button 'Cancel force delete'
+    @domain.reload
+
+    refute @domain.force_delete_scheduled?
+    assert_text 'Force delete procedure has been cancelled'
+  end
+end
