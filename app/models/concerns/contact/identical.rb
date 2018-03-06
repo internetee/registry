ecc68f083d43d7e296897cd3e49ecb552d92ab6c
Artur Beljajev <artur.beljajev@internet.ee>
2018-03-06 10:39:32 +0200

    Enhance identical contact lookup
    #746

--- app/models/concerns/contact/identical.rb
+++ app/models/concerns/contact/identical.rb
@@ -1,18 +1,34 @@
 module Concerns::Contact::Identical
   extend ActiveSupport::Concern
 
-  ATTRIBUTE_FILTER = %w[
+  IDENTIFIABLE_ATTRIBUTES = %w[
     name
+    email
+    phone
+    fax
     ident
     ident_type
     ident_country_code
-    phone
-    email
+    org_name
   ]
-  private_constant :ATTRIBUTE_FILTER
+  private_constant :IDENTIFIABLE_ATTRIBUTES
 
   def identical(registrar)
-    self.class.where(attributes.slice(*ATTRIBUTE_FILTER)).where(registrar: registrar)
+    self.class.where(identifiable_hash)
+      .where(["statuses = ?::character varying[]", "{#{read_attribute(:statuses).join(',')}}"])
+      .where(registrar: registrar)
       .where.not(id: id).take
+  end
+
+  private
+
+  def identifiable_hash
+    attributes = IDENTIFIABLE_ATTRIBUTES
+
+    if self.class.address_processing?
+      attributes += self.class.address_attribute_names
+    end
+
+    slice(*attributes)
   end
 end
--- test/models/contact/identical_test.rb
+++ test/models/contact/identical_test.rb
@@ -1,29 +1,56 @@
 require 'test_helper'
 
 class ContactIdenticalTest < ActiveSupport::TestCase
+  REGULAR_FILTER_ATTRIBUTES = %i[
+    name
+    email
+    phone
+    fax
+    ident
+    ident_type
+    ident_country_code
+    org_name
+  ]
+
   def setup
     @contact = contacts(:william)
     @identical = contacts(:identical_to_william)
   end
 
-  def test_identical
+  def test_returns_identical
     assert_equal @identical, @contact.identical(@identical.registrar)
   end
 
-  def test_not_identical
-    filter_attributes = %i[
-      name
-      ident
-      ident_type
-      ident_country_code
-      phone
-      email
-    ]
-
-    filter_attributes.each do |attribute|
+  def test_does_not_return_non_identical
+    REGULAR_FILTER_ATTRIBUTES.each do |attribute|
       previous_value = @identical.public_send(attribute)
       @identical.update_attribute(attribute, 'other')
       assert_nil @contact.identical(@identical.registrar)
+      @identical.update_attribute(attribute, previous_value)
+    end
+
+    @identical.update!({ statuses: %w[ok linked] })
+    assert_nil @contact.identical(@identical.registrar)
+  end
+
+  def test_takes_address_into_account_when_processing_enabled
+    Setting.address_processing = true
+
+    Contact.address_attribute_names.each do |attribute|
+      previous_value = @identical.public_send(attribute)
+      @identical.update_attribute(attribute, 'other')
+      assert_nil @contact.identical(@identical.registrar)
+      @identical.update_attribute(attribute, previous_value)
+    end
+  end
+
+  def test_ignores_address_when_processing_disabled
+    Setting.address_processing = false
+
+    Contact.address_attribute_names.each do |attribute|
+      previous_value = @identical.public_send(attribute)
+      @identical.update_attribute(attribute, 'other')
+      assert_equal @identical, @contact.identical(@identical.registrar)
       @identical.update_attribute(attribute, previous_value)
     end
   end
