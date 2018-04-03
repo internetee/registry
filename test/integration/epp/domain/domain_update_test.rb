af414c9267b602ee20acb4136ebf34dcbc2ff265
Artur Beljajev <artur.beljajev@internet.ee>
2018-04-03 18:01:56 +0300

    Reorganize tests
    #792

--- test/integration/epp/domain/domain_update_test.rb
+++ test/integration/epp/domain/domain_update_test.rb
@@ -1,6 +1,31 @@
 require 'test_helper'
 
 class EppDomainUpdateTest < ActionDispatch::IntegrationTest
+  def test_update_domain
+    request_xml = <<-XML
+      <?xml version="1.0" encoding="UTF-8" standalone="no"?>
+      <epp xmlns="https://epp.tld.ee/schema/epp-ee-1.0.xsd">
+        <command>
+          <update>
+            <domain:update xmlns:domain="https://epp.tld.ee/schema/domain-eis-1.0.xsd">
+              <domain:name>shop.test</domain:name>
+                <domain:chg>
+                  <domain:authInfo>
+                    <domain:pw>f0ff7d17b0</domain:pw>
+                  </domain:authInfo>
+                </domain:chg>
+            </domain:update>
+          </update>
+        </command>
+      </epp>
+    XML
+
+    post '/epp/command/update', { frame: request_xml }, 'HTTP_COOKIE' => 'session=api_bestnames'
+    assert_equal 'f0ff7d17b0', domains(:shop).transfer_code
+    assert_equal '1000', Nokogiri::XML(response.body).at_css('result')[:code]
+    assert_equal 1, Nokogiri::XML(response.body).css('result').size
+  end
+
   def test_discarded_domain_cannot_be_updated
     domains(:shop).discard
 
