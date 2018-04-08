b4ed8f6c8200fbaa3f5112144a5478c2890e5fbe
Artur Beljajev <artur.beljajev@internet.ee>
2018-04-08 16:01:00 +0300

    Remove double load
    #790

--- app/controllers/admin/domains_controller.rb
+++ app/controllers/admin/domains_controller.rb
@@ -1,7 +1,7 @@
 module Admin
   class DomainsController < BaseController
-    load_and_authorize_resource
-    before_action :set_domain, only: [:show, :edit, :update]
+    before_action :set_domain, only: %i[show edit update]
+    authorize_resource
     helper_method :force_delete_templates
 
     # rubocop: disable Metrics/PerceivedComplexity
