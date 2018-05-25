f1808cfcf1cab4705b87e1ca7b60d12c7880f352
Maciej Szlosarczyk <maciej.szlosarczyk@eestiinternet.ee>
2018-05-25 13:55:21 +0300

    Remove link to WHOIS controller from registrant portal

---
+++ test/integration/registrant/layout_test.rb
@@ -0,0 +1,17 @@
+require 'test_helper'
+
+class RegistrantLayoutTest < ActionDispatch::IntegrationTest
+  def setup
+    super
+    login_as(users(:registrant))
+
+    Setting.days_to_keep_business_registry_cache = 1
+    travel_to Time.zone.parse('2010-07-05')
+  end
+
+  def test_has_link_to_rest_whois
+    visit registrant_domains_url
+    assert(has_link?('Internet.ee', href: 'https://internet.ee'))
+    refute(has_link?('WHOIS', href: registrant_whois_path))
+  end
+end
