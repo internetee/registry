3400a1bd3edecf657344cace670c371414c78bb7
Artur Beljajev <artur.beljajev@internet.ee>
2018-06-06 03:20:25 +0300

    Fix code style issues from CodeClimate

--- lib/gem_ext/builder.rb
+++ lib/gem_ext/builder.rb
@@ -1,6 +1,6 @@
 class Builder::XmlMarkup
   def epp_head
-    self.instruct!
+    instruct!
     epp(
       'xmlns' => 'https://epp.tld.ee/schema/epp-ee-1.0.xsd',
       'xmlns:xsi' => 'http://www.w3.org/2001/XMLSchema-instance',
