183eca884e5010233b9472d63c0809182d7af486
Artur Beljajev <artur.beljajev@internet.ee>
2018-06-13 08:03:19 +0300

    Add `contacts.uuid` and `domains.uuid` uniq constraints

---
+++ db/migrate/20180613045614_add_contacts_and_domains_uuid_uniq_constraint.rb
@@ -0,0 +1,15 @@
+class AddContactsAndDomainsUuidUniqConstraint < ActiveRecord::Migration
+  def up
+    execute <<-SQL
+      ALTER TABLE contacts ADD CONSTRAINT uniq_contact_uuid UNIQUE (uuid);
+      ALTER TABLE domains ADD CONSTRAINT uniq_domain_uuid UNIQUE (uuid);
+    SQL
+  end
+
+  def down
+    execute <<-SQL
+      ALTER TABLE contacts DROP CONSTRAINT uniq_contact_uuid;
+      ALTER TABLE domains DROP CONSTRAINT uniq_domain_uuid;
+    SQL
+  end
+end
