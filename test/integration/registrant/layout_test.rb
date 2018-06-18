843dc8edcbaabae87dd040f6715352ca3ee92f21
Maciej Szlosarczyk <maciej.szlosarczyk@eestiinternet.ee>
2018-06-18 10:30:23 +0300

    Add new links to registrant page layout

--- test/integration/registrant/layout_test.rb
+++ test/integration/registrant/layout_test.rb
@@ -9,9 +9,10 @@ class RegistrantLayoutTest < ActionDispatch::IntegrationTest
     travel_to Time.zone.parse('2010-07-05')
   end
 
-  def test_has_link_to_rest_whois
+  def test_has_link_to_rest_whois_and_internet_ee
     visit registrant_domains_url
+
     assert(has_link?('Internet.ee', href: 'https://internet.ee'))
-    refute(has_link?('WHOIS', href: 'registrant/whois'))
+    assert(has_link?('WHOIS', href: 'https://whois.internet.ee'))
   end
 end
