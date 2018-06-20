fa9731be16d01ba8afc32c196047ca731e81b666
Artur Beljajev <artur.beljajev@internet.ee>
2018-06-20 12:21:44 +0300

    Remove redirection

--- app/controllers/registrar/dashboard_controller.rb
+++ app/controllers/registrar/dashboard_controller.rb
@@ -3,11 +3,6 @@ class Registrar
     authorize_resource class: false
 
     def show
-      if can?(:show, :poll)
-        redirect_to registrar_poll_url and return
-      elsif can?(:show, Invoice)
-        redirect_to registrar_invoices_url and return
-      end
     end
   end
 end
