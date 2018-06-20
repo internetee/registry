1091fe710967b59c8fceb2d0bb26ff6da01d9043
Artur Beljajev <artur.beljajev@internet.ee>
2018-06-20 14:44:34 +0300

    Change date format

--- app/models/directo.rb
+++ app/models/directo.rb
@@ -26,7 +26,7 @@ class Directo < ActiveRecord::Base
             xml.invoice(
                 "SalesAgent"  => Setting.directo_sales_agent,
                 "Number"      => num,
-                "InvoiceDate" => invoice.created_at.strftime("%Y-%m-%dT%H:%M:%S"),
+                "InvoiceDate" => invoice.created_at.strftime("%Y-%m-%d"),
                 "PaymentTerm" => Setting.directo_receipt_payment_term,
                 "Currency"    => invoice.currency,
                 "CustomerCode"=> invoice.buyer.accounting_customer_code
