b164a585237823164b3d65493a646cbcbfb380ce
Maciej Szlosarczyk <maciej.szlosarczyk@eestiinternet.ee>
2018-06-29 11:25:55 +0300

    Add new base test class definition

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -11,6 +11,7 @@ require 'minitest/mock'
 require 'capybara/rails'
 require 'capybara/minitest'
 require 'webmock/minitest'
+require 'selenium/webdriver'
 require 'support/rails5_assetions' # Remove once upgraded to Rails 5
 
 Setting.address_processing = false
@@ -37,5 +38,30 @@ class ActionDispatch::IntegrationTest
     WebMock.reset!
     Capybara.reset_sessions!
     Capybara.use_default_driver
+  end
+end
+
+class JavascriptIntegrationTest < ActionDispatch::IntegrationTest
+  Capybara.register_driver(:chrome) do |app|
+    options = ::Selenium::WebDriver::Chrome::Options.new
+
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--window-size=1400,1400")
+
+    Capybara::Selenium::Driver.new(Rails.application, browser: :chrome, options: options)
+  end
+
+  Capybara.register_server(:silent_puma) do |app, port, _host|
+    require "rack/handler/puma"
+    Rack::Handler::Puma.run(app, Port: port, Threads: "0:2", Silent: true)
+  end
+
+  def setup
+    super
+
+    Capybara.current_driver = :chrome
+    Capybara.server = :silent_puma
   end
 end
