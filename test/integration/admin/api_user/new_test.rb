02aad0d7813e43bcc5e56278acf37811cbca728f
Artur Beljajev <artur.beljajev@internet.ee>
2018-07-15 19:01:43 +0300

    Add tests

---
+++ test/integration/admin/api_user/new_test.rb
@@ -0,0 +1,25 @@
+require 'test_helper'
+
+class AdminAreaNewApiUserTest < ActionDispatch::IntegrationTest
+  setup do
+    sign_in users(:admin)
+  end
+
+  def test_new_api_user_creation_with_required_params
+    visit admin_api_users_url
+    click_link_or_button 'New API user'
+
+    fill_in 'Username', with: 'newtest'
+    fill_in 'Password', with: 'testtest'
+    find('#api_user_registrar_id', visible: false).set(registrars(:bestnames).id)
+
+    assert_difference 'ApiUser.count' do
+      click_link_or_button 'Save'
+    end
+
+    assert_current_path admin_api_user_path(ApiUser.last)
+    assert_text 'Record created'
+    assert_text 'Username newtest'
+    assert_text 'Password testtest'
+  end
+end
