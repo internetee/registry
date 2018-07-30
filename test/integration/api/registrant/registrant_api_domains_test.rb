13562aeb0687040b3464718e7f70b7bfb8e36ee6
Maciej Szlosarczyk <maciej.szlosarczyk@eestiinternet.ee>
2018-07-30 13:56:54 +0300

    Move file to another folder

--- test/integration/api/registrant/registrant_api_domains_test.rb
+++ test/integration/api/registrant/registrant_api_domains_test.rb
@@ -1,6 +1,7 @@
 require 'test_helper'
+require 'auth_token/auth_token_creator'
 
-class RegistrantApiDomainsTest < ApplicationSystemTestCase
+class RegistrantApiDomainsTest < ActionDispatch::IntegrationTest
   def setup
     super
 
@@ -28,7 +29,24 @@ class RegistrantApiDomainsTest < ApplicationSystemTestCase
     assert_equal({errors: ['Domain not found']}, response_json)
   end
 
-  def test_get_non_registrar_domain_details_by_uuid
-    # no op
+  def test_root_returns_domain_list
+    get '/api/v1/registrant/domains', {}, @auth_headers
+    assert_equal(200, response.status)
+  end
+
+  def test_root_returns_401_without_authorization
+    get '/api/v1/registrant/domains', {}, {}
+    assert_equal(401, response.status)
+    json_body = JSON.parse(response.body, symbolize_names: true)
+
+    assert_equal({ errors: ['Not authorized'] }, json_body)
+  end
+
+  private
+
+  def auth_token
+    token_creator = AuthTokenCreator.create_with_defaults(@user)
+    hash = token_creator.token_in_hash
+    "Bearer #{hash[:access_token]}"
   end
 end
