f73a2551d3d61d542983be63ec7773999b57899a
Artur Beljajev <artur.beljajev@internet.ee>
2018-08-07 22:53:02 +0300

    Improve readability

--- test/models/contact_test.rb
+++ test/models/contact_test.rb
@@ -14,18 +14,25 @@ class ContactTest < ActiveSupport::TestCase
     assert @contact.valid?
   end
 
-  def test_email_validation
+  def test_invalid_without_email
     @contact.email = ''
+    assert @contact.invalid?
+  end
+
+  def test_email_format_validation
+    @contact.email = 'invalid'
     assert @contact.invalid?
 
     @contact.email = 'test@bestmail.test'
     assert @contact.valid?
   end
 
-  def test_phone_validation
-    @contact.phone = ''
+  def test_invalid_without_phone
+    @contact.email = ''
     assert @contact.invalid?
+  end
 
+  def test_phone_format_validation
     @contact.phone = '+123.'
     assert @contact.invalid?
 
