ba24e42cec399e69bec7b6827496cd856a29ec1c
Artur Beljajev <artur.beljajev@internet.ee>
2018-08-10 13:52:13 +0300

    Remove useless spec

--- spec/features/registrar/home_link_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
-
-RSpec.feature 'Registrar area home link', db: true do
-  scenario 'is visible' do
-    visit new_registrar_user_session_url
-    expect(page).to have_link('registrar-home-btn', href: registrar_root_path)
-  end
-end
