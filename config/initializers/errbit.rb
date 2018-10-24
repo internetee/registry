0ef6bd04753e77e8058954e5709d3c5fd58d8161
Maciej Szlosarczyk <maciej.szlosarczyk@eestiinternet.ee>
2018-10-24 13:43:17 +0300

    Fix airbrake configuration in test

---
+++ config/initializers/errbit.rb
@@ -0,0 +1,23 @@
+module Patches
+  module Airbrake
+    module SyncSender
+      def build_https(uri)
+        super.tap do |req|
+          req.verify_mode = OpenSSL::SSL::VERIFY_NONE
+        end
+      end
+    end
+  end
+end
+
+Airbrake::SyncSender.prepend(::Patches::Airbrake::SyncSender)
+
+Airbrake.configure do |config|
+  config.host = ENV['airbrake_host']
+  config.project_id = ENV['airbrake_project_id']
+  config.project_key = ENV['airbrake_project_key']
+
+  # Uncomment for Rails apps
+   config.environment = Rails.env
+   config.ignore_environments = %w(development test)
+end
