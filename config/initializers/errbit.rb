c86bd96588653a5d2dbc0d77be10350d89961f1d
Maciej Szlosarczyk <maciej.szlosarczyk@eestiinternet.ee>
2018-10-25 10:29:02 +0300

    Make airbrake environment configurable

--- config/initializers/errbit.rb
+++ config/initializers/errbit.rb
@@ -17,6 +17,6 @@ Airbrake.configure do |config|
   config.project_id = ENV['airbrake_project_id']
   config.project_key = ENV['airbrake_project_key']
 
-  config.environment = Rails.env
+  config.environment = ENV['airbrake_env'] || Rails.env
   config.ignore_environments = %w(development test)
 end
