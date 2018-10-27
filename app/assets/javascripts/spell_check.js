02f9e99428f94cbe2168224c6fd24c7faddba846
Artur Beljajev <artur.beljajev@internet.ee>
2018-10-27 16:18:29 +0300

    Support IE11
    Previously `let of` loop was used, which isn't supported in IE11.
    
    It seems it just fails when there is some JS syntax error
    (doesn't matter where exactly) and dataType: 'json' has no effect.
    
    Closes #982

--- app/assets/javascripts/spell_check.js
+++ app/assets/javascripts/spell_check.js
@@ -1,11 +1,13 @@
-(function() {
+(function () {
     function disableSpellCheck() {
         let selector = 'input[type=text], textarea';
         let textFields = document.querySelectorAll(selector);
 
-        for (let field of textFields) {
-            field.spellcheck = false;
-        }
+        textFields.forEach(
+            function (field, _currentIndex, _listObj) {
+                field.spellcheck = false;
+            }
+        );
     }
 
     disableSpellCheck();
