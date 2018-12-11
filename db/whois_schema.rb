f1c02f13f0e3f11fa0214975dd84814962ddfee7
Maciej Szlosarczyk <maciej.szlosarczyk@eestiinternet.ee>
2018-12-11 10:32:55 +0200

    Regenerate whois schema.rb to correspond with what is really there

--- db/whois_schema.rb
+++ db/whois_schema.rb
@@ -11,10 +11,33 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150113113236) do
+ActiveRecord::Schema.define(version: 20181102124618) do
 
   # These are extensions that must be enabled in order to support this database
-  # enable_extension "plpgsql"
+  enable_extension "plpgsql"
+
+  create_table "ar_internal_metadata", primary_key: "key", force: :cascade do |t|
+    t.string   "value"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "contact_requests", id: :bigserial, force: :cascade do |t|
+    t.integer  "whois_record_id",                 null: false
+    t.string   "secret",                          null: false
+    t.string   "email",                           null: false
+    t.string   "name",                            null: false
+    t.datetime "valid_to",                        null: false
+    t.string   "status",          default: "new", null: false
+    t.inet     "ip_address"
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
+  end
+
+  add_index "contact_requests", ["email"], name: "index_contact_requests_on_email", using: :btree
+  add_index "contact_requests", ["ip_address"], name: "index_contact_requests_on_ip_address", using: :btree
+  add_index "contact_requests", ["secret"], name: "index_contact_requests_on_secret", unique: true, using: :btree
+  add_index "contact_requests", ["whois_record_id"], name: "index_contact_requests_on_whois_record_id", using: :btree
 
   create_table "whois_records", force: :cascade do |t|
     t.string   "name"
