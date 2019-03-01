647bf4c3976aa98b8a66a83d07a774bdf0a0c1d5
Artur Beljajev <artur.beljajev@internet.ee>
2019-03-01 19:53:37 +0200

    Restore `versions` DB table and add learning tests

---
+++ db/migrate/20190302091059_restore_versions.rb
@@ -0,0 +1,15 @@
+class RestoreVersions < ActiveRecord::Migration
+  def change
+    drop_table :versions
+
+    create_table :versions do |t|
+      t.string :item_type, :null => false
+      t.integer :item_id, :null => false
+      t.string :event, :null => false
+      t.string :whodunnit
+      t.text :object
+      t.datetime :created_at
+    end
+    add_index :versions, [:item_type, :item_id]
+  end
+end
--- lib/gem_ext/paper_trail.rb
+++ lib/gem_ext/paper_trail.rb
@@ -1,10 +1,3 @@
-# the following line is required for PaperTrail >= 4.0.0 with Rails
-PaperTrail::Rails::Engine.eager_load!
-
-PaperTrail::Version.module_eval do
-  self.abstract_class = true
-end
-
 # Store console and rake changes in versions
 if defined?(::Rails::Console)
   PaperTrail.whodunnit = "console-#{`whoami`.strip}"
