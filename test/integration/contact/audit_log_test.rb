6bab335f5ac84d6eab9f9244d81d0a344f9746bb
Artur Beljajev <artur.beljajev@internet.ee>
2019-03-04 15:26:25 +0200

    Add tests

---
+++ test/integration/contact/audit_log_test.rb
@@ -0,0 +1,16 @@
+require 'test_helper'
+
+class ContactAuditLogTest < ActionDispatch::IntegrationTest
+  def test_stores_metadata
+    contact = contacts(:john)
+
+    contact.legal_document_id = 1
+    assert_difference 'contact.versions.count' do
+      contact.save!
+    end
+
+    contact_version = contact.versions.last
+    assert_equal ({ legal_documents: [1] }).with_indifferent_access,
+                 contact_version.children.with_indifferent_access
+  end
+end
