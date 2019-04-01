e9a39bdc3b6afd2046ea7784954d4ef88a3865ca
Maciej Szlosarczyk <maciej.szlosarczyk@eestiinternet.ee>
2019-04-01 14:45:55 +0300

    Keep whois schema up to date with actual whois schema

--- db/whois_schema.rb
+++ db/whois_schema.rb
@@ -41,7 +41,6 @@ ActiveRecord::Schema.define(version: 20181102124618) do
 
   create_table "whois_records", force: :cascade do |t|
     t.string   "name"
-    t.text     "body"
     t.json     "json"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
