9d161c671da079086ae2053b5db2dc96d8b8e63e
Artur Beljajev <artur.beljajev@internet.ee>
2019-04-12 19:25:02 +0300

    Remove unused method

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -3,14 +3,6 @@ class ApplicationMailer < ActionMailer::Base
   default from: 'noreply@internet.ee'
   layout 'mailer'
 
-  # turn on delivery on specific (epp) request only, thus rake tasks does not deliver anything
-  def delivery_off?(model, deliver_emails = false)
-    return false if deliver_emails == true
-    logger.info "EMAIL SENDING WAS NOT ACTIVATED " \
-      "BY MODEL OBJECT: id ##{model.try(:id)} deliver_emails returned false"
-    true
-  end
-
   def format(email)
     local, host = email.split('@')
     host = SimpleIDN.to_ascii(host)
