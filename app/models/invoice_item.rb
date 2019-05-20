c807bb9fcd7cb3b02c26dd94769a85985afcf047
Artur Beljajev <artur.beljajev@internet.ee>
2019-05-20 17:33:24 +0300

    Enhance `InvoiceItem`
    #329

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -2,7 +2,18 @@ class InvoiceItem < ActiveRecord::Base
   include Versions
   belongs_to :invoice
 
+  delegate :vat_rate, to: :invoice
+
   def item_sum_without_vat
     (price * quantity).round(2)
+  end
+  alias_method :subtotal, :item_sum_without_vat
+
+  def vat_amount
+    subtotal * (vat_rate / 100)
+  end
+
+  def total
+    subtotal + vat_amount
   end
 end
