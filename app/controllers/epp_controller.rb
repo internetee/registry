99ddcadd893e22ef827e09d82e78e096b8579d62
Artur Beljajev <artur.beljajev@internet.ee>
2019-09-12 17:17:47 +0300

    Remove unnecessary statement
    CSRF protection makes no sense in EPP and is already disabled with
    `skip_before_action :verify_authenticity_token`.

--- app/controllers/epp_controller.rb
+++ app/controllers/epp_controller.rb
@@ -1,6 +1,5 @@
 class EppController < ApplicationController
   layout false
-  protect_from_forgery with: :null_session
   skip_before_action :verify_authenticity_token
 
   before_action :ensure_session_id_passed
