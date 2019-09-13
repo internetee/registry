744d6a2b537b4e3f70d429f886f1fd382d44f644
Artur Beljajev <artur.beljajev@internet.ee>
2019-09-13 18:23:18 +0300

    Remove unnecessary test

--- test/integration/epp/login_test.rb
+++ test/integration/epp/login_test.rb
@@ -134,43 +134,6 @@ class EppLoginTest < EppTestCase
     assert_equal new_password, user.plain_text_password
   end
 
-  def test_not_reached
-    travel_to Time.zone.parse('2010-07-05')
-    EppSession.delete_all
-    request_xml = <<-XML
-      <?xml version="1.0" encoding="UTF-8" standalone="no"?>
-      <epp xmlns="https://epp.tld.ee/schema/epp-ee-1.0.xsd">
-        <command>
-          <login>
-            <clID>test_bestnames</clID>
-            <pw>testtest</pw>
-            <options>
-              <version>1.0</version>
-              <lang>en</lang>
-            </options>
-            <svcs>
-              <objURI>https://epp.tld.ee/schema/domain-eis-1.0.xsd</objURI>
-              <objURI>https://epp.tld.ee/schema/contact-ee-1.1.xsd</objURI>
-              <objURI>urn:ietf:params:xml:ns:host-1.0</objURI>
-              <objURI>urn:ietf:params:xml:ns:keyrelay-1.0</objURI>
-            </svcs>
-          </login>
-        </command>
-      </epp>
-    XML
-
-    (EppSession.limit_per_registrar - 1).times do
-      EppSession.create!(session_id: SecureRandom.hex,
-                         user: users(:api_bestnames),
-                         updated_at: Time.zone.parse('2010-07-05'))
-    end
-
-    assert_difference 'EppSession.count' do
-      post '/epp/session/login', { frame: request_xml }, 'HTTP_COOKIE' => 'session=non-existent'
-    end
-    assert_epp_response :completed_successfully
-  end
-
   def test_user_cannot_login_when_session_limit_reached
     user = users(:api_bestnames)
     travel_to Time.zone.parse('2010-07-05')
