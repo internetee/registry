4a6a6058018e961963ffe85bb54624207dae500b
Artur Beljajev <artur.beljajev@internet.ee>
2019-10-16 13:11:07 +0300

    Use `wkhtmltopdf-binary` gem instead of bundled binary

--- config/initializers/pdfkit.rb
+++ config/initializers/pdfkit.rb
@@ -1,9 +1,4 @@
 PDFKit.configure do |config|
-  installed = %x(which wkhtmltopdf).chomp
-  if installed == "" then
-    installed = "#{Rails.root}/vendor/bin/wkhtmltopdf"
-  end
-  config.wkhtmltopdf = installed
   config.default_options = {
     page_size: 'A4',
     quiet: true,
