0924381473d7ccff663b13c6c4706da560fd206c
Georg Kahest <georg@kahest.ee>
2020-01-27 09:15:45 +0200

    fixup! follow styleguide

--- app/controllers/epp/sessions_controller.rb
+++ app/controllers/epp/sessions_controller.rb
@@ -13,7 +13,9 @@ module Epp
       webclient_request = ENV['webclient_ips'].split(',').map(&:strip).include?(request.ip)
       if webclient_request && !Rails.env.test? && !Rails.env.development?
         client_md5 = Certificate.parse_md_from_string(request.env['HTTP_SSL_CLIENT_CERT'])
-        fail 'webclient cert (cert_path) missing, registrar epp/repp disabled' unless ENV['cert_path'].present?
+        if ENV['cert_path'].blank?
+          raise 'webclient cert (cert_path) missing, registrar (r)epp disabled'
+        end
 
         server_md5 = Certificate.parse_md_from_string(File.read(ENV['cert_path']))
         if client_md5 != server_md5
