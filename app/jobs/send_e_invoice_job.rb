2663c550fa1aee229679e9d407b12b5a2a5c0369
Alex Sherman <yul.golem@gmail.com>
2020-02-04 16:00:24 +0500

    Add idempotent Que job for e-invoice sending

---
+++ app/jobs/send_e_invoice_job.rb
@@ -0,0 +1,39 @@
+class SendEInvoiceJob < Que::Job
+
+  def run(invoice)
+    return if invoice.e_invoice_sent_at
+
+    e_invoice = invoice.to_e_invoice
+    e_invoice.deliver
+
+    ActiveRecord::Base.transaction do
+      invoice.update(e_invoice_sent_at: Time.zone.now)
+      log_success(invoice)
+      destroy
+    end
+
+  rescue Savon::Error => e
+    log_error(invoice: invoice, error: e)
+  end
+
+  private
+
+  def log_success(invoice)
+    message = "E-Invoice for an invoice with ID # #{invoice.id} was sent successfully"
+    logger.info message
+  end
+
+  def log_error(invoice:, error:)
+    message = <<~TEXT.squish
+      There was an error sending e-invoice for invoice with ID # #{invoice.id}.
+      The error message was the following: #{error}.
+      This job will retry
+    TEXT
+    logger.error message
+  end
+
+  def logger
+    Rails.logger
+  end
+
+end
