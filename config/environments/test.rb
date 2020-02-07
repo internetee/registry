8de9c1d7c66cb80e3d77c21c5561983fc7d449f4
Alex Sherman <yul.golem@gmail.com>
2020-02-07 19:01:39 +0500

    Silence deprecation warnings

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,3 +1,4 @@
+
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
@@ -30,7 +31,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
 
   # Print deprecation notices to the stderr.
-  config.active_support.deprecation = :stderr
+  config.active_support.deprecation = :silence
 
   # Raises error for missing translations
   config.action_view.raise_on_missing_translations = true
