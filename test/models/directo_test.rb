d6dd4eccf57329349cada479f5168d80ac595181
Alex Sherman <yul.golem@gmail.com>
2020-02-10 14:35:48 +0500

    Check if Directo counter is in range
    See #277

--- test/models/directo_test.rb
+++ test/models/directo_test.rb
@@ -5,6 +5,19 @@ class DirectoTest < ActiveSupport::TestCase
     @invoice = invoices(:one)
   end
 
+  def test_monthly_invoices_max_range_raises_if_overlaps
+
+    Setting.directo_monthly_number_max = Setting.directo_monthly_number_last.to_i + Registrar.count - 1
+    error_message = 'Directo counter is out of period (max allowed number is smaller than last '\
+                    'counternumber plus Registrar\'s count)'
+
+    error = assert_raises RuntimeError do
+      Directo.send_monthly_invoices
+    end
+
+    assert_equal error_message, error.message
+  end
+
   def test_xml_is_include_transaction_date
     @invoice.update(total: @invoice.account_activity.bank_transaction.sum)
     @invoice.account_activity.bank_transaction.update(paid_at: Time.zone.now)
