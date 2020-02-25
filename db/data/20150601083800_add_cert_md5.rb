4ddbb08e80879fcf365120b1ad897f9c97675ba7
Alex Sherman <yul.golem@gmail.com>
2020-02-25 14:19:24 +0500

    Move data migrations from rake tasks to gem
    See #1298

--- db/data/20150601083800_add_cert_md5.rb
+++ db/data/20150601083800_add_cert_md5.rb
@@ -1,28 +1,27 @@
-class AddCertMd5 < ActiveRecord::Migration
+class AddCertMd5 < ActiveRecord::Migration[5.1]
   def self.up
-    Certificate.all.each do |x|
-      if x.crt.present? && x.csr.present?
-        x.interface = Certificate::REGISTRAR
-        x.md5 = OpenSSL::Digest::MD5.new(x.parsed_crt.to_der).to_s
-
-        pc = x.parsed_crt.try(:subject).try(:to_s) || ''
-        cn = pc.scan(/\/CN=(.+)/).flatten.first
-        x.common_name = cn.split('/').first
-      elsif x.crt.present? && x.csr.blank?
-        x.interface = Certificate::API
-        x.md5 = OpenSSL::Digest::MD5.new(x.parsed_crt.to_der).to_s
-
-        pc = x.parsed_crt.try(:subject).try(:to_s) || ''
-        cn = pc.scan(/\/CN=(.+)/).flatten.first
-        x.common_name = cn.split('/').first
-      elsif x.crt.blank? && x.csr.present?
-        x.interface = Certificate::REGISTRAR
-      end
-      x.save
-    end
+    # Certificate.all.each do |x|
+    #   if x.crt.present? && x.csr.present?
+    #     x.interface = Certificate::REGISTRAR
+    #     x.md5 = OpenSSL::Digest::MD5.new(x.parsed_crt.to_der).to_s
+    #
+    #     pc = x.parsed_crt.try(:subject).try(:to_s) || ''
+    #     cn = pc.scan(/\/CN=(.+)/).flatten.first
+    #     x.common_name = cn.split('/').first
+    #   elsif x.crt.present? && x.csr.blank?
+    #     x.interface = Certificate::API
+    #     x.md5 = OpenSSL::Digest::MD5.new(x.parsed_crt.to_der).to_s
+    #
+    #     pc = x.parsed_crt.try(:subject).try(:to_s) || ''
+    #     cn = pc.scan(/\/CN=(.+)/).flatten.first
+    #     x.common_name = cn.split('/').first
+    #   elsif x.crt.blank? && x.csr.present?
+    #     x.interface = Certificate::REGISTRAR
+    #   end
+    #   x.save
+    # end
   end
 
   def self.down
-    raise ActiveRecord::IrreversibleMigration
   end
 end
