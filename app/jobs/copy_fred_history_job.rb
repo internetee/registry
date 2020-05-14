3fa9ad5101e28fcf92d3c1a9f2099fd40bb99660
Alex Sherman <yul.golem@gmail.com>
2020-05-14 18:18:05 +0500

    Fix FRED domain history processing

--- app/jobs/copy_fred_history_job.rb
+++ app/jobs/copy_fred_history_job.rb
@@ -12,14 +12,14 @@ class CopyFredHistoryJob < Que::Job
     history_entries.each do |entry|
       recorded_at = entry[:update]
       attrs = { recorded_at: recorded_at, object_id: domain.id }
-
       already_exist = Audit::DomainHistory.find_by(attrs).present?
       next if already_exist
 
-      attrs[:old_value] = { statuses: [process_status(entry[:from_status])] }
-      attrs[:new_value] = { statuses: [process_status(entry[:to_status])],
+      attrs[:old_value] = { statuses: process_status(entry[:from_status]) }
+      attrs[:new_value] = { statuses: process_status(entry[:to_status]),
                             name: entry[:name] }
       attrs[:action] = process_action(entry[:status])
+      binding.pry
       new_history_array << attrs
     end
 
@@ -40,14 +40,15 @@ class CopyFredHistoryJob < Que::Job
   end
 
   def process_status(status)
-    status.blank? ? 'ok' : status
+    status = status.gsub(/{|}|NULL/, '').split(',')
+    status.blank? ? ['ok'] : status
   end
 
   def process_action(action)
     case action
     when 'DomainUpdate', 'DomainRenew', 'DomainTransfer'
       'UPDATE'
-    when `DomainCreate`
+    when 'DomainCreate'
       'INSERT'
     else
       'DELETE'
@@ -56,19 +57,22 @@ class CopyFredHistoryJob < Que::Job
 
   def object_history(legacy_id)
     sql = <<~SQL.gsub(/\s+/, " ").strip
-      select ena.status, oh.*, ae.*, obr.*, eos1.name as from_status, eos2.name as to_status
+      select ena.status, oh.id, oh.historyid, obr.name, d.registrant,
+             array_agg(eos1.name) as from_status,  array_agg(eos2.name) as to_status
       from object_history oh
       join history h on h.id = oh.historyid
       join action a on a.id = h.action
       join action_elements ae on a.id = ae.actionid
       join object_registry obr on obr.id = oh.id
-      left join object_state obs1 on obs1.ohid_from = h.id
-      left join object_state obs2 on obs2.ohid_to = h.id
+      join domain d on obr.id = d.id
+      left join object_state obs1 on h.id = obs1.ohid_from
+      left join object_state obs2 on h.id = obs2.ohid_to
       left join enum_object_states eos1 on obs1.state_id = eos1.id
       left join enum_object_states eos2 on obs2.state_id = eos2.id
       join enum_action ena on ena.id = a.action
       where obr.type = 3
-      AND obr.id=:id
+      and obr.id = :id
+      group by oh.historyid, ena.status, oh.id, obr.name, crdate, d.registrant
       order by obr.name ASC, crdate ASC
     SQL
     bind = { id: legacy_id }
@@ -93,6 +97,16 @@ class CopyFredHistoryJob < Que::Job
       select * from contact_history
     SQL
     result_entries(sql)
+  end
+
+  def initial_contact_state(id)
+    sql = <<~SQL.gsub(/\s+/, " ").strip
+      select * from contact_history
+      where id = :id
+      order by historyid asc limit 1
+    SQL
+    bind = { id: id }
+    result_entries(sql: sql, bind: bind)
   end
 end
 
