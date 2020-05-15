13e76fff407bd319bc16009f5f9e0b9261770cb9
Alex Sherman <yul.golem@gmail.com>
2020-05-15 14:08:09 +0500

    Save FRED domain history with initial registrant/admin contact

--- app/jobs/copy_fred_history_job.rb
+++ app/jobs/copy_fred_history_job.rb
@@ -1,25 +1,52 @@
 class CopyFredHistoryJob < Que::Job
   include FredSqlUtils
+  attr_accessor :initial_contacts_history, :object_history, :domain_contacts
 
   def run
     process_domains
-    process_contacts
+    # process_contacts
   end
 
   def process_domain(domain)
     new_history_array = []
-    history_entries = object_history(domain.legacy_id)
+    history_entries = object_history.select { |entry| entry[:id] == domain.legacy_id }
     history_entries.each do |entry|
       recorded_at = entry[:update]
       attrs = { recorded_at: recorded_at, object_id: domain.id }
+
       already_exist = Audit::DomainHistory.find_by(attrs).present?
       next if already_exist
 
+      start_reg = initial_contacts_history.detect{ |val| val[:id] == entry[:registrant] }
+      admin_contact_id = domain_contacts.detect{ |val| val[:domainid] == entry[:id] }[:contactid]
+      admin = initial_contacts_history.detect{ |val| val[:id] == admin_contact_id }
+
       attrs[:old_value] = { statuses: process_status(entry[:from_status]) }
       attrs[:new_value] = { statuses: process_status(entry[:to_status]),
-                            name: entry[:name] }
+                            name: entry[:name],
+                            children: { registrant: [entry[:registrant]],
+                                        admin_contact: [admin_contact_id],
+                                        registrant_initial: { id: entry[:registrant],
+                                                              fax: start_reg[:fax],
+                                                              zip: start_reg[:postalcode],
+                                                              city: start_reg[:city],
+                                                              name: start_reg[:name],
+                                                              email: start_reg[:email],
+                                                              ident: start_reg[:ssn],
+                                                              phone: start_reg[:telephone],
+                                                              state: start_reg[:stateorprovince],
+                                                              street: start_reg[:street1] },
+                                        admin_contacts_initial: [{ id: admin[:id],
+                                                                   fax: admin[:fax],
+                                                                   zip: admin[:postalcode],
+                                                                   city: admin[:city],
+                                                                   name: admin[:name],
+                                                                   email: admin[:email],
+                                                                   ident: admin[:ssn],
+                                                                   phone: admin[:telephone],
+                                                                   state: admin[:stateorprovince],
+                                                                   street: admin[:street1] }] } }
       attrs[:action] = process_action(entry[:status])
-      binding.pry
       new_history_array << attrs
     end
 
@@ -55,9 +82,13 @@ class CopyFredHistoryJob < Que::Job
     end
   end
 
-  def object_history(legacy_id)
+  def object_history
+    @object_history ||= object_request
+  end
+
+  def object_request
     sql = <<~SQL.gsub(/\s+/, " ").strip
-      select ena.status, oh.id, oh.historyid, obr.name, d.registrant,
+      select ena.status, oh.id, h.valid_from as update, oh.historyid, obr.name, d.registrant,
              array_agg(eos1.name) as from_status,  array_agg(eos2.name) as to_status
       from object_history oh
       join history h on h.id = oh.historyid
@@ -71,42 +102,45 @@ class CopyFredHistoryJob < Que::Job
       left join enum_object_states eos2 on obs2.state_id = eos2.id
       join enum_action ena on ena.id = a.action
       where obr.type = 3
-      and obr.id = :id
-      group by oh.historyid, ena.status, oh.id, obr.name, crdate, d.registrant
+      group by oh.historyid, ena.status, h.valid_from, oh.id, obr.name, crdate, d.registrant
       order by obr.name ASC, crdate ASC
     SQL
-    bind = { id: legacy_id }
-    result_entries(sql: sql, bind: bind)
+    result_entries(sql: sql)
   end
 
-  def domain_with_registrant_history(legacy_id)
-    sql = <<~SQL.gsub(/\s+/, " ").strip
-      select dh.*, o.name as domain_name, c.*
-      from domain_history dh
-      join object_registry o on dh.id = o.id
-      join contact c on dh.registrant = c.id
-      where o.id=:id
-      order by domain_name ASC
-    SQL
-    bind = { id: legacy_id }
-    result_entries(sql: sql, bind: bind)
+  def domain_contacts
+    @domain_contacts ||= domain_contact_request
   end
 
-  def contact_history
+  def domain_contact_request
     sql = <<~SQL.gsub(/\s+/, " ").strip
-      select * from contact_history
+      SELECT DISTINCT ON (domainid)
+                          domainid, contactid
+      FROM   domain_contact_map_history
+      ORDER  BY domainid, historyid;
     SQL
-    result_entries(sql)
+    result_entries(sql: sql)
   end
 
-  def initial_contact_state(id)
+  def initial_contacts_history
+    @initial_contacts_history ||= initial_contacts_request
+  end
+
+  def initial_contacts_request
     sql = <<~SQL.gsub(/\s+/, " ").strip
-      select * from contact_history
-      where id = :id
-      order by historyid asc limit 1
+            WITH summary AS (
+              SELECT ch.id,
+                     ch.name,
+                     ch.historyid,
+                     ROW_NUMBER() OVER(PARTITION BY ch.id
+                                           ORDER BY ch.historyid asc) AS rk
+                FROM contact_history ch)
+      SELECT s.*, ch.*
+        FROM summary s
+        join contact_history ch on s.historyid = ch.historyid
+       WHERE s.rk = 1
     SQL
-    bind = { id: id }
-    result_entries(sql: sql, bind: bind)
+    result_entries(sql: sql)
   end
 end
 
