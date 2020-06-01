e92afb59c1a5cd3c4894ff280491aac1dc3268d3
Alex Sherman <yul.golem@gmail.com>
2020-06-01 15:11:47 +0500

    Add tests, fix error handling

--- app/controllers/admin/legal_documents_controller.rb
+++ app/controllers/admin/legal_documents_controller.rb
@@ -5,14 +5,11 @@ module Admin
     def show
       @ld = LegalDocument.find(params[:id])
       filename = @ld.path.split('/').last
-      file = File.open(@ld.path).read
-
-      if file
-        send_data file, filename: filename
-      else
-        flash[:notice] = I18n.t('legal_doc_not_found')
-        redirect_to [:admin, @ld.documentable]
-      end
+      file = File.open(@ld.path)&.read
+      send_data file, filename: filename
+    rescue Errno::ENOENT
+      flash[:notice] = I18n.t('legal_doc_not_found')
+      redirect_to [:admin, @ld.documentable]
     end
   end
 end
