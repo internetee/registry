2fdc4875115a528e4c46a858bbe3cfc234d8da32
Alex Sherman <yul.golem@gmail.com>
2020-06-22 15:03:48 +0500

    Fix possible doubling of contact objects after race

---
+++ db/migrate/20200622080240_add_unique_constraints_to_domain_objects.rb
@@ -0,0 +1,25 @@
+class AddUniqueConstraintsToDomainObjects < ActiveRecord::Migration[6.0]
+  def up
+    execute <<-SQL
+      alter table domain_contacts
+        add constraint uniq_contact_of_type_per_domain unique (domain_id, type, contact_id);
+    SQL
+
+    execute <<-SQL
+      alter table nameservers
+        add constraint uniq_hostname_per_domain unique (domain_id, hostname);
+    SQL
+  end
+
+  def down
+    execute <<-SQL
+      alter table domain_contacts
+        drop constraint if exists uniq_contact_of_type_per_domain;
+    SQL
+
+    execute <<-SQL
+      alter table nameservers
+        drop constraint if exists uniq_hostname_per_domain;
+    SQL
+  end
+end
