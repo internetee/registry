3d734ff746291c9b6d65ab54f0c005804c14d888
Alex Sherman <yul.golem@gmail.com>
2020-06-30 19:16:33 +0500

    Add opt-out commentary field to registrar

--- db/migrate/20200630081231_add_legal_doc_optout_to_registrar.rb
+++ db/migrate/20200630081231_add_legal_doc_optout_to_registrar.rb
@@ -1,5 +1,6 @@
 class AddLegalDocOptoutToRegistrar < ActiveRecord::Migration[6.0]
   def change
     add_column :registrars, :legaldoc_optout, :boolean, null: false, default: false
+    add_column :registrars, :legaldoc_optout_comment, :text
   end
 end
