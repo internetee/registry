b5668ea49485066d2452d5021c2afb32da4f5278
Karl Erik Õunapuu <karlerik@kreative.ee>
2020-09-07 13:11:40 +0300

    Fix some CC issues

--- app/models/bank_statement.rb
+++ app/models/bank_statement.rb
@@ -6,15 +6,15 @@ class BankStatement < ApplicationRecord
 
   validates :bank_code, :iban, presence: true
 
-  FULLY_BINDED = 'fully_binded'
-  PARTIALLY_BINDED = 'partially_binded'
-  NOT_BINDED = 'not_binded'
+  FULLY_BINDED = 'fully_binded'.freeze
+  PARTIALLY_BINDED = 'partially_binded'.freeze
+  NOT_BINDED = 'not_binded'.freeze
 
   # TODO: Cache this to database so it can be used for searching
   def status
     if bank_transactions.unbinded.count == bank_transactions.count
       NOT_BINDED
-    elsif bank_transactions.unbinded.count == 0
+    elsif bank_transactions.unbinded.count.zero?
       FULLY_BINDED
     else
       PARTIALLY_BINDED
