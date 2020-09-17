22f9c2058d137a021034030c66ac33f8049f17a2
Karl Erik Õunapuu <karlerik@kreative.ee>
2020-09-17 11:55:52 +0300

    Create api/v1/bounces endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -64,6 +64,7 @@ Rails.application.routes.draw do
       end
 
       resources :auctions, only: %i[index show update], param: :uuid
+      resources :bounces, only: %i[create]
     end
 
     match '*all', controller: 'cors', action: 'cors_preflight_check', via: [:options],
