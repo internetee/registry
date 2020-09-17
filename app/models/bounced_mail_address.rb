6af37a787dc56ec7b4d89079d6bf11d5ddf58651
Karl Erik Õunapuu <karlerik@kreative.ee>
2020-09-17 13:26:48 +0300

    Restructure bounced mails view

--- app/models/bounced_mail_address.rb
+++ app/models/bounced_mail_address.rb
@@ -1,2 +1,24 @@
 class BouncedMailAddress < ApplicationRecord
+  validates :email, presence: true
+  validates :bounce_reason, presence: true
+
+  def diagnostic
+    response_json['diagnosticCode']
+  end
+
+  def action
+    response_json['action']
+  end
+
+  def status
+    response_json['status']
+  end
+
+  def self.record(json)
+    bounced_records = json['bounce']['bouncedRecipients']
+    bounced_records.each do |record|
+      bounce_record = BouncedMailAddress.new(email: record['emailAddress'], response_json: record)
+      bounce_record.save
+    end
+  end
 end
