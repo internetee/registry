659cb7f4e67ee51545b6ea0ce5b20e030e881591
Karl Erik Õunapuu <karlerik@kreative.ee>
2020-09-21 13:34:34 +0300

    Combine migrations, remove json objects from bounced mails

--- db/migrate/20200921084356_remove_incidents_from_bounced_mail_addresses.rb
+++
@@ -1,9 +0,0 @@
-class RemoveIncidentsFromBouncedMailAddresses < ActiveRecord::Migration[6.0]
-  def up
-    remove_column :bounced_mail_addresses, :incidents
-  end
-
-  def down
-    add_column :bounced_mail_addresses, :incidents, :integer, null: false, default: 1
-  end
-end
