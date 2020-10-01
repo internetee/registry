3f07e3ffc3f54796e2c66acc1a3cb5ff9bb4b244
Karl Erik Õunapuu <karlerik@kreative.ee>
2020-10-01 17:01:54 +0300

    Expand TARA auth flow to registrant portal

--- config/routes.rb
+++ config/routes.rb
@@ -195,6 +195,15 @@ Rails.application.routes.draw do
     # POST /registrant/sign_in is not used
     devise_for :users, path: '', class_name: 'RegistrantUser'
 
+      # /registrant/id path is hardcoded in Apache config for authentication with Estonian ID-card
+      # Client certificate is asked only on login form submission, therefore the path must be different from the one in
+      # `new_registrant_user_session_path` route, in case some other auth type will be implemented
+      post 'id' => 'sessions#create', as: :id_card_sign_in
+      match '/open_id/callback', via: %i[get post], to: 'tara#callback', as: :tara_registrant_callback
+      match '/open_id/cancel', via: %i[get post delete], to: 'tara#cancel',
+            as: :tara_registrant_cancel
+    end
+
     resources :registrars, only: :show
     # resources :companies, only: :index
     resources :domains, only: %i[index show] do
