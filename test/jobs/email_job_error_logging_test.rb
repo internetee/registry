e3421175f685c2bdb1141c2cbf36b73486cbb81e
Alex Sherman <yul.golem@gmail.com>
2020-10-28 16:02:42 +0500

    Add final job test

--- test/jobs/email_job_error_logging_test.rb
+++ test/jobs/email_job_error_logging_test.rb
@@ -9,20 +9,28 @@ class EmailJobLoggingTest < ActiveJob::TestCase
 
   def test_runs_saving_method
     domain = domains(:shop)
+    error = 'StandardError'
 
-    perform_enqueued_jobs do
-      StubEmailJob.perform_now domain.id
+    assert_difference -> { BouncedMailAddress.all.count }, 1 do
+      perform_enqueued_jobs do
+        StubEmailJob.perform_now(domain.id, error)
+      end
     end
+
+    bounced_mail_address = BouncedMailAddress.last
+    assert_equal bounced_mail_address.email, domain.registrant.email
+    assert_equal bounced_mail_address.job_name, 'StubEmailJob'
+    assert_equal bounced_mail_address.error_description, error
   end
 end
 
 class StubEmailJob < EmailJob
   queue_as :default
 
-  def perform(domain_id)
+  def perform(domain_id, error)
     domain = Domain.find(domain_id)
     @email =  domain.registrant.email
 
-    raise StandardError
+    raise error.constantize
   end
 end
