6a6265e3a5b97918558ae4218119061ce665679c
Alex Sherman <yul.golem@gmail.com>
2020-11-18 18:09:31 +0500

    Move CancelForceDelete to interactor

---
+++ app/interactions/cancel_force_delete_interaction/restore_statuses_before_force_delete.rb
@@ -0,0 +1,9 @@
+module CancelForceDeleteInteraction
+  class RestoreStatusesBeforeForceDelete < Base
+    def execute
+      domain.statuses = domain.statuses_before_force_delete
+      domain.statuses_before_force_delete = nil
+      domain.save(validate: false)
+    end
+  end
+end
