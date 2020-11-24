89c5413fc702c8399474ff18f6f245af531deeba
Alex Sherman <yul.golem@gmail.com>
2020-11-24 13:26:38 +0500

    Unused class clean-up

--- app/jobs/domain_delete_confirm_email_job.rb
+++
@@ -1,6 +0,0 @@
-class DomainDeleteConfirmEmailJob < Que::Job
-  # def run(domain_id)
-  #   domain = Domain.find(domain_id)
-  #   DomainDeleteConfirmInteraction::SendRequest.run(domain: domain)
-  # end
-end
