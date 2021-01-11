1bbacbf56c16b7345ac5d74e2a01410cbf22d56f
Alex Sherman <yul.golem@gmail.com>
2021-01-11 13:29:22 +0500

    Fix class/routes namespace for ContactRequest endpoint

---
+++ app/controllers/api/v1/contact_requests_controller.rb
@@ -0,0 +1,23 @@
+module Api
+  module V1
+    class ContactRequestsController < BaseController
+      before_action :authenticate_shared_key
+
+      # POST api/v1/contact_requests/
+      def create
+        return head(:bad_request) if contact_request_params[:email].blank?
+
+        ContactRequest.save_record(contact_request_params)
+        head(:created)
+      rescue ActionController::ParameterMissing
+        head(:bad_request)
+      end
+
+      def update; end
+
+      def contact_request_params
+        params.require(:contact_request).permit(:email, :whois_record_id, :name, :status, :id)
+      end
+    end
+  end
+end
