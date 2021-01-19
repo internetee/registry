a5f8e277ddfd01fac63e9c0bc2e04b07216d4001
Oleg Hasjanov <oleg.hasjanov@eestiinternet.ee>
2021-01-19 11:53:00 +0200

    covered white_ips and covered a little certificates

--- test/integration/admin_area/certificates_test.rb
+++ test/integration/admin_area/certificates_test.rb
@@ -13,32 +13,16 @@ class AdminAreaCertificatesIntegrationTest < JavaScriptApplicationSystemTestCase
 
         @apiuser = users(:api_bestnames)
         @certificate = certificates(:api)
-        # @certificate.update!(csr: "-----BEGIN CERTIFICATE REQUEST-----
-        # MIICszCCAZsCAQAwbjELMAkGA1UEBhMCRUUxFDASBgNVBAMMC2ZyZXNoYm94LmVl
-        # MRAwDgYDVQQHDAdUYWxsaW5uMREwDwYDVQQKDAhGcmVzaGJveDERMA8GA1UECAwI
-        # SGFyanVtYWExETAPBgNVBAsMCEZyZXNoYm94MIIBIjANBgkqhkiG9w0BAQEFAAOC
-        # AQ8AMIIBCgKCAQEA1VVESynZoZhIbe8s9zHkELZ/ZDCGiM2Q8IIGb1IOieT5U2mx
-        # IsVXz85USYsSQY9+4YdEXnupq9fShArT8pstS/VN6BnxdfAiYXc3UWWAuaYAdNGJ
-        # Dr5Jf6uMt1wVnCgoDL7eJq9tWMwARC/viT81o92fgqHFHW0wEolfCmnpik9o0ACD
-        # FiWZ9IBIevmFqXtq25v9CY2cT9+eZW127WtJmOY/PKJhzh0QaEYHqXTHWOLZWpnp
-        # HH4elyJ2CrFulOZbHPkPNB9Nf4XQjzk1ffoH6e5IVys2VV5xwcTkF0jY5XTROVxX
-        # lR2FWqic8Q2pIhSks48+J6o1GtXGnTxv94lSDwIDAQABoAAwDQYJKoZIhvcNAQEL
-        # BQADggEBAEFcYmQvcAC8773eRTWBJJNoA4kRgoXDMYiiEHih5iJPVSxfidRwYDTF
-        # sP+ttNTUg3JocFHY75kuM9T2USh+gu/trRF0o4WWa+AbK3JbbdjdT1xOMn7XtfUU
-        # Z/f1XCS9YdHQFCA6nk4Z+TLWwYsgk7n490AQOiB213fa1UIe83qIfw/3GRqRUZ7U
-        # wIWEGsHED5WT69GyxjyKHcqGoV7uFnqFN0sQVKVTy/NFRVQvtBUspCbsOirdDRie
-        # AB2KbGHL+t1QrRF10szwCJDyk5aYlVhxvdI8zn010nrxHkiyQpDFFldDMLJl10BW
-        # 2w9PGO061z+tntdRcKQGuEpnIr9U5Vs=
-        # -----END CERTIFICATE REQUEST----\n")
+        @certificate.update!(csr: "-----BEGIN CERTIFICATE REQUEST-----\nMIICszCCAZsCAQAwbjELMAkGA1UEBhMCRUUxFDASBgNVBAMMC2ZyZXNoYm94LmVl\nMRAwDgYDVQQHDAdUYWxsaW5uMREwDwYDVQQKDAhGcmVzaGJveDERMA8GA1UECAwI\nSGFyanVtYWExETAPBgNVBAsMCEZyZXNoYm94MIIBIjANBgkqhkiG9w0BAQEFAAOC\nAQ8AMIIBCgKCAQEA1VVESynZoZhIbe8s9zHkELZ/ZDCGiM2Q8IIGb1IOieT5U2mx\nIsVXz85USYsSQY9+4YdEXnupq9fShArT8pstS/VN6BnxdfAiYXc3UWWAuaYAdNGJ\nDr5Jf6uMt1wVnCgoDL7eJq9tWMwARC/viT81o92fgqHFHW0wEolfCmnpik9o0ACD\nFiWZ9IBIevmFqXtq25v9CY2cT9+eZW127WtJmOY/PKJhzh0QaEYHqXTHWOLZWpnp\nHH4elyJ2CrFulOZbHPkPNB9Nf4XQjzk1ffoH6e5IVys2VV5xwcTkF0jY5XTROVxX\nlR2FWqic8Q2pIhSks48+J6o1GtXGnTxv94lSDwIDAQABoAAwDQYJKoZIhvcNAQEL\nBQADggEBAEFcYmQvcAC8773eRTWBJJNoA4kRgoXDMYiiEHih5iJPVSxfidRwYDTF\nsP+ttNTUg3JocFHY75kuM9T2USh+gu/trRF0o4WWa+AbK3JbbdjdT1xOMn7XtfUU\nZ/f1XCS9YdHQFCA6nk4Z+TLWwYsgk7n490AQOiB213fa1UIe83qIfw/3GRqRUZ7U\nwIWEGsHED5WT69GyxjyKHcqGoV7uFnqFN0sQVKVTy/NFRVQvtBUspCbsOirdDRie\nAB2KbGHL+t1QrRF10szwCJDyk5aYlVhxvdI8zn010nrxHkiyQpDFFldDMLJl10BW\n2w9PGO061z+tntdRcKQGuEpnIr9U5Vs=\n-----END CERTIFICATE REQUEST-----\n")
     end
 
-    # Helpers
+    # Helpers =======================
     def show_certificate_info
         visit admin_api_user_certificate_path(api_user_id: @apiuser.id, id: @certificate.id)
         assert_text 'Certificates'
     end
 
-    # Tests 
+    # TESTs ===========================
     def test_show_certificate_info
         show_certificate_info
     end
@@ -52,18 +36,38 @@ class AdminAreaCertificatesIntegrationTest < JavaScriptApplicationSystemTestCase
         assert_text 'Record deleted'        
     end
 
-    # TODO
-    # Should be display "Revoke this certificate" button
+    # download_csr_admin_api_user_certificate GET  /admin/api_users/:api_user_id/certificates/:id/download_csr(.:format)
+    def test_download_csr
+        get download_csr_admin_api_user_certificate_path(api_user_id: @apiuser.id, id: @certificate.id)
     
-    # def test_revoke_certificate
-    #     show_certificate_info
+        assert_response :ok
+        assert_equal 'application/octet-stream', response.headers['Content-Type']
+        assert_equal "attachment; filename=\"test_bestnames.csr.pem\"; filename*=UTF-8''test_bestnames.csr.pem", response.headers['Content-Disposition']
+        assert_not_empty response.body
+    end
 
-    #     element = find(:xpath, "//body/div[2]").native.attribute('outerHTML')
-    #     puts element
+    # download_crt_admin_api_user_certificate GET  /admin/api_users/:api_user_id/certificates/:id/download_crt(.:format)
+    def test_download_crt
+        get download_crt_admin_api_user_certificate_path(api_user_id: @apiuser.id, id: @certificate.id)
+    
+        assert_response :ok
+        assert_equal 'application/octet-stream', response.headers['Content-Type']
+        assert_equal "attachment; filename=\"test_bestnames.crt.pem\"; filename*=UTF-8''test_bestnames.crt.pem", response.headers['Content-Disposition']
+        assert_not_empty response.body
+    end
 
-    #     # find(:xpath, "/html/body/div[2]/div[5]/div/div/div[1]/div[2]/a[2]").click
+    def test_failed_to_revoke_certificate
+        show_certificate_info
 
-    #     # assert_text 'Record deleted'
-    # end
+        find(:xpath, "//a[text()='Revoke this certificate']").click
+        assert_text 'Failed to update record'
+
+        # element = find(:xpath, "//body").native.attribute('outerHTML')
+        # puts element
+
+        # find(:xpath, "/html/body/div[2]/div[5]/div/div/div[1]/div[2]/a[2]").click
+
+        # assert_text 'Record deleted'
+    end
 
 end
