8288d68640e27fcb6e21856855d357f82e455cf5
Oleg Hasjanov <oleg.hasjanov@eestiinternet.ee>
2021-01-21 11:15:01 +0200

    some fix

--- test/integration/admin_area/epp_logs_test.rb
+++ test/integration/admin_area/epp_logs_test.rb
@@ -7,40 +7,17 @@ class AdminEppLogsIntegrationTest < ApplicationSystemTestCase
         sign_in users(:admin)
     end
 
-    # def test_helper_test
-    #     user = users(:admin)
-    #     new_session_id = 'new-session-id'
-    
-    #     request_xml = <<-XML
-    #       <?xml version="1.0" encoding="UTF-8" standalone="no"?>
-    #       <epp xmlns="https://epp.tld.ee/schema/epp-ee-1.0.xsd">
-    #         <command>
-    #           <login>
-    #             <clID>#{user.username}</clID>
-    #             <pw>#{user.plain_text_password}</pw>
-    #             <options>
-    #               <version>1.0</version>
-    #               <lang>en</lang>
-    #             </options>
-    #             <svcs>
-    #               <objURI>https://epp.tld.ee/schema/domain-eis-1.0.xsd</objURI>
-    #               <objURI>https://epp.tld.ee/schema/contact-ee-1.1.xsd</objURI>
-    #               <objURI>urn:ietf:params:xml:ns:host-1.0</objURI>
-    #               <objURI>urn:ietf:params:xml:ns:keyrelay-1.0</objURI>
-    #             </svcs>
-    #           </login>
-    #         </command>
-    #       </epp>
-    #     XML
-    #     assert_difference 'EppSession.count' do
-    #       post '/epp/session/login', params: { frame: request_xml },
-    #            headers: { 'HTTP_COOKIE' => "session=#{new_session_id}" }
-    #     end
-    #     assert_epp_response :completed_successfully
-    #     session = EppSession.last
-    #     assert_equal new_session_id, session.session_id
-    #     assert_equal user, session.user
-    # end
+    def test_helper_test
+        request_xml = <<-XML
+        <?xml version="1.0" encoding="UTF-8" standalone="no"?>
+        <epp xmlns="https://epp.tld.ee/schema/epp-ee-1.0.xsd">
+          <hello/>
+        </epp>
+      XML
+  
+      get epp_hello_path, params: { frame: request_xml },
+          headers: { 'HTTP_COOKIE' => 'session=non-existent' }
+    end
 
     def test_visit_epp_logs_page
         visit admin_epp_logs_path
@@ -48,10 +25,10 @@ class AdminEppLogsIntegrationTest < ApplicationSystemTestCase
     end
 
     def test_show_epp_log_page
-        sign_out users(:admin)
-        sign_in users(:admin)
         visit admin_epp_logs_path
-        puts find(:xpath, "//body", match: :first).native
+        test_helper_test
+        visit admin_epp_logs_path
+
         find(:xpath, "//tbody/tr/td/a", match: :first).click
         assert_text 'Details'
     end
