541c2d769aa2931a8cfc25df97ac367c89422822
Oleg Hasjanov <oleg.hasjanov@eestiinternet.ee>
2021-01-22 10:47:31 +0200

    Contact test for new registrant after domain transfer

--- test/integration/epp/domain/transfer/request_test.rb
+++ test/integration/epp/domain/transfer/request_test.rb
@@ -12,6 +12,28 @@ class EppDomainTransferRequestTest < EppTestCase
     Setting.transfer_wait_time = @original_transfer_wait_time
   end
 
+  def test_new_contacts_should_be_created_after_transfer_domain
+    registrar_id = @domain.registrar.id
+    contacts_id_values = []
+
+    contact_id = Contact.find_by(registrar_id: registrar_id)
+
+    @domain.domain_contacts[0].update!(contact_id: contact_id.id)
+    @domain.domain_contacts[1].update!(contact_id: contact_id.id)
+    
+    @domain.domain_contacts.each do |contact|
+      contacts_id_values.push(contact.contact_id)
+    end
+
+    post epp_transfer_path, params: { frame: request_xml },
+           headers: { 'HTTP_COOKIE' => 'session=api_goodnames' }
+
+    @domain.reload
+
+    assert_epp_response :completed_successfully
+    assert_equal @domain.domain_contacts[0].contact_id, @domain.domain_contacts[1].contact_id
+  end
+
   def test_transfers_domain_at_once
     post epp_transfer_path, params: { frame: request_xml },
          headers: { 'HTTP_COOKIE' => 'session=api_goodnames' }
