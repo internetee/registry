4b388cc6714992670efccd72c2e9edba27ff3ced
Alex Sherman <yul.golem@gmail.com>
2021-01-25 15:42:42 +0500

    Add test for admin csv download

---
+++ test/system/admin_area/domains/csv_test.rb
@@ -0,0 +1,18 @@
+require 'application_system_test_case'
+
+class AdminAreaCsvTest < ApplicationSystemTestCase
+  setup do
+    sign_in users(:admin)
+  end
+
+  def test_downloads_domain_list_as_csv
+    search_params = {"valid_to_lteq"=>nil}
+    expected_csv = Domain.includes(:registrar, :registrant).search(search_params).result.to_csv
+
+    travel_to Time.zone.parse('2010-07-05 10:30')
+    visit admin_domains_url
+    click_link(class: 'glyphicon glyphicon-list-alt')
+    assert_equal "attachment; filename=\"domains.csv\"; filename*=UTF-8''domains.csv", response_headers['Content-Disposition']
+    assert_equal expected_csv, page.body
+  end
+end
