486fceca9c99ae04c5475199e4288781da5d396a
Oleg Hasjanov <oleg.hasjanov@eestiinternet.ee>
2021-01-29 12:19:14 +0200

    changed tabs

--- test/integration/admin_area/account_activities_test.rb
+++ test/integration/admin_area/account_activities_test.rb
@@ -2,38 +2,38 @@ require 'test_helper'
 require 'application_system_test_case'
 
 class AdminAreaAccountActivitiesIntegrationTest < ApplicationSystemTestCase
-    # /admin/account_activities
-    setup do
-        sign_in users(:admin)
-        @original_default_language = Setting.default_language
-    end
-    
-    def test_show_account_activities_page
-        account_activities(:one).update(sum: "123.00")
-        visit admin_account_activities_path
-        assert_text 'Account activities'
-    end
+  # /admin/account_activities
+  setup do
+    sign_in users(:admin)
+    @original_default_language = Setting.default_language
+  end
+  
+  def test_show_account_activities_page
+    account_activities(:one).update(sum: "123.00")
+    visit admin_account_activities_path
+    assert_text 'Account activities'
+  end
 
-    def test_default_url_params
-        account_activities(:one).update(sum: "123.00")
-        visit admin_root_path
-        click_link_or_button 'Settings', match: :first
-        find(:xpath, "//ul/li/a[text()='Account activities']").click
-        
-        assert has_current_path?(admin_account_activities_path(created_after: 'today'))
-    end
+  def test_default_url_params
+    account_activities(:one).update(sum: "123.00")
+    visit admin_root_path
+    click_link_or_button 'Settings', match: :first
+    find(:xpath, "//ul/li/a[text()='Account activities']").click
+    
+    assert has_current_path?(admin_account_activities_path(created_after: 'today'))
+  end
 
-    def test_download_account_activity
-        now = Time.zone.parse('2010-07-05 08:00')
-        travel_to now
-        account_activities(:one).update(sum: "123.00")
-    
-        get admin_account_activities_path(format: :csv)
-    
-        assert_response :ok
-        assert_equal "text/csv", response.headers['Content-Type']
-        assert_equal %(attachment; filename="account_activities_#{Time.zone.now.to_formatted_s(:number)}.csv"; filename*=UTF-8''account_activities_#{Time.zone.now.to_formatted_s(:number)}.csv),
-                     response.headers['Content-Disposition']
-        assert_not_empty response.body
-    end
+  def test_download_account_activity
+    now = Time.zone.parse('2010-07-05 08:00')
+    travel_to now
+    account_activities(:one).update(sum: "123.00")
+
+    get admin_account_activities_path(format: :csv)
+
+    assert_response :ok
+    assert_equal "text/csv", response.headers['Content-Type']
+    assert_equal %(attachment; filename="account_activities_#{Time.zone.now.to_formatted_s(:number)}.csv"; filename*=UTF-8''account_activities_#{Time.zone.now.to_formatted_s(:number)}.csv),
+                  response.headers['Content-Disposition']
+    assert_not_empty response.body
+  end
 end
--- test/integration/admin_area/admin_users_test.rb
+++ test/integration/admin_area/admin_users_test.rb
@@ -2,103 +2,100 @@ require 'test_helper'
 require 'application_system_test_case'
 
 class AdminAreaAdminUsersIntegrationTest < JavaScriptApplicationSystemTestCase
-    include Devise::Test::IntegrationHelpers
-    include ActionView::Helpers::NumberHelper
+  include Devise::Test::IntegrationHelpers
+  include ActionView::Helpers::NumberHelper
 
-    setup do
-        WebMock.allow_net_connect!
-        @original_default_language = Setting.default_language
-        sign_in users(:admin)
-    end
+  setup do
+    WebMock.allow_net_connect!
+    @original_default_language = Setting.default_language
+    sign_in users(:admin)
+  end
 
-    # Helpers funcs
-    def createNewAdminUser(valid)
-        visit admin_admin_users_path
-        click_on 'New admin user'
+  def test_create_new_admin_user
+    createNewAdminUser(true)
+  end
 
-        fill_in 'Username', with: 'test_user_name'
-        # If valid=true creating valid user, if else, then with invalid data
-        if valid
-            fill_in 'Password', with: 'test_password'
-            fill_in 'Password confirmation', with: 'test_password'
-        else
-            fill_in 'Password', with: 'test_password'
-            fill_in 'Password confirmation', with: 'test_password2'
-        end
-        fill_in 'Identity code', with: '38903110313'
-        fill_in 'Email', with: 'oleg@tester.ee'
+  def test_create_with_invalid_data_new_admin_user
+    createNewAdminUser(false)
+  end
 
-        select 'Estonia', from: 'admin_user_country_code', match: :first
+  def test_edit_successfully_exist_record
+    createNewAdminUser(true)
 
-        select_element = find(:xpath, "/html/body/div[2]/form/div[2]/div/div[7]/div[2]/div/div[1]")
-        select_element.click
+    visit admin_admin_users_path
+    click_on 'test_user_name'
 
-        option_element = find(:xpath, "/html/body/div[2]/form/div[2]/div/div[7]/div[2]/div/div[2]/div/div[1]")
-        option_element.click
+    assert_text 'General'
+    click_on 'Edit'
 
-        click_on 'Save'
+    fill_in 'Password', with: 'test_password'
+    fill_in 'Password confirmation', with: 'test_password'
 
-        if valid
-            assert_text 'Record created'
-        else
-            assert_text 'Failed to create record'
-        end
-    end
+    click_on 'Save'
+    assert_text 'Record updated'
+  end
 
-    # Tests
-    # "/admin/admin_users"
-    def test_create_new_admin_user
-        createNewAdminUser(true)
-    end
+  def test_edit_exist_record_with_invalid_data
+    createNewAdminUser(true)
 
-    def test_create_with_invalid_data_new_admin_user
-        createNewAdminUser(false)
-    end
+    visit admin_admin_users_path
+    click_on 'test_user_name'
 
-    def test_edit_successfully_exist_record
-        createNewAdminUser(true)
+    assert_text 'General'
+    click_on 'Edit'
 
-        visit admin_admin_users_path
-        click_on 'test_user_name'
+    fill_in 'Password', with: 'test_password'
+    fill_in 'Password confirmation', with: 'test_password2'
 
-        assert_text 'General'
-        click_on 'Edit'
+    click_on 'Save'
+    assert_text 'Failed to update record'
+  end
 
+  def test_delete_exist_record
+    createNewAdminUser(true)
+
+    visit admin_admin_users_path
+    click_on 'test_user_name'
+    assert_text 'General'
+    click_on 'Delete'
+
+    page.driver.browser.switch_to.alert.accept
+
+    assert_text 'Record deleted'
+  end
+
+  private
+
+  def createNewAdminUser(valid)
+    visit admin_admin_users_path
+    click_on 'New admin user'
+
+    fill_in 'Username', with: 'test_user_name'
+    # If valid=true creating valid user, if else, then with invalid data
+    if valid
         fill_in 'Password', with: 'test_password'
         fill_in 'Password confirmation', with: 'test_password'
-
-        click_on 'Save'
-        assert_text 'Record updated'
-    end
-
-    def test_edit_exist_record_with_invalid_data
-        createNewAdminUser(true)
-
-        visit admin_admin_users_path
-        click_on 'test_user_name'
-
-        assert_text 'General'
-        click_on 'Edit'
-
+    else
         fill_in 'Password', with: 'test_password'
         fill_in 'Password confirmation', with: 'test_password2'
-
-        click_on 'Save'
-        assert_text 'Failed to update record'
     end
+    fill_in 'Identity code', with: '38903110313'
+    fill_in 'Email', with: 'oleg@tester.ee'
 
-    def test_delete_exist_record
-        createNewAdminUser(true)
+    select 'Estonia', from: 'admin_user_country_code', match: :first
 
-        visit admin_admin_users_path
-        click_on 'test_user_name'
+    select_element = find(:xpath, "/html/body/div[2]/form/div[2]/div/div[7]/div[2]/div/div[1]")
+    select_element.click
 
-        assert_text 'General'
-        click_on 'Delete'
+    option_element = find(:xpath, "/html/body/div[2]/form/div[2]/div/div[7]/div[2]/div/div[2]/div/div[1]")
+    option_element.click
 
-        # Accept to delete in modal window
-        page.driver.browser.switch_to.alert.accept
+    click_on 'Save'
 
-        assert_text 'Record deleted'
+    if valid
+        assert_text 'Record created'
+    else
+        assert_text 'Failed to create record'
     end
+  end
 end
