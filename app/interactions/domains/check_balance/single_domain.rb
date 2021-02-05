d1bb6fcc608056d53764222c9498dcf2a2e6b8ec
Karl Erik Õunapuu <karlerik@kreative.ee>
2021-02-05 16:22:54 +0200

    REPP: Fix domain renew

--- app/interactions/domains/check_balance/single_domain.rb
+++ app/interactions/domains/check_balance/single_domain.rb
@@ -9,15 +9,25 @@ module Domains
       string :unit
 
       def execute
-        return domain_pricelist.price.amount if domain_pricelist.try(:price)
+        if domain_pricelist.try(:price)
+          price = domain_pricelist.price.amount
+          return price if balance_ok?(price)
 
-        domain.add_epp_error(2104, nil, nil, I18n.t(:active_price_missing_for_this_operation))
-        errors.add(:domain, I18n.t(:active_price_missing_for_operation_with_domain,
-                                   domain: domain.name))
+          domain.add_epp_error(2104, nil, nil, I18n.t(:billing_failure_credit_balance_low))
+          errors.add(:domain, I18n.t(:billing_failure_credit_balance_low, domain: domain.name))
+        else
+          domain.add_epp_error(2104, nil, nil, I18n.t(:active_price_missing_for_this_operation))
+          errors.add(:domain, I18n.t(:active_price_missing_for_operation_with_domain, domain: domain.name))
+        end
+
         false
       end
 
       private
+
+      def balance_ok?(price)
+        domain.registrar.cash_account.balance >= price
+      end
 
       def domain_pricelist
         domain.pricelist(operation, period.try(:to_i), unit)
