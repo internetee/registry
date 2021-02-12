416390a3c41b91313c166a02b6fe11542458737a
Karl Erik Õunapuu <karlerik@kreative.ee>
2021-02-12 15:33:34 +0200

    Add detailed balance to REPP

--- app/controllers/repp/v1/accounts_controller.rb
+++ app/controllers/repp/v1/accounts_controller.rb
@@ -2,9 +2,37 @@ module Repp
   module V1
     class AccountsController < BaseController
       def balance
+        return activity if params[:detailed] == 'true'
+
         resp = { balance: current_user.registrar.cash_account.balance,
                  currency: current_user.registrar.cash_account.currency }
         render_success(data: resp)
+      end
+
+      def activity
+        resp = { balance: current_user.registrar.cash_account.balance,
+                 currency: current_user.registrar.cash_account.currency }
+        resp[:activities] = activities
+        render_success(data: resp)
+      end
+
+      def activities
+        bal = current_user.registrar.cash_account.balance
+        act = []
+        activities = current_user.registrar.cash_account.activities.order(created_at: :desc)
+        activities.each do |a|
+          act << {
+            created_at: a.created_at,
+            description: a.description,
+            type: a.activity_type == 'add_credit' ? 'credit' : 'debit',
+            sum: a.sum,
+            balance: bal,
+          }
+
+          bal = a.activity_type == 'add_credit' ? bal = bal + a.sum : bal - a.sum
+        end
+
+        act
       end
     end
   end
