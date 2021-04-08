3eecdd8d2987edb7216084b38bfa8ab26114fed0
Alex Sherman <yul.golem@gmail.com>
2021-04-08 13:42:20 +0500

    Add invalid email to FD status note

--- app/interactions/domains/cancel_force_delete/clear_force_delete_data.rb
+++ app/interactions/domains/cancel_force_delete/clear_force_delete_data.rb
@@ -5,6 +5,7 @@ module Domains
         domain.force_delete_data = nil
         domain.force_delete_date = nil
         domain.force_delete_start = nil
+        domain.status_notes[DomainStatus::FORCE_DELETE] = nil
         domain.save(validate: false)
       end
     end
