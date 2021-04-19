649d3559720fca46ae220306b2ac7c57b04634a4
Oleg Hasjanov <oleg.hasjanov@eestiinternet.ee>
2021-04-19 10:21:30 +0300

    added logging

--- app/jobs/bounced_emails_cleaner_job.rb
+++ app/jobs/bounced_emails_cleaner_job.rb
@@ -4,7 +4,16 @@ class BouncedEmailsCleanerJob < ApplicationJob
   def perform
     BouncedMailAddress.find_each do |bounce|
       count = Contact.where(email: bounce.email).count
-      bounce.destroy if count.zero?
+      if count.zero?
+        logger.info "#{bounce.inspect} ARE DELETED!"
+        bounce.destroy
+      end
     end
+  end
+
+  private
+
+  def logger
+    @logger ||= Logger.new(Rails.root.join('log', 'deleted_bounced_mails.log'))
   end
 end
