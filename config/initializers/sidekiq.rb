3b2f840ce294727512983155608279c43437d9cf
Alex Sherman <yul.golem@gmail.com>
2021-04-27 17:16:25 +0500

    Add redis_url to sidekiq initializer

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,1 +1,11 @@
 require 'sidekiq/web' # Require at the top of the initializer
+
+if ENV['redis_url'].present?
+  Sidekiq.configure_server do |config|
+    config.redis = { url: ENV['redis_url'] }
+  end
+
+  Sidekiq.configure_client do |config|
+    config.redis = { url: ENV['redis_url'] }
+  end
+end
