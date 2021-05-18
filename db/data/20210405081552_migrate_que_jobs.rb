8b398d74f2eecbf3c654619e30769e0097916495
Alex Sherman <yul.golem@gmail.com>
2021-05-18 13:19:10 +0500

    Move job migration to rake task

--- db/data/20210405081552_migrate_que_jobs.rb
+++ db/data/20210405081552_migrate_que_jobs.rb
@@ -1,25 +1,25 @@
 class MigrateQueJobs < ActiveRecord::Migration[6.0]
   def up
-    QueJob.all.each do |job|
-      if skip_condition(job)
-        logger.info "Skipped Que job migration: #{job.inspect}"
-      else
-        args = job.args
-
-        job.job_class.constantize.set(wait_until: job.run_at).perform_later(args)
-      end
-    end
+    # QueJob.all.each do |job|
+    #   if skip_condition(job)
+    #     logger.info "Skipped Que job migration: #{job.inspect}"
+    #   else
+    #     args = job.args
+    #
+    #     job.job_class.constantize.set(wait_until: job.run_at).perform_later(args)
+    #   end
+    # end
   end
 
   def down
     # raise ActiveRecord::IrreversibleMigration
   end
 
-  def logger
-    @logger ||= Logger.new(Rails.root.join('log', 'que_to_sidekiq_migration.log'))
-  end
-
-  def skip_condition(job)
-    job.last_error.present? || !(job.job_class.constantize < ApplicationJob)
-  end
+  # def logger
+  #   @logger ||= Logger.new(Rails.root.join('log', 'que_to_sidekiq_migration.log'))
+  # end
+  #
+  # def skip_condition(job)
+  #   job.last_error.present? || !(job.job_class.constantize < ApplicationJob)
+  # end
 end
