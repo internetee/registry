8063d538a637188abcdefd7abd80ca11be5f2789
Oleg Hasjanov <oleg.phenomenon@gmail.com>
2021-07-09 16:28:48 +0300

    refactoring

--- app/jobs/migrate_before_force_delete_statuses_job.rb
+++ app/jobs/migrate_before_force_delete_statuses_job.rb
@@ -1,9 +1,11 @@
 class MigrateBeforeForceDeleteStatusesJob < ApplicationJob
   def perform
     domains = Domain.where.not(statuses_before_force_delete: nil)
-    domains.each do |domain|
-      domain.force_delete_domain_statuses_history = domain.statuses_before_force_delete
-      domain.save
+    domains.find_in_batches do |domain_batches|
+      domain_batches.each do |domain|
+        domain.force_delete_domain_statuses_history = domain.statuses_before_force_delete
+        domain.save
+      end
     end
   end
 end
--- app/jobs/migrate_statuses_to_domain_history_job.rb
+++ app/jobs/migrate_statuses_to_domain_history_job.rb
@@ -1,6 +1,6 @@
 class MigrateStatusesToDomainHistoryJob < ApplicationJob
   def perform
-    domains = Domain.all.select { |d| !d.locked_by_registrant? }
+    domains = Domain.where(locked_by_registrant_at: nil)
     domains.each do |domain|
       domain.admin_store_statuses_history = domain.statuses
       domain.save
