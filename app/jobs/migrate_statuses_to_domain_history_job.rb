0950361e0a03b7c62ac367bc8f27d984406f0986
Oleg Hasjanov <oleg.phenomenon@gmail.com>
2021-07-13 12:04:54 +0300

    added stdout to log file

--- app/jobs/migrate_statuses_to_domain_history_job.rb
+++ app/jobs/migrate_statuses_to_domain_history_job.rb
@@ -1,9 +1,37 @@
 class MigrateStatusesToDomainHistoryJob < ApplicationJob
   def perform
+    logger.info 'Ran MigrateStatusesToDomainHistoryJob!'
+
     domains = Domain.where(locked_by_registrant_at: nil)
-    domains.each do |domain|
-      domain.admin_store_statuses_history = domain.statuses
-      domain.save
+    logger.info "Total domains are #{domains.count}"
+
+    interate_domain_in_batches(domains)
+  end
+
+  private
+
+  def interate_domain_in_batches(domains)
+    count = 0
+
+    domains.find_in_batches do |domain_batches|
+      count += domain_batches.count
+      logger.info "Proccesing #{count} domains of #{domains.count}"
+      domain_batches.each do |domain|
+        migrate_data_to_admin_store_field(domain)
+      end
     end
+  end
+
+  def migrate_data_to_admin_store_field(domain)
+    domain.admin_store_statuses_history = domain.statuses
+    domain.save
+  rescue StandardError => e
+    logger.warn "#{domain.name} crashed!"
+    logger.warn e.to_s
+    raise e
+  end
+
+  def logger
+    @logger ||= Logger.new(Rails.root.join('log', 'migrate_statuses_to_domain_history.log'))
   end
 end
