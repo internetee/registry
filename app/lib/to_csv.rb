67d7a307a18b5a841e903e013bf0003f02e3286c
dinsmol <dinar184@mail.ru>
2021-08-13 12:45:54 +0300

    replaced each to find_each

--- app/lib/to_csv.rb
+++ app/lib/to_csv.rb
@@ -2,7 +2,7 @@ module ToCsv
   def to_csv
     CSV.generate do |csv|
       csv << column_names
-      all.each do |item|
+      all.find_each do |item|
         csv << item.attributes.values_at(*column_names)
       end
     end
