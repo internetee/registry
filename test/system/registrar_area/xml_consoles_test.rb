2f349658ae74857c4330492930f26c751dd07755
dinsmol <dinar184@mail.ru>
2021-10-07 14:53:52 +0300

    added tests

--- test/system/registrar_area/xml_consoles_test.rb
+++ test/system/registrar_area/xml_consoles_test.rb
@@ -14,7 +14,97 @@ class RegistrarAreaXmlConsolesTest < ApplicationSystemTestCase
     assert el.text.include? 'CONNECTION ERROR - Is the EPP server running?'
   end
 
+  def test_update_dnskey
+    @domain = domains(:shop)
+    visit registrar_xml_console_path
+    fill_in 'payload', with: schema_dnskey_add
+    click_on 'Send EPP Request'
+
+    el = page.find('.CodeRay', visible: :all)
+    assert el.text.include? 'Command completed successfully'
+
+    @domain.statuses << DomainStatus::SERVER_UPDATE_PROHIBITED
+    assert @domain.statuses.include? DomainStatus::SERVER_UPDATE_PROHIBITED
+
+    ENV['obj_and_extensions_prohibited'] = 'true'
+    assert Feature.obj_and_extensions_statuses_enabled?
+
+    visit registrar_xml_console_path
+    fill_in 'payload', with: schema_dnskey_update
+    click_on 'Send EPP Request'
+
+    el = page.find('.CodeRay', visible: :all)
+    assert el.text.include? 'Command completed successfully'
+  end
+
   private
+
+  def schema_dnskey_add
+    @dnskey = dnskeys(:one)
+    <<~XML
+<?xml version="1.0" encoding="utf-8"?>
+<epp xmlns="https://epp.tld.ee/schema/epp-ee-1.0.xsd" xmlns:domain="https://epp.tld.ee/schema/domain-ee-1.2.xsd" xmlns:secDNS="urn:ietf:params:xml:ns:secDNS-1.1" xmlns:eis="https://epp.tld.ee/schema/eis-1.0.xsd" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="https://epp.tld.ee/schema/epp-ee-1.0.xsd epp-ee-1.0.xsd                          https://epp.tld.ee/schema/domain-ee-1.2.xsd domain-ee-1.2.xsd                          urn:ietf:params:xml:ns:secDNS-1.1 secDNS-1.1.xsd                          https://epp.tld.ee/schema/eis-1.0.xsd eis-1.0.xsd">
+    <command>
+        <update>
+            <domain:update>
+                <domain:name>shop.test</domain:name>
+            </domain:update>
+        </update>
+        <extension>
+                <secDNS:update>
+                    <secDNS:add>
+                        <secDNS:keyData>
+                            <secDNS:flags>#{@dnskey.flags}</secDNS:flags>
+                            <secDNS:protocol>#{@dnskey.protocol}</secDNS:protocol>
+                            <secDNS:alg>#{@dnskey.alg}</secDNS:alg>
+                            <secDNS:pubKey>#{@dnskey.public_key}</secDNS:pubKey>
+                        </secDNS:keyData>
+                    </secDNS:add>
+                </secDNS:update>
+        </extension>
+    <clTRID>0.04946500 1632965705</clTRID>
+    </command>
+</epp>
+    XML
+  end
+
+  def schema_dnskey_update
+    @dnskey = dnskeys(:one)
+    @dnskeynew = dnskeys(:two)
+    <<~XML
+<?xml version="1.0" encoding="utf-8"?>
+<epp xmlns="https://epp.tld.ee/schema/epp-ee-1.0.xsd" xmlns:domain="https://epp.tld.ee/schema/domain-ee-1.2.xsd" xmlns:secDNS="urn:ietf:params:xml:ns:secDNS-1.1" xmlns:eis="https://epp.tld.ee/schema/eis-1.0.xsd" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="https://epp.tld.ee/schema/epp-ee-1.0.xsd epp-ee-1.0.xsd                          https://epp.tld.ee/schema/domain-ee-1.2.xsd domain-ee-1.2.xsd                          urn:ietf:params:xml:ns:secDNS-1.1 secDNS-1.1.xsd                          https://epp.tld.ee/schema/eis-1.0.xsd eis-1.0.xsd">
+    <command>
+        <update>
+            <domain:update>
+                <domain:name>shop.test</domain:name>
+            </domain:update>
+        </update>
+        <extension>
+                <secDNS:update>
+                    <secDNS:rem>         \n
+                        <secDNS:keyData>
+                            <secDNS:flags>#{@dnskey.flags}</secDNS:flags>
+                            <secDNS:protocol>#{@dnskey.protocol}</secDNS:protocol>
+                            <secDNS:alg>#{@dnskey.alg}</secDNS:alg>
+                            <secDNS:pubKey>#{@dnskey.public_key}</secDNS:pubKey>
+                        </secDNS:keyData>         \n
+                    </secDNS:rem>
+                    <secDNS:add>         \n
+                        <secDNS:keyData>
+                            <secDNS:flags>#{@dnskeynew.flags}</secDNS:flags>
+                            <secDNS:protocol>#{@dnskeynew.protocol}</secDNS:protocol>
+                            <secDNS:alg>#{@dnskeynew.alg}</secDNS:alg>
+                            <secDNS:pubKey>#{@dnskeynew.public_key}</secDNS:pubKey>
+                        </secDNS:keyData>         \n
+                    </secDNS:add>
+                </secDNS:update>
+        </extension>
+    <clTRID>0.04946500 1632965705</clTRID>
+    </command>
+</epp>
+    XML
+  end
 
   def schema_example
     <<~XML
