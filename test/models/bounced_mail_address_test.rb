1048fbdd35fcaaa4dd788cd7d93a9499305ecd6e
olegphenomenon <oleg.hasjanov@eestiinternet.ee>
2021-10-15 14:55:49 +0300

    refactored tests, changed condition for validate email data

--- test/models/bounced_mail_address_test.rb
+++ test/models/bounced_mail_address_test.rb
@@ -136,7 +136,8 @@ class BouncedMailAddressTest < ActiveSupport::TestCase
     BouncedMailAddress.record(sns_bounce_payload)
     bounced_mail = BouncedMailAddress.last
     registrant = domains(:shop).registrant
-
+    registrant.verify_email(check_level: 'smtp')
+    
     assert_equal registrant.email, bounced_mail.email
     assert registrant.email_verification_failed?
   end
