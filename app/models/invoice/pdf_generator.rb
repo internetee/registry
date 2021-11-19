5189d8788722c380682c84058f779a5ad33b9e5e
dinsmol <dinar184@mail.ru>
2021-11-19 09:30:30 +0300

    Fix codeclimate errors

--- app/models/invoice/pdf_generator.rb
+++ app/models/invoice/pdf_generator.rb
@@ -26,22 +26,14 @@ class Invoice
       return unless @invoice.qr_enabled?
 
       qrcode = RQRCode::QRCode.new(@invoice.linkpay_url)
-
-      # NOTE: showing with default options specified explicitly
       png = qrcode.as_png(
-        bit_depth: 1,
-        border_modules: 4,
         color_mode: ChunkyPNG::COLOR_GRAYSCALE,
         color: 'black',
-        file: nil,
         fill: 'white',
-        module_px_size: 6,
-        resize_exactly_to: false,
-        resize_gte_to: false,
         size: 240
       )
 
-      path = "#{Rails.root}/public/#{@invoice.number}.png"
+      path = Rails.root.join("public/#{@invoice.number}.png")
       IO.binwrite(path, png.to_s)
     end
   end
