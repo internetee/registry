fa5203d0a1292d937a0c81dec04a545ffb37d2bb
dinsmol <dinar184@mail.ru>
2021-12-15 17:31:53 +0300

    Fix domain statuses logic

--- app/models/domain.rb
+++ app/models/domain.rb
@@ -121,8 +121,8 @@ class Domain < ApplicationRecord
   validate :status_is_consistant
   def status_is_consistant
     has_error = (hold_status? && statuses.include?(DomainStatus::SERVER_MANUAL_INZONE))
-    if !has_error && (statuses & DELETE_STATUSES).any?
-      has_error = statuses.include? DomainStatus::SERVER_DELETE_PROHIBITED unless locked_by_registrant?
+    if !has_error && statuses.include?(DomainStatus::PENDING_DELETE)
+      has_error = statuses.include? DomainStatus::SERVER_DELETE_PROHIBITED
     end
     errors.add(:domains, I18n.t(:object_status_prohibits_operation)) if has_error
   end
