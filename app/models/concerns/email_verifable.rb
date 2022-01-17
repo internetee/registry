b72a77600b7cc73472a4d5be84d311f9fe9a0d22
dinsmol <dinar184@mail.ru>
2022-01-17 10:15:57 +0300

    Add debug info

--- app/models/concerns/email_verifable.rb
+++ app/models/concerns/email_verifable.rb
@@ -17,6 +17,7 @@ module EmailVerifable
 
   def need_to_start_force_delete?
     flag = false
+    puts "validation_evenrs: #{validation_events}"
     ValidationEvent::INVALID_EVENTS_COUNT_BY_LEVEL.each do |level, count|
       if validation_events.count >= count && validate_email_data(level: level, count: count)
         flag = true
