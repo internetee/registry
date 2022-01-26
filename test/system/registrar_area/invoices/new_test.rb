a7416a6e5c7afc5320bd1e5a369470cce7c05c1d
olegphenomenon <oleg.hasjanov@eestiinternet.ee>
2022-01-26 13:24:58 +0200

    added link generator into admin part

--- test/system/registrar_area/invoices/new_test.rb
+++ test/system/registrar_area/invoices/new_test.rb
@@ -17,6 +17,17 @@ class NewInvoiceTest < ApplicationSystemTestCase
   end
 
   def test_create_new_invoice_with_positive_amount
+    invoice_n = Invoice.order(number: :desc).last.number
+    stub_request(:post, "http://eis_billing_system:3000/api/v1/invoice_generator/invoice_number_generator").
+      with(
+        headers: {
+              'Accept'=>'Bearer WA9UvDmzR9UcE5rLqpWravPQtdS8eDMAIynzGdSOTw==--9ZShwwij3qmLeuMJ--NE96w2PnfpfyIuuNzDJTGw==',
+              'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+              'Authorization'=>'Bearer foobar',
+              'Content-Type'=>'application/json',
+              'User-Agent'=>'Ruby'
+            }).
+      to_return(status: 200, body: "{\"invoice_number\":\"#{invoice_n + 3}\"}", headers: {})
     visit registrar_invoices_path
     click_link_or_button 'Add deposit'
     fill_in 'Amount', with: '200.00'
@@ -27,12 +38,23 @@ class NewInvoiceTest < ApplicationSystemTestCase
     end
 
     assert_text 'Please pay the following invoice'
-    assert_text 'Invoice no. 131050'
+    assert_text "Invoice no. #{invoice_n + 3}"
     assert_text 'Subtotal 200,00 €'
     assert_text 'Pay invoice'
   end
 
   def test_create_new_invoice_with_comma_in_number
+    invoice_n = Invoice.order(number: :desc).last.number
+    stub_request(:post, "http://eis_billing_system:3000/api/v1/invoice_generator/invoice_number_generator").
+      with(
+        headers: {
+              'Accept'=>'Bearer WA9UvDmzR9UcE5rLqpWravPQtdS8eDMAIynzGdSOTw==--9ZShwwij3qmLeuMJ--NE96w2PnfpfyIuuNzDJTGw==',
+              'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+              'Authorization'=>'Bearer foobar',
+              'Content-Type'=>'application/json',
+              'User-Agent'=>'Ruby'
+            }).
+      to_return(status: 200, body: "{\"invoice_number\":\"#{invoice_n + 3}\"}", headers: {})
     visit registrar_invoices_path
     click_link_or_button 'Add deposit'
     fill_in 'Amount', with: '200,00'
@@ -43,7 +65,7 @@ class NewInvoiceTest < ApplicationSystemTestCase
     end
 
     assert_text 'Please pay the following invoice'
-    assert_text 'Invoice no. 131050'
+    assert_text "Invoice no. #{invoice_n + 3}"
     assert_text 'Subtotal 200,00 €'
     assert_text 'Pay invoice'
   end
