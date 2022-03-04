479f14dbd46455145a1ad6f4077afa34ad81d2a4
olegphenomenon <oleg.hasjanov@eestiinternet.ee>
2022-03-04 12:45:27 +0200

    updated tests

---
+++ test/services/send_data_to_directo_test.rb
@@ -0,0 +1,15 @@
+require 'test_helper'
+
+class SendDataToDirectoTest < ActiveSupport::TestCase
+  setup do
+    Spy.on_instance_method(EisBilling::BaseController, :authorized).and_return(true)
+  end
+
+  def test_should_send_data_to_billing_directo
+    stub_request(:post, "http://eis_billing_system:3000/api/v1/directo/directo").
+    to_return(status: 200, body: "ok", headers: {})
+
+    res = EisBilling::SendDataToDirecto.send_request(object_data: [], monthly: true, dry: true)
+    assert_equal res.body, "ok"
+  end
+end
