9c6876d12191130c7fcc85e04b53de4bace52bef
Thiago Youssef <thiagodyoussef@gmail.com>
2022-04-06 11:58:22 +0300

    Remove `que_jobs` table

---
+++ db/migrate/20220406085500_remove_que_jobs.rb
@@ -0,0 +1,5 @@
+class RemoveQueJobs < ActiveRecord::Migration[6.1]
+  def change
+    drop_table :que_jobs
+  end
+end
--- db/structure.sql
+++ db/structure.sql
@@ -2306,48 +2306,6 @@ ALTER SEQUENCE public.prices_id_seq OWNED BY public.prices.id;
 
 
 --
--- Name: que_jobs; Type: TABLE; Schema: public; Owner: -
---
-
-CREATE TABLE public.que_jobs (
-    priority smallint DEFAULT 100 NOT NULL,
-    run_at timestamp with time zone DEFAULT now() NOT NULL,
-    job_id bigint NOT NULL,
-    job_class text NOT NULL,
-    args json DEFAULT '[]'::json NOT NULL,
-    error_count integer DEFAULT 0 NOT NULL,
-    last_error text,
-    queue text DEFAULT ''::text NOT NULL
-);
-
-
---
--- Name: TABLE que_jobs; Type: COMMENT; Schema: public; Owner: -
---
-
-COMMENT ON TABLE public.que_jobs IS '3';
-
-
---
--- Name: que_jobs_job_id_seq; Type: SEQUENCE; Schema: public; Owner: -
---
-
-CREATE SEQUENCE public.que_jobs_job_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
---
--- Name: que_jobs_job_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
---
-
-ALTER SEQUENCE public.que_jobs_job_id_seq OWNED BY public.que_jobs.job_id;
-
-
---
 -- Name: registrant_verifications; Type: TABLE; Schema: public; Owner: -
 --
 
@@ -3178,13 +3136,6 @@ ALTER TABLE ONLY public.prices ALTER COLUMN id SET DEFAULT nextval('public.price
 
 
 --
--- Name: que_jobs job_id; Type: DEFAULT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.que_jobs ALTER COLUMN job_id SET DEFAULT nextval('public.que_jobs_job_id_seq'::regclass);
-
-
---
 -- Name: registrant_verifications id; Type: DEFAULT; Schema: public; Owner: -
 --
 
@@ -3699,14 +3650,6 @@ ALTER TABLE ONLY public.payment_orders
 
 ALTER TABLE ONLY public.prices
     ADD CONSTRAINT prices_pkey PRIMARY KEY (id);
-
-
---
--- Name: que_jobs que_jobs_pkey; Type: CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.que_jobs
-    ADD CONSTRAINT que_jobs_pkey PRIMARY KEY (queue, priority, run_at, job_id);
 
 
 --
@@ -5281,6 +5224,7 @@ INSERT INTO "schema_migrations" (version) VALUES
 ('20220106123143'),
 ('20220113201642'),
 ('20220113220809'),
-('20220316140727');
+('20220316140727'),
+('20220406085500');
 
 
