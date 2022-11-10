0a77011bdb86e774ca354b57c9f8f5f71b8e750b
Sergei Tsoganov <sergei@Sergeis-MacBook-Pro.local>
2022-11-10 17:59:39 +0200

    Fixed test

--- lib/serializers/repp/domain.rb
+++ lib/serializers/repp/domain.rb
@@ -64,7 +64,7 @@ module Serializers
           status_note = status_notes[status.to_s].presence || ''
           statuses_with_notes.merge!({ "#{status}": status_note })
         end
-        statuses_with_notes
+        statuses_with_notes.with_indifferent_access
       end
 
       private
