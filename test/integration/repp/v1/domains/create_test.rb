2272e9ee42a8879585f3d4b8666493ee1f7addd1
Oleg Hasjanov <oleg.phenomenon@gmail.com>
2023-10-19 14:22:55 +0300

    added tests for epp and repp request domain cration

--- test/integration/repp/v1/domains/create_test.rb
+++ test/integration/repp/v1/domains/create_test.rb
@@ -4,6 +4,7 @@ class ReppV1DomainsCreateTest < ActionDispatch::IntegrationTest
   def setup
     @user = users(:api_bestnames)
     @domain = domains(:shop)
+    @bsa_domain = bsa_protected_domains(:one)
     token = Base64.encode64("#{@user.username}:#{@user.plain_text_password}")
     token = "Basic #{token}"
 
@@ -133,5 +134,73 @@ class ReppV1DomainsCreateTest < ActionDispatch::IntegrationTest
 
     assert @user.registrar.domains.find_by(name: 'domeener.test').present?
     assert_equal 'ABADIATS', @user.registrar.domains.find_by(name: 'domeener.test').transfer_code
+  end
+
+  def test_domain_cannnot_be_created_if_it_in_bsa_protected_list
+    @auth_headers['Content-Type'] = 'application/json'
+    contact = contacts(:john)
+
+    payload = {
+      domain: {
+        name: @bsa_domain.domain_name,
+        registrant: contact.code,
+        period: 1,
+        period_unit: 'y'
+      }
+    }
+
+    post "/repp/v1/domains", headers: @auth_headers, params: payload.to_json
+    json = JSON.parse(response.body, symbolize_names: true)
+    assert_response :bad_request
+    assert_equal 2003, json[:code]
+    assert_equal 'Required parameter missing; reserved>pw element required for reserved domains', json[:message]
+
+    refute @user.registrar.domains.find_by(name: @bsa_domain.domain_name).present?
+  end
+
+  def test_bsa_protected_domain_can_be_created_with_valid_registration_code
+    @auth_headers['Content-Type'] = 'application/json'
+    contact = contacts(:john)
+
+    payload = {
+      domain: {
+        name: @bsa_domain.domain_name,
+        registrant: contact.code,
+        reserved_pw: @bsa_domain.registration_code,
+        period: 1,
+        period_unit: 'y'
+      }
+    }
+
+    post "/repp/v1/domains", headers: @auth_headers, params: payload.to_json
+    json = JSON.parse(response.body, symbolize_names: true)
+    assert_response :ok
+    assert_equal 1000, json[:code]
+    assert_equal 'Command completed successfully', json[:message]
+
+    assert @user.registrar.domains.find_by(name: @bsa_domain.domain_name).present?
+  end
+
+  def test_bsa_protected_domain_cannot_be_created_with_invalid_registration_code
+    @auth_headers['Content-Type'] = 'application/json'
+    contact = contacts(:john)
+
+    payload = {
+      domain: {
+        name: @bsa_domain.domain_name,
+        registrant: contact.code,
+        reserved_pw: 'invalid_registration_code',
+        period: 1,
+        period_unit: 'y'
+      }
+    }
+
+    post "/repp/v1/domains", headers: @auth_headers, params: payload.to_json
+    json = JSON.parse(response.body, symbolize_names: true)
+    assert_response :bad_request
+    assert_equal 2202, json[:code]
+    assert_equal 'Invalid authorization information; invalid reserved>pw value', json[:message]
+
+    refute @user.registrar.domains.find_by(name: @bsa_domain.domain_name).present?
   end
 end
