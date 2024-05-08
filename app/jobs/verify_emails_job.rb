97c51f9024a678b13bce78d89d1edb2f89a8c418
tsoganov <sergei.tsoganov@ut.ee>
2024-05-08 09:59:44 +0300

    Refactored verify_emails_job

--- app/jobs/verify_emails_job.rb
+++ app/jobs/verify_emails_job.rb
@@ -2,21 +2,27 @@ class VerifyEmailsJob < ApplicationJob
   discard_on StandardError
 
   def perform(email:, check_level: 'mx', force: false)
-    contact = Contact.find_by(email: email)
+    contact = fetch_contact(email)
+    return unless contact && need_to_verify?(contact, force)
 
-    return logger.info "Contact #{email} not found!" if contact.nil?
-
-    return unless need_to_verify?(contact, force)
-
-    validate_check_level(check_level)
-
-    logger.info "Trying to verify contact email #{email} with check_level #{check_level}"
-    contact.verify_email(check_level: check_level)
+    verify_contact_email(contact, check_level)
   rescue StandardError => e
     handle_error(e)
   end
 
   private
+
+  def fetch_contact(email)
+    contact = Contact.find_by(email: email)
+    logger.info "Contact #{email} not found!" unless contact
+    contact
+  end
+
+  def verify_contact_email(contact, check_level)
+    validate_check_level(check_level)
+    logger.info "Trying to verify contact email #{contact.email} with check_level #{check_level}"
+    contact.verify_email(check_level: check_level)
+  end
 
   def validate_check_level(check_level)
     return if valid_check_levels.include? check_level
