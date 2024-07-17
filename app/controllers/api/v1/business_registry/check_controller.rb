7d7fd34bd643fda9de79bfcdb7ada751d37af9c3
oleghasjanov <oleg.phenomenon@gmail.com>
2024-07-17 14:42:18 +0300

    added name checker and availability control for domain names

--- app/controllers/api/v1/business_registry/check_controller.rb
+++ app/controllers/api/v1/business_registry/check_controller.rb
@@ -3,17 +3,30 @@ module Api
     module BusinessRegistry
       class CheckController < ::Api::V1::BaseController
         before_action :set_cors_header
+        before_action :validate_organization_name
         # before_action :authenticate
 
         def show
-          name = params[:name]
-          render json: { message: "OK - #{name}" }, status: :ok
+          name = params[:organization_name]
+          all_variants = ::BusinessRegistry::DomainNameGeneratorService.generate(name)
+          available_variants = ::BusinessRegistry::DomainAvailabilityChecker.filter_available(all_variants)
+          render json: { variants: available_variants }, status: :ok
         end
 
         private
 
         def set_cors_header
-          response.headers['Access-Control-Allow-Origin'] = request.headers['Origin']
+          allowed_origins = ENV['ALLOWED_ORIGINS'].split(',')
+          if allowed_origins.include?(request.headers['Origin'])
+            response.headers['Access-Control-Allow-Origin'] = request.headers['Origin']
+          end
+        end
+
+        def validate_organization_name
+          name = params[:organization_name]
+          if name.blank? || name.length > 100 || !name.match?(/\A[\p{L}\p{N}\s\-]+\z/)
+            render json: { error: 'Invalid organization name' }, status: :bad_request
+          end
         end
       end
     end
--- test/integration/api/business_registry/check_test.rb
+++ test/integration/api/business_registry/check_test.rb
@@ -1,17 +1,50 @@
 require 'test_helper'
 
 class CheckTest < ApplicationIntegrationTest
+  fixtures :reserved_domains
+
   def setup
     super
-
-    # @contact = contacts(:john)
+    ENV['ALLOWED_ORIGINS'] = 'http://example.com,http://test.com'
   end
 
-  def test_return_code_that_all_ok
-    get '/api/v1/business_registry/check/common.ee'
+  def test_return_list_of_available_organization_domain_names
+    get '/api/v1/business_registry/check?organization_name=Company Name AS', headers: { 'Origin' => 'http://example.com' }
     json = JSON.parse(response.body, symbolize_names: true)
 
-    puts(json)
-    # assert_equal json[:errors], 'Contact not found'
+    assert_response :success
+    assert_equal 'http://example.com', response.headers['Access-Control-Allow-Origin']
+    refute_includes json[:variants], 'company-name.test'
+    assert_includes json[:variants], 'companyname'
+    assert_includes json[:variants], 'company-name'
+    assert_includes json[:variants], 'company_name'
+    assert_includes json[:variants], "companyname#{Time.current.year}"
+    refute_includes json[:variants].join, 'as'
+  end
+
+  def test_single_word_company_name
+    get '/api/v1/business_registry/check?organization_name=Reserved', headers: { 'Origin' => 'http://test.com' }
+    json = JSON.parse(response.body, symbolize_names: true)
+
+    assert_response :success
+    assert_equal 'http://test.com', response.headers['Access-Control-Allow-Origin']
+    assert_includes json[:variants], 'reserved'
+    refute_includes json[:variants], 'reserved.test'
+    assert_includes json[:variants], "reserved#{Time.current.year}"
+  end
+
+  def test_invalid_organization_name
+    get '/api/v1/business_registry/check?organization_name=Invalid!@#Name', headers: { 'Origin' => 'http://example.com' }
+    json = JSON.parse(response.body, symbolize_names: true)
+
+    assert_response :bad_request
+    assert_equal 'Invalid organization name', json[:error]
+  end
+
+  def test_cors_with_disallowed_origin
+    get '/api/v1/business_registry/check?organization_name=Test', headers: { 'Origin' => 'http://malicious.com' }
+    
+    assert_response :success
+    assert_nil response.headers['Access-Control-Allow-Origin']
   end
 end
