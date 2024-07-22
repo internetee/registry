61c488a81a632d225baa8dd45dee68aabd8f84bc
oleghasjanov <oleg.phenomenon@gmail.com>
2024-07-22 14:33:05 +0300

    added refresh token endpoing, added token for reserved domains, added ip constraint, added origin constraint, added test

--- app/controllers/api/v1/business_registry/registration_code_controller.rb
+++ app/controllers/api/v1/business_registry/registration_code_controller.rb
@@ -3,17 +3,36 @@ module Api
     module BusinessRegistry
       class RegistrationCodeController < ::Api::V1::BaseController
         before_action :set_cors_header
-        # before_action :authenticate
+        before_action :find_reserved_domain
+        before_action :authenticate, only: [:show]
 
         def show
-          name = params[:name]
-          render json: { message: "OK - #{name} | Registration code: some code" }, status: :ok
+          if @reserved_domain.token_expired?
+            render json: { error: "Token expired" }, status: :unauthorized
+          else
+            render json: { name: @reserved_domain.name, registration_code: @reserved_domain.password }, status: :ok
+          end
         end
 
         private
 
         def set_cors_header
-          response.headers['Access-Control-Allow-Origin'] = request.headers['Origin']
+          allowed_origins = ENV['ALLOWED_ORIGINS'].split(',')
+          if allowed_origins.include?(request.headers['Origin'])
+            response.headers['Access-Control-Allow-Origin'] = request.headers['Origin']
+          else
+            render json: { error: "Unauthorized origin" }, status: :unauthorized
+          end
+        end
+
+        def find_reserved_domain
+          token = request.headers['Authorization']&.split(' ')&.last
+          @reserved_domain = ReservedDomain.find_by(access_token: token)
+          if @reserved_domain.nil?
+            render json: { error: "Invalid token" }, status: :unauthorized
+          elsif @reserved_domain.token_expired?
+            render json: { error: "Token expired" }, status: :unauthorized
+          end
         end
       end
     end
--- app/controllers/api/v1/business_registry/reserve_controller.rb
+++ app/controllers/api/v1/business_registry/reserve_controller.rb
@@ -3,18 +3,39 @@ module Api
     module BusinessRegistry
       class ReserveController < ::Api::V1::BaseController
         before_action :set_cors_header
-        # before_action :authenticate
+        before_action :validate_params
+        before_action :authenticate, only: [:create]
 
         def create
-          name = params[:name]
-          organization_code = params[:organization_code]
-          render json: { message: "OK - #{name} | #{organization_code}" }, status: :created
+          name = params[:name]&.downcase&.strip
+        
+          reserved_domain = ReservedDomain.find_or_initialize_by(name: name)
+        
+          if reserved_domain.persisted?
+            reserved_domain.refresh_token if reserved_domain.token_expired?
+            render json: { message: "Domain already reserved", token: reserved_domain.access_token }, status: :ok
+          elsif reserved_domain.save
+            render json: { message: "Domain reserved successfully", token: reserved_domain.access_token }, status: :created
+          else
+            render json: { error: "Failed to reserve domain", details: reserved_domain.errors.full_messages }, status: :unprocessable_entity
+          end
         end
 
         private
 
         def set_cors_header
-          response.headers['Access-Control-Allow-Origin'] = request.headers['Origin']
+          allowed_origins = ENV['ALLOWED_ORIGINS'].split(',')
+          if allowed_origins.include?(request.headers['Origin'])
+            response.headers['Access-Control-Allow-Origin'] = request.headers['Origin']
+          else
+            render json: { error: "Unauthorized origin" }, status: :unauthorized
+          end
+        end
+
+        def validate_params
+          if params[:name].blank?
+            render json: { error: "Missing required parameter: name" }, status: :bad_request
+          end
         end
       end
     end
