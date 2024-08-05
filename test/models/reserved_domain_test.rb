6a57fe71c2b5429ea16f9b836a9cce7c47deb964
oleghasjanov <oleg.phenomenon@gmail.com>
2024-08-05 14:15:01 +0300

    rollback

--- test/models/reserved_domain_test.rb
+++ test/models/reserved_domain_test.rb
@@ -13,23 +13,4 @@ class ReservedDomainTest < ActiveSupport::TestCase
     reserved_domain = ReservedDomain.new(password: 'reserved-001')
     assert_equal 'reserved-001', reserved_domain.registration_code
   end
-
-  def test_create_reserved_domain_with_punycode_name
-    reserved_domain = ReservedDomain.create(name: 'xn--4ca7aey.test')
-    assert reserved_domain.valid?
-  end
-
-  def test_create_reserved_domain_with_unicode_name
-    reserved_domain = ReservedDomain.create(name: 'õäöü.test')
-    assert reserved_domain.valid?
-  end
-
-  def test_cannot_create_the_same_domain_twicde_with_punycode_and_unicode
-    punycode_reserved_domain = ReservedDomain.new(name: 'xn--4ca7aey.test')
-    assert punycode_reserved_domain.valid?
-    punycode_reserved_domain.save && punycode_reserved_domain.reload
-
-    unicode_reserved_domain = ReservedDomain.new(name: 'õäöü.test')
-    assert_not unicode_reserved_domain.valid?
-  end
 end
