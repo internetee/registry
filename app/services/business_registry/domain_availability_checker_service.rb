31da4a07dd1b41f849ed41c136bb7d30fbbd7974
oleghasjanov <oleg.phenomenon@gmail.com>
2024-10-22 14:25:25 +0300

    fixed domain availability

--- app/services/business_registry/domain_availability_checker_service.rb
+++ app/services/business_registry/domain_availability_checker_service.rb
@@ -1,8 +1,11 @@
 module BusinessRegistry
   class DomainAvailabilityCheckerService
     def self.filter_available(domains)
-      reserved_domains = ReservedDomain.where(name: domains).pluck(:name)
-      domains - reserved_domains
+      result = Epp::Domain.check_availability(domains)
+      available_domains = domains.select { |domain| result.find { |r| r[:name] == domain }[:avail].positive? }
+
+      domains_in_reserved = ReservedDomain.where(name: available_domains).pluck(:name)
+      available_domains - domains_in_reserved
     end
   end
 end
