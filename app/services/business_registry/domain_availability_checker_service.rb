9e8e93063c1cbb8b128fc9e99f959da84d8b90cb
oleghasjanov <oleg.phenomenon@gmail.com>
2024-10-23 12:01:02 +0300

    fixed registration code, release and reserved endpoints. also update status paid flow

--- app/services/business_registry/domain_availability_checker_service.rb
+++ app/services/business_registry/domain_availability_checker_service.rb
@@ -7,5 +7,12 @@ module BusinessRegistry
       domains_in_reserved = ReservedDomain.where(name: available_domains).pluck(:name)
       available_domains - domains_in_reserved
     end
+
+    def self.is_domain_available?(domain)
+      result = Epp::Domain.check_availability([domain])
+      result = result.find { |r| r[:name] == domain }[:avail].positive?
+
+      result && ReservedDomain.find_by(name: domain).nil?
+    end
   end
 end
