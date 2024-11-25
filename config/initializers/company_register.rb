88d7689295fc27c47bf028511c18b3a23bea05dc
oleghasjanov <oleg.phenomenon@gmail.com>
2024-11-25 12:44:01 +0200

    changed ident to string

--- config/initializers/company_register.rb
+++ config/initializers/company_register.rb
@@ -2,6 +2,5 @@ CompanyRegister.configure do |config|
   config.username = ENV['company_register_username']
   config.password = ENV['company_register_password']
   config.cache_period = ENV['company_register_cache_period_days'].to_i.days
-  # config.test_mode = ENV['company_register_test_mode'] == 'true'
-  config.test_mode = false
+  config.test_mode = ENV['company_register_test_mode'] == 'true'
 end
