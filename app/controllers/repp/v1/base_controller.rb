2e9a4177c04aaf55bd1d2b6be7d79df2b10563ec
oleghasjanov <oleg.phenomenon@gmail.com>
2025-03-14 12:06:44 +0200

    added logs

--- app/controllers/repp/v1/base_controller.rb
+++ app/controllers/repp/v1/base_controller.rb
@@ -150,6 +150,11 @@ module Repp
         crt = request.headers['User-Certificate']
         com = request.headers['User-Certificate-CN']
 
+        Rails.logger.info '============== crts =============='
+        Rails.logger.info crt
+        Rails.logger.info com
+        Rails.logger.info '============== crts =============='
+
         return if @current_user.pki_ok?(crt, com, api: false)
 
         render_invalid_cert_response
