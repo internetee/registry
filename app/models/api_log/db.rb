6e61fb2727595bd38cce11676fc308e28e81a58d
tsoganov <sergei.tsoganov@ut.ee>
2025-04-01 14:32:35 +0300

    Test

--- app/models/api_log/db.rb
+++ app/models/api_log/db.rb
@@ -1,8 +1,7 @@
 module ApiLog
   class Db < ApplicationRecord
     self.abstract_class = true
-    # to_sym is needed because passing a string to ActiveRecord::Base.establish_connection
-    # for a configuration lookup is deprecated
+
     establish_connection "api_log_#{Rails.env}".to_sym
 
     def self.ransackable_associations(*)
