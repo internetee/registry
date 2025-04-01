42f73e8cfa2bef7ba82c7d29d31e021f51a9017d
tsoganov <sergei.tsoganov@ut.ee>
2025-04-01 15:54:22 +0300

    Disable db reading mode in test

--- test/services/report_runner_test.rb
+++ test/services/report_runner_test.rb
@@ -5,12 +5,6 @@ class ReportRunnerTest < ActiveSupport::TestCase
   setup do
     @report = reports(:one)
     @params = ActionController::Parameters.new({ '0' => { 'start_date' => '2025-01-01' } })
-
-    # Save the original connection configuration
-    @original_db_config = ActiveRecord::Base.connection_db_config
-
-    # Set up the connection pool for the reading role
-    ActiveRecord::Base.connects_to(database: { reading: :test, writing: :test })
   end
 
   def test_run_report_returns_completed_status_with_results_when_successful
