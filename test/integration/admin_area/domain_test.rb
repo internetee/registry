3994892126b6984cf9e541be52d459541e9beb6d
mmeest <martin.maasik.001@mail.ee>
2025-06-09 14:22:07 +0300

    added test for domain search for contacts with special chars

---
+++ test/integration/admin_area/domain_test.rb
@@ -0,0 +1,22 @@
+require 'test_helper'
+
+class Admin::DomainsControllerTest < ApplicationIntegrationTest
+  setup do
+    sign_in users(:admin)
+    @john = contacts(:john)
+    @john.update!(ident: '1234-1234')
+    @domain = domains(:shop)
+  end
+
+  def test_search_by_hyphenated_registrant_ident_should_fail
+    get admin_domains_path, params: { q: { registrant_ident_matches: '1234-1234' } }
+    assert_response :success
+    assert_not_includes @response.body, @domain.name, "Search should not find domain when searching for hyphenated registrant ident"
+  end
+
+  def test_search_by_hyphenated_contact_ident_should_fail
+    get admin_domains_path, params: { q: { contacts_ident_matches: '1234-1234' } }
+    assert_response :success
+    assert_not_includes @response.body, @domain.name, "Search should not find domain when searching for hyphenated contact ident"
+  end
+end 
