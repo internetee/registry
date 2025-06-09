56b21440e3efc982345c67cfd94e21e2c8edf4ee
mmeest <martin.maasik.001@mail.ee>
2025-06-09 14:34:15 +0300

    modified tests

--- test/integration/admin_area/domain_test.rb
+++ test/integration/admin_area/domain_test.rb
@@ -1,22 +1,25 @@
 require 'test_helper'
 
 class Admin::DomainsControllerTest < ApplicationIntegrationTest
-  setup do
-    sign_in users(:admin)
-    @john = contacts(:john)
-    @john.update!(ident: '1234-1234')
-    @domain = domains(:shop)
-  end
+    setup do
+        sign_in users(:admin)
+        @john = contacts(:john)
+        @john.update!(ident: '1234-1234') # ident with hyphen
+        @domain = domains(:shop)
+        @domain.update!(registrant: @john) # make sure the domain is linked to @john
+    end
 
-  def test_search_by_hyphenated_registrant_ident_should_fail
-    get admin_domains_path, params: { q: { registrant_ident_matches: '1234-1234' } }
-    assert_response :success
-    assert_not_includes @response.body, @domain.name, "Search should not find domain when searching for hyphenated registrant ident"
-  end
+    def test_search_by_hyphenated_registrant_ident_should_succeed
+        get admin_domains_path, params: { q: { registrant_ident_matches: '1234-1234' } }
+        assert_response :success
+        assert_includes @response.body, @domain.name,
+        "Search should find domain when searching by hyphenated registrant ident"
+    end
 
-  def test_search_by_hyphenated_contact_ident_should_fail
-    get admin_domains_path, params: { q: { contacts_ident_matches: '1234-1234' } }
-    assert_response :success
-    assert_not_includes @response.body, @domain.name, "Search should not find domain when searching for hyphenated contact ident"
-  end
-end 
+    def test_search_by_hyphenated_contact_ident_should_succeed
+        get admin_domains_path, params: { q: { contacts_ident_matches: '1234-1234' } }
+        assert_response :success
+        assert_includes @response.body, @domain.name,
+        "Search should find domain when searching by hyphenated contact ident"
+    end
+end
