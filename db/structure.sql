c256a1290a32256651c645909a3f8852cd6c76ac
oleghasjanov <oleg.phenomenon@gmail.com>
2025-06-27 12:27:59 +0300

    feat: Add system disclosed attributes for contact phone numbers
    - Add system_disclosed_attributes array column to contacts table
    - Update OrgRegistrantPhoneCheckerJob to manage both user and system disclosed attributes
    - Include system_disclosed_attributes in contact API serializer
    
    This change introduces a new way to track system-level disclosure of contact
    attributes, separate from user-defined disclosures. When a phone number matches
    the company register data, it will be marked as disclosed both in user and
    system attributes. This allows better tracking of which attributes were
    automatically disclosed by the system versus manually disclosed by users.

--- db/structure.sql
+++ db/structure.sql
@@ -697,7 +697,8 @@ CREATE TABLE public.contacts (
     company_register_status character varying,
     ident_request_sent_at timestamp without time zone,
     verified_at timestamp without time zone,
-    verification_id character varying
+    verification_id character varying,
+    system_disclosed_attributes character varying[] DEFAULT '{}'::character varying[]
 );
 
 
@@ -5728,6 +5729,7 @@ INSERT INTO "schema_migrations" (version) VALUES
 ('20250204094550'),
 ('20250219102811'),
 ('20250313122119'),
-('20250319104749');
+('20250319104749'),
+('20250627084536');
 
 
