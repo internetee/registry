b3dbabe32f9d6ac8a051095467bbfd46bedaa150
oleghasjanov <oleg.phenomenon@gmail.com>
2025-07-17 15:04:48 +0300

    feat(admin): notify registrar about domain status changes
    - Add inform_registrar_about_status_changes method to detect added and removed statuses after domain update.
    - Send notification to registrar with details of which statuses were set and which were removed.
    - Integrate notification logic into domain update action.

---
+++ test/integration/domain/domain_test.rb
@@ -0,0 +1,22 @@
+require 'test_helper'
+
+class DomainTest < ApplicationIntegrationTest
+
+  def setup
+    @domain = domains(:shop)
+    @admin = users(:admin)
+    sign_in @admin
+  end
+
+  def test_inform_registrar_about_status_changes
+    patch admin_domain_path(domains(:shop)), params: { domain: { statuses: [DomainStatus::PENDING_UPDATE,] } }
+
+    # Status OK is removed because, if:
+    # (statuses.length > 1) || !valid?
+    # then status OK is removed by manage_automatic_statuses method in domain.rb
+    assert_equal "Set on #{domains(:shop).name}: #{DomainStatus::PENDING_UPDATE}. Removed from #{domains(:shop).name}: #{DomainStatus::OK}", domains(:shop).registrar.notifications.last.text
+    
+    patch admin_domain_path(domains(:shop)), params: { domain: { statuses: [DomainStatus::PENDING_DELETE_CONFIRMATION] } }
+    assert_equal "Set on #{domains(:shop).name}: #{DomainStatus::PENDING_DELETE_CONFIRMATION}. Removed from #{domains(:shop).name}: #{DomainStatus::PENDING_UPDATE}", domains(:shop).registrar.notifications.last.text
+  end
+end
