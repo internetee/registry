c6d5d18e7fbd402d1ca37b327cc1fef033125fc5
tsoganov <sergei.tsoganov@ut.ee>
2025-10-23 13:02:04 +0300

    Corrected accreditation api

--- app/controllers/api/v1/accreditation_center/domains_controller.rb
+++ app/controllers/api/v1/accreditation_center/domains_controller.rb
@@ -3,14 +3,14 @@ require 'serializers/repp/domain'
 module Api
   module V1
     module AccreditationCenter
-      class DomainsController < ::Api::V1::AccreditationCenter::BaseController
+      class DomainsController < BaseController
         def show
           @domain = Domain.find_by(name: params[:name])
 
           if @domain
             render json: { code: 1000, domain: Serializers::Repp::Domain.new(@domain,
                                                                              sponsored: true).to_json },
-                   status: :found
+                   status: :ok
           else
             render json: { errors: 'Domain not found' }, status: :not_found
           end
