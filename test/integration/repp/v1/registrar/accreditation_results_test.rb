9f92ae0c95069626de4a284b2433199e95003e60
tsoganov <sergei.tsoganov@ut.ee>
2025-10-30 11:45:44 +0200

    Removed legacy accreditation code

--- test/integration/repp/v1/registrar/accreditation_results_test.rb
+++ test/integration/repp/v1/registrar/accreditation_results_test.rb
@@ -1,14 +1,9 @@
 require 'test_helper'
 
 class ReppV1AccreditationResultsTest < ActionDispatch::IntegrationTest
-  TEMPORARY_SECRET_KEY = ENV['accreditation_secret'].freeze
-
   def setup
     @user = users(:api_bestnames)
-
-    token = "Basic #{TEMPORARY_SECRET_KEY}"
-
-    @auth_headers = { 'Authorization' => token }
+    @accr_bot = users(:accr_bot)
     ENV['allow_accr_endspoints'] = 'true'
   end
 
@@ -17,23 +12,34 @@ class ReppV1AccreditationResultsTest < ActionDispatch::IntegrationTest
     super
   end
 
+  def test_should_only_allow_accr_bot_to_push_results
+    post '/repp/v1/registrar/accreditation/push_results',
+         headers: auth_headers(@user),
+         params: {accreditation_result: {username: @user.username, result: true} }
+    json = JSON.parse(response.body, symbolize_names: true)
+
+    assert_response :unauthorized
+    assert_equal json[:code], 2202
+    assert_equal json[:message], 'Only accr_bot can update accreditation results'
+  end
+
   def test_should_return_valid_response
     post '/repp/v1/registrar/accreditation/push_results',
-      headers: @auth_headers,
-      params: {accreditation_result: {username: @user.username, result: true} }
+         headers: auth_headers(@accr_bot),
+         params: {accreditation_result: { username: @user.username, result: true } }
     json = JSON.parse(response.body, symbolize_names: true)
 
     assert_response :ok
     assert_emails 2
     assert_equal json[:data][:user][:username], @user.username
-    assert_equal json[:data][:result], "true"
-    assert_equal json[:data][:message], "Accreditation info successfully added"
+    assert_equal json[:data][:result], 'true'
+    assert_equal json[:data][:message], 'Accreditation info successfully added'
   end
 
   def test_should_return_valid_response_invalid_authorization
     post '/repp/v1/registrar/accreditation/push_results',
-      headers: { 'Authorization' => 'Basic temporary-secret-ke'},
-      params: {accreditation_result: {username: @user.username, result: true} }
+         headers: { 'Authorization' => 'Basic temporary-secret-ke' },
+         params: { accreditation_result: { username: @user.username, result: true } }
     json = JSON.parse(response.body, symbolize_names: true)
 
     assert_response :unauthorized
@@ -45,14 +51,24 @@ class ReppV1AccreditationResultsTest < ActionDispatch::IntegrationTest
 
   def test_should_return_valid_response_record_exception
     post '/repp/v1/registrar/accreditation/push_results',
-      headers: @auth_headers,
-      params: {accreditation_result: { username: "chungachanga", result: true} }
+         headers: auth_headers(@accr_bot),
+         params: { accreditation_result: { username: 'chungachanga', result: true } }
     json = JSON.parse(response.body, symbolize_names: true)
 
-    assert_response :ok
+    assert_response :not_found
 
     assert_emails 0
     assert_equal json[:code], 2303
-    assert_equal json[:message], "Object 'chungachanga' does not exist"
+    assert_equal json[:message], 'Object does not exist'
+  end
+
+  private
+
+  def generate_token(user)
+    Base64.encode64("#{user.username}:#{user.plain_text_password}")
+  end
+
+  def auth_headers(user)
+    { 'Authorization' => "Basic #{generate_token(user)}" }
   end
 end
