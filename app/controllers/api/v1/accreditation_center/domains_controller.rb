27d6c9b70c6cc90c1b1721cf8b83d89f737fa5ac
tsoganov <sergei.tsoganov@ut.ee>
2025-11-20 16:13:53 +0200

    Refactored accreditation api

--- app/controllers/api/v1/accreditation_center/domains_controller.rb
+++ app/controllers/api/v1/accreditation_center/domains_controller.rb
@@ -8,9 +8,9 @@ module Api
           @domain = Domain.find_by(name: params[:name])
 
           if @domain
-            render json: { code: 1000, domain: Serializers::Repp::Domain.new(@domain,
-                                                                             sponsored: true).to_json },
-                   status: :ok
+            render json: {
+              domain: Serializers::Repp::Domain.new(@domain, sponsored: true).to_json
+            }, status: :ok
           else
             render json: { errors: 'Domain not found' }, status: :not_found
           end
